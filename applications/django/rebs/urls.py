5d88d4c2839e185c5e6a7dbac6cd99c82569f935
Austin-Kho <kori.susie@gmail.com>
2022-01-31 01:34:47 +0900

    document app create

--- applications/django/rebs/urls.py
+++ applications/django/rebs/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('project/', include('project.urls')),
     path('contract/', include('contract.urls')),
     path('notice/', include('notice.urls')),
-    # path('docs/', include('document.urls')),
+    path('docs/', include('document.urls')),
 
     # pdf url
-    # path('pdf-bill/', PdfExportBill.as_view(), name='pdf-bill'),
-    # path('pdf-payments/', PdfExportPayments.as_view(), name='pdf-payments'),
+    path('pdf-bill/', PdfExportBill.as_view(), name='pdf-bill'),
+    path('pdf-payments/', PdfExportPayments.as_view(), name='pdf-payments'),
 ]
