aab564277da6755c8c1b787d31348e2a17dfd60c
Austin-Kho <kori.susie@gmail.com>
2022-01-30 22:18:39 +0900

    book app update

--- applications/django/_config/urls.py
+++ applications/django/_config/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('accounts/', include('account.urls')),
+
+    path('rebs/', include('_rebs.urls')),
+    path('', RedirectView.as_view(url='/rebs/dashboard/'), name='home'),
+
+    path('book/', include('books.urls')),
 ]
