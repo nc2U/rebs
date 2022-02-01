506136b88efa4193b568d3e94d856414980f23d7
Austin-Kho <kori.susie@gmail.com>
2022-02-01 15:48:40 +0900

    mdeditor setting update

--- applications/django/_config/settings/base.py
+++ applications/django/_config/settings/base.py
@@ -222,3 +222,32 @@ APP_ORDER = [
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+MDEDITOR_CONFIGS = {
+    'default': {
+        'language': 'en',
+        'width': '100%',  # Custom edit box width
+        'heigth': 500,  # Custom edit box height
+        'toolbar': ["undo", "redo", "|",
+                    "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+                    "h1", "h2", "h3", "h5", "h6", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "link", "reference-link", "code", "preformatted-text", "code-block", "table", "datetime", "emoji",
+                    "html-entities", "pagebreak", "|", "goto-line", "|", "help", "info",
+                    "||", "preview", "watch", "fullscreen"],  # custom edit box toolbar
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png"],  # image upload format type
+        'image_folder': 'mke_images',  # image save the folder name
+        'theme': 'default',  # edit box theme, dark / default
+        'preview_theme': 'default',  # Preview area theme, dark / default
+        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
+        'toolbar_autofixed': True,  # Whether the toolbar capitals
+        'search_replace': True,  # Whether to open the search for replacement
+        'emoji': True,  # whether to open the expression function
+        'tex': True,  # whether to open the tex chart function
+        'flow_chart': True,  # whether to open the flow chart function
+        'sequence': True,  # Whether to open the sequence diagram function
+        'watch': True,  # Live preview
+        'lineWrapping': True,  # lineWrapping
+        'lineNumbers': True  # lineNumbers
+    }
+}
