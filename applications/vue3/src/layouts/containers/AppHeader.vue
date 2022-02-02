5093170c378b46f3682e891ed769dff2a3e7ce6f
Austin-Kho <kori.susie@gmail.com>
2022-02-03 02:57:44 +0900

    Header update

--- applications/vue3/src/layouts/containers/AppHeader.vue
+++ applications/vue3/src/layouts/containers/AppHeader.vue
@@ -7,7 +7,7 @@
       <CHeaderBrand class="mx-auto d-lg-none" to="/">
         <CIcon :icon="logo" height="48" alt="Logo" />
       </CHeaderBrand>
-      <CHeaderNav class="d-none d-md-flex me-auto">
+      <CHeaderNav class="d-none d-lg-flex me-auto">
         <AppBreadcrumb />
       </CHeaderNav>
       <CHeaderNav class="ms-auto me-4">
