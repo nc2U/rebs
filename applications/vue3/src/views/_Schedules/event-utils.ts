234164b3621639ea976b4984f8b8b306dc85c558
Austin-Kho <kori.susie@gmail.com>
2022-02-03 03:23:22 +0900

    Schedules event-utils update

--- applications/vue3/src/views/_Schedules/event-utils.ts
+++ applications/vue3/src/views/_Schedules/event-utils.ts
@@ -1,19 +1,19 @@
 import { EventInput } from '@fullcalendar/vue3'
 
 let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+const todayStr: string = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
     title: 'All-day event',
-    start: todayStr
+    start: todayStr,
   },
   {
     id: createEventId(),
     title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
+    start: todayStr + 'T12:00:00',
+  },
 ]
 
 export function createEventId() {
