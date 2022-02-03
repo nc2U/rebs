1fd96e2818a082df7a7c1a25026d39f459237171
Austin-Kho <kori.susie@gmail.com>
2022-02-03 14:07:37 +0900

    TodoList update ing

--- applications/vue3/src/views/_Dashboard/components/TodoMVC.vue
+++ applications/vue3/src/views/_Dashboard/components/TodoMVC.vue
@@ -71,11 +71,11 @@
       </button>
     </footer>
   </section>
-  <footer class="info">
-    <p>Double-click to edit a todo</p>
-    <p>Written by <a href="http://evanyou.me">Evan You</a></p>
-    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-  </footer>
+  <!--  <footer class="info">-->
+  <!--    <p>Double-click to edit a todo</p>-->
+  <!--    <p>Written by <a href="http://evanyou.me">Evan You</a></p>-->
+  <!--    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>-->
+  <!--  </footer>-->
 </template>
 
 <script lang="ts">
