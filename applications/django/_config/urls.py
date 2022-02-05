278bf1bdfa0f6bf230ed2be1829f2f1f5ce7305e
Austin-Kho <kori.susie@gmail.com>
2022-02-06 02:01:37 +0900

    api index auth update

--- applications/django/_config/urls.py
+++ applications/django/_config/urls.py
@@ -25,6 +25,7 @@ from django.views.generic import RedirectView, TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
