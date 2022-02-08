bc9af9d4bc9a74746c1eb3d62c96f6f2e25d4a2d
Austin-Kho <kori.susie@gmail.com>
2022-02-08 16:52:20 +0900

    OrderAddForm update

--- applications/vue3/src/views/projects/OrderGroup/components/OrderAddForm.vue
+++ applications/vue3/src/views/projects/OrderGroup/components/OrderAddForm.vue
@@ -8,28 +8,28 @@
     <CRow class="p-2" color="success">
       <CCol md="3" class="mb-2">
         <CFormInput
-          v-model="aa"
+          v-model="form.order_number"
           placeholder="등록차수"
           type="number"
+          min="1"
           required
         />
       </CCol>
 
       <CCol md="3" class="mb-2">
-        <CFormSelect v-model="bb" placeholder="aa">
-          <option
-            v-for="(s, i) in sort"
-            :value="s.value"
-            :key="i"
-            :selected="s.value == ''"
-          >
-            {{ s.label }}
+        <CFormSelect v-model="form.sort" required>
+          <option v-for="(sort, i) in sorts" :value="sort.value" :key="i">
+            {{ sort.label }}
           </option>
         </CFormSelect>
       </CCol>
 
       <CCol md="3" class="mb-2">
-        <CFormInput v-model="cc" placeholder="차수그룹 명칭" required />
+        <CFormInput
+          v-model="form.order_group_name"
+          placeholder="차수그룹 명칭"
+          required
+        />
       </CCol>
 
       <CCol md="3" class="d-grid gap-2 d-lg-block mb-3">
@@ -37,29 +37,56 @@
       </CCol>
     </CRow>
   </CForm>
+
+  <ConfirmModal ref="confirmModal">
+    <template v-slot:header>&lt;진행 확인!&gt;</template>
+    <template v-slot:default>
+      프로젝트의 차수그룹 정보 등록을 진행하시겠습니까?
+    </template>
+    <template v-slot:footer>
+      <CButton color="primary" @click="modalAction">저장</CButton>
+    </template>
+  </ConfirmModal>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ConfirmModal from '@/components/Modals/ConfirmModal.vue'
 
 export default defineComponent({
   name: 'OrderAddForm',
+  components: { ConfirmModal },
   data() {
     return {
-      sort: [
+      sorts: [
         { label: '구분선택', value: '' },
         { label: '일반분양', value: '1' },
         { label: '조합모집', value: '2' },
       ],
+      form: {
+        order_number: null,
+        sort: '',
+        order_group_name: '',
+      },
       validated: false,
     }
   },
   methods: {
-    onSubmit() {
-      this.$emit('on-submit', {})
+    onSubmit(event: any) {
+      const form = event.currentTarget
+      if (form.checkValidity() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+
+        this.validated = true
+      } else {
+        ;(this as any).$refs.confirmModal.callModal()
+      }
+    },
+    modalAction() {
+      this.$emit('on-submit', this.form)
+      this.validated = false
     },
   },
 })
 </script>
-
-<style lang="scss" scoped></style>
