cc89b3b28199507da01cd621c7ba682ec9dde4e7
Austin-Kho <kori.susie@gmail.com>
2022-02-12 14:31:32 +0900

    Price app update ing

--- applications/vue3/src/views/projects/Price/components/PriceSelectForm.vue
+++ applications/vue3/src/views/projects/Price/components/PriceSelectForm.vue
@@ -1,47 +1,49 @@
 <template>
-  <CRow class="p-2">
-    <CCol md="4" class="mb-2">
-      <CRow>
-        <CFormLabel for="sel1" class="col-sm-3 col-form-label">
-          차수선택
-        </CFormLabel>
-        <CCol sm="9">
-          <CFormSelect
-            id="sel1"
-            v-model="order"
-            @change="onOrderSelect"
-            :disabled="orderDisabled"
-          >
-            <option value="">차수 선택</option>
-            <option v-for="order in orders" :key="order.pk" :value="order.pk">
-              {{ order.order_group_name }}
-            </option>
-          </CFormSelect>
-        </CCol>
-      </CRow>
-    </CCol>
+  <CCallout color="secondary">
+    <CRow class="p-2">
+      <CCol md="4" class="mb-2">
+        <CRow>
+          <CFormLabel for="sel1" class="col-sm-3 col-form-label">
+            차수선택
+          </CFormLabel>
+          <CCol sm="9">
+            <CFormSelect
+              id="sel1"
+              v-model="order"
+              @change="onOrderSelect"
+              :disabled="orderDisabled"
+            >
+              <option value="">---------</option>
+              <option v-for="order in orders" :key="order.pk" :value="order.pk">
+                {{ order.order_group_name }}
+              </option>
+            </CFormSelect>
+          </CCol>
+        </CRow>
+      </CCol>
 
-    <CCol md="4" class="mb-2">
-      <CRow>
-        <CFormLabel for="sel2" class="col-sm-3 col-form-label">
-          타입선택
-        </CFormLabel>
-        <CCol sm="9">
-          <CFormSelect
-            id="sel2"
-            v-model="type"
-            @change="onTypeSelect"
-            :disabled="order == ''"
-          >
-            <option value="">타입 선택</option>
-            <option v-for="type in types" :key="type.pk" :value="type.pk">
-              {{ type.name }}
-            </option>
-          </CFormSelect>
-        </CCol>
-      </CRow>
-    </CCol>
-  </CRow>
+      <CCol md="4" class="mb-2">
+        <CRow>
+          <CFormLabel for="sel2" class="col-sm-3 col-form-label">
+            타입선택
+          </CFormLabel>
+          <CCol sm="9">
+            <CFormSelect
+              id="sel2"
+              v-model="type"
+              @change="onTypeSelect"
+              :disabled="order == ''"
+            >
+              <option value="">---------</option>
+              <option v-for="type in types" :key="type.pk" :value="type.pk">
+                {{ type.name }}
+              </option>
+            </CFormSelect>
+          </CCol>
+        </CRow>
+      </CCol>
+    </CRow>
+  </CCallout>
 </template>
 
 <script lang="ts">
@@ -49,6 +51,8 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'PriceSelectForm',
+  components: {},
+
   data() {
     return {
       order: '',
