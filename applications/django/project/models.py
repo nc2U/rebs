f3eed2b98214aec3d9eaa3123fa26d52f609fd12
Austin-Kho <kori.susie@gmail.com>
2022-02-12 17:15:15 +0900

    project floor model ordering update

--- applications/django/project/models.py
+++ applications/django/project/models.py
@@ -79,7 +79,7 @@ class UnitFloorType(models.Model):  # 층별 타입
         return self.alias_name
 
     class Meta:
-        ordering = ['-project', '-end_floor', 'id']
+        ordering = ['-project', '-end_floor', '-id']
         verbose_name = '03. 층별 조건'
         verbose_name_plural = '03. 층별 조건'
 
