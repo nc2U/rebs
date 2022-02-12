f04c84e0c7d2d5ec9586cb19463f0909d40102c7
Austin-Kho <kori.susie@gmail.com>
2022-02-12 19:01:09 +0900

    contract List index update

--- applications/vue3/src/views/contracts/List/Index.vue
+++ applications/vue3/src/views/contracts/List/Index.vue
@@ -1,41 +1,29 @@
 <template>
-  <CCard class="mb-4">
-    <CCardHeader>
-      <CIcon name="cil-justify-center" />
-      <strong class="pl-1"> {{ pageTitle }}</strong>
-    </CCardHeader>
+  <ContentHeader :page-title="pageTitle" :nav-menu="navMenu">
+    <IndexSummary :project="project" />
+  </ContentHeader>
 
-    <CCardBody>
-      <HeaderNav :menus="navMenu" />
-
-      <ProjectSelect :project="project" @proj-select="projSelect" />
-      <IndexSummary :project="project" />
-    </CCardBody>
-  </CCard>
-
-  <ContentBody></ContentBody>
+  <ContentBody> Hello world!</ContentBody>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HeaderMixin from '@/views/contracts/_menu/headermixin1'
-import ProjectMixin from '@/views/projects/projectMixin'
-
-import HeaderNav from '@/components/HeaderNav.vue'
-import ProjectSelect from '@/layouts/ContentHeader/ProjectSelect/Index.vue'
 import IndexSummary from './components/IndexSummary.vue'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
-// import IndexList from './components/IndexList.vue'
+import { mapState } from 'vuex'
 
 export default defineComponent({
   name: 'ContractIndex',
-  mixins: [HeaderMixin, ProjectMixin],
+  mixins: [HeaderMixin],
   components: {
-    HeaderNav,
-    ProjectSelect,
+    ContentHeader,
     IndexSummary,
     ContentBody,
-    // IndexList,
+  },
+  computed: {
+    ...mapState('project', ['project']),
   },
 })
 </script>
