c7b525cb98856dbbcd880369229ef64c7084f2e1
Austin-Kho <kori.susie@gmail.com>
2022-02-13 22:44:00 +0900

    contract store update

--- applications/vue3/src/store/modules/contract/getters.ts
+++ applications/vue3/src/store/modules/contract/getters.ts
@@ -1,3 +1,21 @@
-const getters = {}
+import { ContractState } from '@/store/modules/contract/state'
+
+const getters = {
+  contractIndex: (state: ContractState) => {
+    return state.contractList.map(c => ({
+      pk: c.pk,
+      serial_number: c.serial_number,
+      order_group: c.order_group,
+      unit_type: c.contractunit?.unit_type,
+      type_color: c.contractunit?.unitnumber?.unit_type,
+      unit_number: `${c.contractunit?.unitnumber?.bldg_no}-${c.contractunit?.unitnumber?.bldg_unit_no}`,
+      contractor: c.contractor?.name,
+      is_registed: c.contractor?.is_registed,
+      address: c.contractor?.contractoraddress?.dm_address1,
+      cell_phone: c.contractor?.contractorcontact?.cell_phone,
+      contract_date: c.contractor?.contract_date,
+    }))
+  },
+}
 
 export default getters
--- applications/vue3/src/store/modules/contract/state.ts
+++ applications/vue3/src/store/modules/contract/state.ts
@@ -1,68 +1,44 @@
 interface UnitNumber {
   pk: number
-  project: string
+  unit_type: string
   floor_type: string
   bldg_no: string
   bldg_unit_no: string
-  contract_unit: number
-  bldg_line: number
-  floor_no: number
-  is_hold: boolean
-  hold_reason: string
 }
 
 interface ContractUnit {
   pk: number
-  project: string
   unit_type: string
-  unit_code: string
   unitnumber: UnitNumber | null
-  contract: number
 }
 
 interface ContractorAddress {
   pk: number
-  contractor: number
-  id_zipcode: string
-  id_address1: string
-  id_address2: string
-  id_address3: string
-  dm_zipcode: string
   dm_address1: string
-  dm_address2: string
-  dm_address3: string
 }
 
 interface ContractorContact {
   pk: number
-  contractor: number
   cell_phone: string
-  home_phone: string
-  other_phone: string
   email: string
 }
 
 interface Contractor {
   pk: number
-  contract: number
   name: string
-  birth_date: string
-  gender: string
+  is_registed: boolean
   contractoraddress: ContractorAddress | null
   contractorcontact: ContractorContact | null
-  is_registed: boolean
   status: string
-  reservation_date: string | null
   contract_date: string
-  note: string
 }
 
 export interface Contract {
   pk: number
   project: number
-  order_group: number
   serial_number: string
   activation: boolean
+  order_group: number
   contractunit: ContractUnit | null
   contractor: Contractor | null
   user: number
