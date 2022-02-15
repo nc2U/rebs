6d040c13becfa74da037d957876f9c4af0b73623
Austin-Kho <kori.susie@gmail.com>
2022-02-15 12:38:22 +0900

    project Type app debug

--- applications/vue3/src/views/projects/Type/components/TypeFormList.vue
+++ applications/vue3/src/views/projects/Type/components/TypeFormList.vue
@@ -34,7 +34,7 @@
 
     <CTableBody v-else>
       <CTableRow>
-        <CTableDataCell colspan="5" class="text-center p-5 text-danger">
+        <CTableDataCell colspan="8" class="text-center p-5 text-danger">
           등록된 데이터가 없습니다.
         </CTableDataCell>
       </CTableRow>
