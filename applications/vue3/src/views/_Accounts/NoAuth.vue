292c1381c16769deb5cad08246fcd5fc41a29c1a
Austin-Kho <kori.susie@gmail.com>
2022-02-21 18:01:14 +0900

    NoAuth component update

--- applications/vue3/src/views/_Accounts/NoAuth.vue
+++ applications/vue3/src/views/_Accounts/NoAuth.vue
@@ -1,23 +1,36 @@
 <template>
-  <h3>Not Authorization!</h3>
+  <ContentBody>
+    <CCardBody class="blank-body text-center">
+      <span> Authorization not granted! </span>
+      <p class="text-medium-emphasis">
+        이 페이지에 대한 열람 권한이 없습니다. 관리자에게 문의하여 주십시요.
+      </p>
+    </CCardBody>
+    <CCardFooter>&nbsp;</CCardFooter>
+  </ContentBody>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
 
 export default defineComponent({
-  name: '',
-  components: {},
-  props: {},
-  setup() {
-    return {}
+  name: 'NoAuth',
+  components: {
+    ContentBody,
   },
-  data() {
-    return {
-      sample: '',
-    }
-  },
-  computed: {},
-  methods: {},
 })
 </script>
+
+<style lang="scss" scoped>
+.card-body {
+  height: 600px;
+  padding-top: 230px;
+}
+
+.card-body span {
+  color: darkred;
+  font-size: 2em;
+  line-height: 80px;
+}
+</style>
