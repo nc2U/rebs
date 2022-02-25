02822069aade98691a16a69b468d1c677f987410
Austin-Kho <kori.susie@gmail.com>
2022-02-25 20:33:23 +0900

    project cash url update

--- applications/django/cash/urls.py
+++ applications/django/cash/urls.py
@@ -5,18 +5,16 @@ app_name = 'cash-inout'
 
 urlpatterns = [
     path('dayly-report/', DaylyCashReport.as_view(), name='report'),
-    path('', CashInoutLV.as_view(), name='index'),
-    path('create/', CashInoutCV.as_view(), name='create'),
-    path('update/<int:pk>/', CashInoutUV.as_view(), name='update'),
-    path('delete/<int:pk>/', CashInoutDV, name='delete'),
-
-    path('project/report/', ProjectCashReport.as_view(), name='project-report'),
-    path('project/', ProjectCashInoutLV.as_view(), name='project-index'),
-    path('project/create/', ProjectCashInoutCV.as_view(), name='project-create'),
-    path('project/update/<int:pk>/', ProjectCashInoutUV.as_view(), name='project-update'),
-    path('project/delete/<int:pk>/', ProjectCashInoutDV, name='project-delete'),
-
-    path('project/payment/', SalesPaymentLV.as_view(), name='payment-index'),
-    path('project/payment/register/', SalesPaymentRegister.as_view(), name='payment-register'),
-    path('project/payment/delete/<int:pk>/', paymentDeleteView, name='payment-delete'),
+    path('cash/', CashInoutLV.as_view(), name='index'),
+    path('cash/create/', CashInoutCV.as_view(), name='create'),
+    path('cash/update/<int:pk>/', CashInoutUV.as_view(), name='update'),
+    path('cash/delete/<int:pk>/', CashInoutDV, name='delete'),
+    path('project-cash/report/', ProjectCashReport.as_view(), name='project-report'),
+    path('project-cash/', ProjectCashInoutLV.as_view(), name='project-index'),
+    path('project-cash/create/', ProjectCashInoutCV.as_view(), name='project-create'),
+    path('project-cash/update/<int:pk>/', ProjectCashInoutUV.as_view(), name='project-update'),
+    path('project-cash/delete/<int:pk>/', ProjectCashInoutDV, name='project-delete'),
+    path('project-cash/payment/', SalesPaymentLV.as_view(), name='payment-index'),
+    path('project-cash/payment/register/', SalesPaymentRegister.as_view(), name='payment-register'),
+    path('project-cash/payment/delete/<int:pk>/', paymentDeleteView, name='payment-delete'),
 ]
