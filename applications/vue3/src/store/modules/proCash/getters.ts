9a7cc898de264d3de970d7772ac6283e66e5c6df
Austin-Kho <kori.susie@gmail.com>
2022-02-27 18:34:54 +0900

    proCash store getters update

--- applications/vue3/src/store/modules/proCash/getters.ts
+++ applications/vue3/src/store/modules/proCash/getters.ts
@@ -6,15 +6,18 @@ const getters = {
       ? state.proCashBookList.map((p: any) => ({
           pk: p.pk,
           project: p.project,
-          sort: state.sortList
+          sort: p.sort,
+          sort_desc: state.sortList
             ? state.sortList.filter(s => s.pk === p.sort).map(s => s.name)[0]
             : '',
-          project_account_d1: state.formAccD1List
+          project_account_d1: p.project_account_d1,
+          project_account_d1_desc: state.formAccD1List
             ? state.formAccD1List
                 .filter(d => d.pk === p.project_account_d1)
                 .map(d => d.name)[0]
             : '',
-          project_account_d2: state.formAccD2List
+          project_account_d2: p.project_account_d2,
+          project_account_d2_desc: state.formAccD2List
             ? state.formAccD2List
                 .filter(d => d.pk === p.project_account_d2)
                 .map(d => d.name)[0]
@@ -22,14 +25,16 @@ const getters = {
           is_contract_payment: p.is_contract_payment,
           content: p.content,
           trader: p.trader,
-          bank_account: state.proBankAccountList
+          bank_account: p.bank_account,
+          bank_account_desc: state.proBankAccountList
             ? state.proBankAccountList
                 .filter(b => b.pk === p.bank_account)
                 .map(b => b.alias_name)[0]
             : '',
           income: p.income,
           outlay: p.outlay,
-          evidence: p.evidence_desc,
+          evidence: p.evidence,
+          evidence_desc: p.evidence_desc,
           deal_date: p.deal_date,
         }))
       : []
