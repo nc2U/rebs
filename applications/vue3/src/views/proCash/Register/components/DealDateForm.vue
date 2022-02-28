f59746eab6f269757e49057477511f642b5fe60e
Austin-Kho <kori.susie@gmail.com>
2022-02-28 14:47:34 +0900

    proCash Register app update ing

--- applications/vue3/src/views/proCash/Register/components/DealDateForm.vue
+++ applications/vue3/src/views/proCash/Register/components/DealDateForm.vue
@@ -1,8 +1,13 @@
 <template>
   <CCallout>
     <CRow>
-      <CCol md="3">
-        <CFormInput placeholder="거래일자" />
+      <CFormLabel class="col-lg-1 col-form-label">거래일자</CFormLabel>
+      <CCol md="6" lg="3">
+        <CFormInput
+          :value="getToday"
+          v-maska="'####-##-##'"
+          placeholder="거래일자"
+        />
       </CCol>
     </CRow>
   </CCallout>
@@ -10,9 +15,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { maska } from 'maska'
+import moment from 'moment'
 
 export default defineComponent({
-  name: '',
+  name: 'DealDateForm',
+  directives: { maska },
   components: {},
   props: {},
   setup() {
@@ -23,7 +31,12 @@ export default defineComponent({
       sample: '',
     }
   },
-  computed: {},
+  computed: {
+    getToday() {
+      const today = new Date()
+      return moment(today).format('YYYY-MM-DD')
+    },
+  },
   methods: {},
 })
 </script>
