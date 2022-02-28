1c3b3e8ae016d6dd676ec48c10884d83eec73478
Austin-Kho <kori.susie@gmail.com>
2022-02-28 16:46:44 +0900

    DatePicker component create

---
+++ applications/vue3/src/components/DatePicker/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <Datepicker
+    name="date-picker"
+    textInput
+    autoApply
+    :format="dateFormat"
+    position="left"
+    :textInputOptions="textInputOptions"
+    inputClassName="form-control"
+    :enableTimePicker="false"
+    v-maska="'####-##-##'"
+    teleport="#app"
+    :dark="dark"
+    required
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Datepicker from 'vue3-date-time-picker'
+import moment from 'moment'
+import { maska } from 'maska'
+
+export default defineComponent({
+  name: 'DatePicker',
+  directives: { maska },
+  components: { Datepicker },
+  props: {},
+  setup() {
+    return {}
+  },
+  computed: {
+    textInputOptions() {
+      return {
+        format: 'YYYY-MM-DD',
+      }
+    },
+    dark(this: any) {
+      return this.$store.state.theme === 'dark'
+    },
+  },
+  methods: {
+    dateFormat: (date: any) => moment(date).format('YYYY-MM-DD'),
+  },
+})
+</script>
