0ec39c193adfd7a6fac27445050c8d0c2e89fe64
Austin-Kho <kori.susie@gmail.com>
2022-03-01 23:36:55 +0900

    comCash ListController update

--- applications/vue3/src/views/comCash/Manage/components/ListController.vue
+++ applications/vue3/src/views/comCash/Manage/components/ListController.vue
@@ -1,28 +1,31 @@
 <template>
   <CCallout color="primary" class="pb-0 mb-4">
     <CRow>
-      <CCol lg="8">
+      <CCol lg="3">
         <CRow>
-          <CCol md="6" lg="2" class="mb-3">
-            <CFormInput
-              v-model="form.from_date"
+          <CCol md="6" class="mb-3">
+            <DatePicker
+              v-model="from_date"
               @keydown.enter="listFiltering(1)"
               v-maska="'####-##-##'"
               placeholder="시작일 (From)"
             />
           </CCol>
-
-          <CCol md="6" lg="2" class="mb-3">
-            <CFormInput
-              v-model="form.to_date"
+          <CCol md="6" class="mb-3">
+            <DatePicker
+              v-model="to_date"
               @keydown.enter="listFiltering(1)"
               v-maska="'####-##-##'"
               placeholder="종료일 (To)"
             />
           </CCol>
+        </CRow>
+      </CCol>
 
-          <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="form.sort" @change="sortSelect">
+      <CCol lg="5">
+        <CRow>
+          <CCol md="6" lg="3" class="mb-3">
+            <CFormSelect v-model="sort" @change="sortSelect">
               <option value="">구분</option>
               <option value="1">입금</option>
               <option value="2">출금</option>
@@ -30,8 +33,8 @@
             </CFormSelect>
           </CCol>
 
-          <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="form.account_d1" @change="accountD1Select">
+          <CCol md="6" lg="3" class="mb-3">
+            <CFormSelect v-model="account_d1" @change="accountD1Select">
               <option value="">계정[대분류]</option>
               <option
                 v-for="acc1 in formAccD1List"
@@ -43,11 +46,11 @@
             </CFormSelect>
           </CCol>
 
-          <CCol md="6" lg="2" class="mb-3">
+          <CCol md="6" lg="3" class="mb-3">
             <CFormSelect
-              v-model="form.account_d2"
+              v-model="account_d2"
               @change="accountD2Select"
-              :disabled="!this.form.account_d1"
+              :disabled="!this.account_d1"
             >
               <option value="">계정[중분류]</option>
               <option
@@ -60,11 +63,11 @@
             </CFormSelect>
           </CCol>
 
-          <CCol md="6" lg="2" class="mb-3">
+          <CCol md="6" lg="3" class="mb-3">
             <CFormSelect
-              v-model="form.account_d3"
+              v-model="account_d3"
               @change="listFiltering(1)"
-              :disabled="!this.form.account_d1"
+              :disabled="!this.account_d1"
             >
               <option value="">계정[소분류]</option>
               <option
@@ -82,7 +85,7 @@
       <CCol lg="4">
         <CRow>
           <CCol md="6" lg="4" class="mb-3">
-            <CFormSelect v-model="form.bank_account" @change="listFiltering(1)">
+            <CFormSelect v-model="bank_account" @change="listFiltering(1)">
               <option value="">거래계좌</option>
               <option v-for="acc in comBankList" :value="acc.pk" :key="acc.pk">
                 {{ acc.alias_name }}
@@ -93,7 +96,7 @@
           <CCol md="6" lg="8" class="mb-3">
             <CInputGroup class="flex-nowrap">
               <CFormInput
-                v-model="form.search"
+                v-model="search"
                 @keydown.enter="listFiltering(1)"
                 placeholder="적요, 거래처 검색"
                 aria-label="Username"
@@ -105,6 +108,7 @@
         </CRow>
       </CCol>
     </CRow>
+
     <CRow>
       <CCol color="warning" class="p-2 pl-3">
         <strong>
@@ -122,36 +126,36 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import DatePicker from '@/components/DatePicker/index.vue'
 import { maska } from 'maska'
 import { mapState } from 'vuex'
 
 export default defineComponent({
   name: 'ListController',
+  components: { DatePicker },
   directives: { maska },
   data() {
     return {
-      form: {
-        from_date: '',
-        to_date: '',
-        sort: '',
-        account_d1: '',
-        account_d2: '',
-        account_d3: '',
-        bank_account: '',
-        search: '',
-      },
+      from_date: '',
+      to_date: '',
+      sort: '',
+      account_d1: '',
+      account_d2: '',
+      account_d3: '',
+      bank_account: '',
+      search: '',
     }
   },
   computed: {
     formsCheck(this: any) {
-      const a = this.form.from_date === ''
-      const b = this.form.to_date === ''
-      const c = this.form.sort === ''
-      const d = this.form.account_d1 === ''
-      const e = this.form.account_d2 === ''
-      const f = this.form.account_d3 === ''
-      const g = this.form.bank_account === ''
-      const h = this.form.search === ''
+      const a = this.from_date === ''
+      const b = this.to_date === ''
+      const c = this.sort === ''
+      const d = this.account_d1 === ''
+      const e = this.account_d2 === ''
+      const f = this.account_d3 === ''
+      const g = this.bank_account === ''
+      const h = this.search === ''
       return a && b && c && d && e && f && g && h
     },
     ...mapState('comCash', [
@@ -162,36 +166,46 @@ export default defineComponent({
       'cashBookCount',
     ]),
   },
+  watch: {
+    from_date() {
+      this.listFiltering(1)
+    },
+    to_date() {
+      this.listFiltering(1)
+    },
+  },
   methods: {
     sortSelect() {
       this.listFiltering(1)
-      this.form.account_d1 = ''
-      this.form.account_d2 = ''
-      this.form.account_d3 = ''
+      this.account_d1 = ''
+      this.account_d2 = ''
+      this.account_d3 = ''
     },
     accountD1Select() {
       this.listFiltering(1)
-      this.form.account_d2 = ''
-      this.form.account_d3 = ''
+      this.account_d2 = ''
+      this.account_d3 = ''
     },
     accountD2Select() {
       this.listFiltering(1)
-      this.form.account_d3 = ''
+      this.account_d3 = ''
     },
-    listFiltering(page = 1) {
-      this.$nextTick(() =>
-        this.$emit('list-filtering', { ...{ page }, ...this.form }),
-      )
+    listFiltering(this: any, page = 1) {
+      this.$nextTick(() => {
+        this.from_date = this.from_date ? this.dateFormat(this.from_date) : ''
+        this.to_date = this.to_date ? this.dateFormat(this.to_date) : ''
+        return this.$emit('list-filtering', { ...{ page }, ...this })
+      })
     },
     resetForm() {
-      this.form.from_date = ''
-      this.form.to_date = ''
-      this.form.sort = ''
-      this.form.account_d1 = ''
-      this.form.account_d2 = ''
-      this.form.account_d3 = ''
-      this.form.bank_account = ''
-      this.form.search = ''
+      this.from_date = ''
+      this.to_date = ''
+      this.sort = ''
+      this.account_d1 = ''
+      this.account_d2 = ''
+      this.account_d3 = ''
+      this.bank_account = ''
+      this.search = ''
       this.listFiltering(1)
     },
   },
