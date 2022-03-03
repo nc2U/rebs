cdd4409513f16649f49377f8b027d273b3bf46f1
Austin-Kho <kori.susie@gmail.com>
2022-03-03 22:01:49 +0900

    test commit3

--- applications/vue3/src/views/contracts/List/components/ListController.vue
+++ applications/vue3/src/views/contracts/List/components/ListController.vue
@@ -4,7 +4,7 @@
       <CCol lg="7">
         <CRow>
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="status" @change="listFiltering(1)">
+            <CFormSelect v-model="form.status" @change="listFiltering(1)">
               <option value="2">계약 현황</option>
               <option value="1">청약 현황</option>
               <option value="3">해지 신청</option>
@@ -12,7 +12,7 @@
           </CCol>
 
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="order_group" @change="listFiltering(1)">
+            <CFormSelect v-model="form.order_group" @change="listFiltering(1)">
               <option value="">차수선택</option>
               <option
                 v-for="order in orderGroupList"
@@ -25,7 +25,7 @@
           </CCol>
 
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="unit_type" @change="listFiltering(1)">
+            <CFormSelect v-model="form.unit_type" @change="listFiltering(1)">
               <option value="">타입선택</option>
               <option
                 v-for="type in simpleTypes"
@@ -38,7 +38,7 @@
           </CCol>
 
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="building" @change="listFiltering(1)">
+            <CFormSelect v-model="form.building" @change="listFiltering(1)">
               <option value="">동 선택</option>
               <option
                 v-for="bldg in buildingList"
@@ -50,7 +50,7 @@
             </CFormSelect>
           </CCol>
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="registed" @change="listFiltering(1)">
+            <CFormSelect v-model="form.registed" @change="listFiltering(1)">
               <option value="">인가 구분</option>
               <option value="true">인가</option>
               <option value="false">미인가</option>
@@ -58,7 +58,7 @@
           </CCol>
 
           <CCol md="6" lg="2" class="mb-3">
-            <CFormSelect v-model="ordering" @change="listFiltering(1)">
+            <CFormSelect v-model="form.ordering" @change="listFiltering(1)">
               <option value="-created_at">등록일시 내림차순</option>
               <option value="created_at">등록일시 올림차순</option>
               <option value="-contractor__contract_date">
@@ -80,7 +80,7 @@
         <CRow>
           <CCol md="6" class="mb-3">
             <DatePicker
-              v-model="from_date"
+              v-model="form.from_date"
               @keydown.enter="listFiltering(1)"
               v-maska="'####-##-##'"
               placeholder="계약일 (From)"
@@ -89,7 +89,7 @@
 
           <CCol md="6" class="mb-3">
             <DatePicker
-              v-model="to_date"
+              v-model="form.to_date"
               @keydown.enter="listFiltering(1)"
               v-maska="'####-##-##'"
               placeholder="계약일 (To)"
@@ -103,7 +103,7 @@
           <CCol class="mb-3">
             <CInputGroup class="flex-nowrap">
               <CFormInput
-                v-model="search"
+                v-model="form.search"
                 @keydown.enter="listFiltering(1)"
                 placeholder="계약자, 일련번호, 비고"
                 aria-label="Username"
@@ -140,28 +140,30 @@ export default defineComponent({
   directives: { maska },
   data() {
     return {
-      status: '2',
-      order_group: '',
-      unit_type: '',
-      building: '',
-      registed: '',
-      from_date: '',
-      to_date: '',
-      ordering: '-created_at',
-      search: '',
+      form: {
+        status: '2',
+        order_group: '',
+        unit_type: '',
+        building: '',
+        registed: '',
+        from_date: '',
+        to_date: '',
+        ordering: '-created_at',
+        search: '',
+      },
     }
   },
   computed: {
     formsCheck(this: any) {
-      const a = this.status === '2'
-      const b = this.order_group === ''
-      const c = this.unit_type === ''
-      const d = this.building === ''
-      const e = this.registed === ''
-      const f = this.from_date === ''
-      const g = this.to_date === ''
-      const h = this.ordering === '-created_at'
-      const i = this.search === ''
+      const a = this.form.status === '2'
+      const b = this.form.order_group === ''
+      const c = this.form.unit_type === ''
+      const d = this.form.building === ''
+      const e = this.form.registed === ''
+      const f = this.form.from_date === ''
+      const g = this.form.to_date === ''
+      const h = this.form.ordering === '-created_at'
+      const i = this.form.search === ''
       const groupA = a && b && c && d && e
       const groupB = f && g && h && i
       return groupA && groupB
@@ -170,32 +172,28 @@ export default defineComponent({
     ...mapState('project', ['buildingList']),
     ...mapGetters('project', ['simpleTypes']),
   },
-  watch: {
-    from_date() {
-      this.listFiltering(1)
-    },
-    to_date() {
-      this.listFiltering(1)
-    },
-  },
   methods: {
     listFiltering(this: any, page = 1) {
       this.$nextTick(() => {
-        this.from_date = this.from_date ? this.dateFormat(this.from_date) : ''
-        this.to_date = this.to_date ? this.dateFormat(this.to_date) : ''
-        this.$emit('cont-filtering', { ...{ page }, ...this })
+        this.form.from_date = this.form.from_date
+          ? this.dateFormat(this.form.from_date)
+          : ''
+        this.form.to_date = this.form.to_date
+          ? this.dateFormat(this.form.to_date)
+          : ''
+        this.$emit('cont-filtering', { ...{ page }, ...this.form })
       })
     },
     resetForm() {
-      this.status = '2'
-      this.order_group = ''
-      this.unit_type = ''
-      this.building = ''
-      this.registed = ''
-      this.from_date = ''
-      this.to_date = ''
-      this.ordering = '-created_at'
-      this.search = ''
+      this.form.status = '2'
+      this.form.order_group = ''
+      this.form.unit_type = ''
+      this.form.building = ''
+      this.form.registed = ''
+      this.form.from_date = ''
+      this.form.to_date = ''
+      this.form.ordering = '-created_at'
+      this.form.search = ''
       this.listFiltering(1)
     },
   },
