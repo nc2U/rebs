e2d971a4e947a7baa72f4bab0e7c507ef38e042d
Austin-Kho <kori.susie@gmail.com>
2022-03-15 11:38:36 +0900

    contract list update

--- applications/vue3/src/views/contracts/List/components/Contract.vue
+++ applications/vue3/src/views/contracts/List/components/Contract.vue
@@ -1,5 +1,5 @@
 <template>
-  <CTableRow v-if="contract">
+  <CTableRow v-if="contract" class="text-center">
     <CTableDataCell>
       <router-link
         :to="{ name: '계약등록 관리', query: { contract: contract.pk } }"
@@ -8,7 +8,7 @@
       </router-link>
     </CTableDataCell>
     <CTableDataCell>{{ contract.order_group.order_group_name }}</CTableDataCell>
-    <CTableDataCell>
+    <CTableDataCell class="text-left">
       <CIcon
         name="cibDiscover"
         :style="'color:' + contract.type_color"
@@ -17,7 +17,7 @@
       />
       {{ contract.unit_type }}
     </CTableDataCell>
-    <CTableDataCell>{{ contract.house_unit }}</CTableDataCell>
+    <CTableDataCell class="text-left">{{ contract.house_unit }}</CTableDataCell>
     <CTableDataCell>
       <router-link
         :to="{ name: '계약등록 관리', query: { contract: contract.pk } }"
@@ -25,22 +25,46 @@
         {{ contract.contractor }}
       </router-link>
     </CTableDataCell>
+    <CTableDataCell>{{ contract.last_paid_order }}</CTableDataCell>
+    <CTableDataCell class="text-right">
+      {{ numFormat(contract.total_paid) }}
+    </CTableDataCell>
     <CTableDataCell>
       <CBadge :color="contract.is_registed ? 'success' : 'danger'">
         {{ contract.is_registed ? '인가완료' : '미 인 가' }}
       </CBadge>
     </CTableDataCell>
-    <CTableDataCell>{{ contract.address }}</CTableDataCell>
+    <CTableDataCell class="text-left">
+      {{ cutString(contract.address, 13) }}
+    </CTableDataCell>
     <CTableDataCell>{{ contract.cell_phone }}</CTableDataCell>
     <CTableDataCell>{{ contract.contract_date }}</CTableDataCell>
+    <CTableDataCell>
+      <CButton
+        type="button"
+        color="success"
+        size="sm"
+        @click="
+          $router.push({
+            name: '계약등록 관리',
+            query: { contract: contract.pk },
+          })
+        "
+      >
+        수정
+      </CButton>
+    </CTableDataCell>
   </CTableRow>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CashesList from '@/views/comCash/Manage/components/CashesList.vue'
 
 export default defineComponent({
   name: 'Contract',
+  components: { CashesList },
+
   props: {
     contract: {
       type: Object,
