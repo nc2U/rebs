84d8dc5f3883481065f904a85c1c195d1c3ad64a
Austin-Kho <kori.susie@gmail.com>
2022-03-18 08:21:19 +0900

    Accounts description modal update

--- applications/vue3/src/views/proCash/Imprest/components/ProImprestList.vue
+++ applications/vue3/src/views/proCash/Imprest/components/ProImprestList.vue
@@ -1,33 +1,28 @@
 <template>
   <CTable hover responsive align="middle">
     <colgroup>
-      <col width="8%" />
-      <col width="6%" />
-      <col width="7%" />
-      <col width="10%" />
-      <col width="12%" />
-      <col width="11%" />
-      <col width="11%" />
-      <col width="10%" />
-      <col width="10%" />
-      <col width="9%" />
-      <col width="6%" />
+      <col width="8%"/>
+      <col width="6%"/>
+      <col width="7%"/>
+      <col width="10%"/>
+      <col width="12%"/>
+      <col width="11%"/>
+      <col width="11%"/>
+      <col width="10%"/>
+      <col width="10%"/>
+      <col width="9%"/>
+      <col width="6%"/>
     </colgroup>
 
     <CTableHead>
       <CTableRow color="dark" class="text-center">
         <CTableHeaderCell scope="col">거래일자</CTableHeaderCell>
         <CTableHeaderCell scope="col">구분</CTableHeaderCell>
-        <CTableHeaderCell scope="col">
-          계정
-          <router-link to="" @click="showD1">
-            <CIcon name="cilDescription" />
-          </router-link>
-        </CTableHeaderCell>
+        <CTableHeaderCell scope="col">계정</CTableHeaderCell>
         <CTableHeaderCell scope="col">
           세부계정
           <router-link to="" @click="showD1">
-            <CIcon name="cilDescription" />
+            <CIcon name="cilCog" @click="$refs.DAccount.callModal()"/>
           </router-link>
         </CTableHeaderCell>
         <CTableHeaderCell scope="col">적요</CTableHeaderCell>
@@ -35,7 +30,7 @@
         <CTableHeaderCell scope="col">
           거래계좌
           <!--          <router-link to="" @click="showD1">-->
-          <CIcon name="cilCog" />
+          <CIcon name="cilCog"/>
           <!--          </router-link>-->
         </CTableHeaderCell>
         <CTableHeaderCell scope="col">입금액</CTableHeaderCell>
@@ -47,35 +42,60 @@
 
     <CTableBody>
       <ProImprest
-        v-for="imprest in getProImprestLogs"
-        :imprest="imprest"
-        :key="imprest.pk"
-        @multi-submit="multiSubmit"
-        @on-delete="onDelete"
+          v-for="imprest in getProImprestLogs"
+          :imprest="imprest"
+          :key="imprest.pk"
+          @multi-submit="multiSubmit"
+          @on-delete="onDelete"
       />
     </CTableBody>
   </CTable>
 
   <CSmartPagination
-    :activePage="1"
-    :limit="8"
-    :pages="proImprestPages(15)"
-    class="mt-3"
-    @active-page-change="pageSelect"
+      :activePage="1"
+      :limit="8"
+      :pages="proImprestPages(15)"
+      class="mt-3"
+      @active-page-change="pageSelect"
   />
+
+  <AlertModal ref="DAccount" size="lg">
+    <template v-slot:header> 프로젝트 계정 분류 보기</template>
+    <template v-slot:default>
+      <CAccordion>
+        <CAccordionItem v-for="d1 in allAccD1List" :item-key="d1.pk">
+          <CAccordionHeader>{{ `[${d1.code}] ${d1.name} (${d1.description})` }}</CAccordionHeader>
+          <CAccordionBody class="pl-3">
+            <CRow
+                v-for="d2 in allAccD2List.filter(d2 => d2.d1 === d1.pk)"
+                :key="d2.pk"
+                class="pl-2 mb-2"
+            >
+              <CCol>
+                [{{ d2.code }}] {{ d2.name }} ------ ({{ d2.description }})
+              </CCol>
+            </CRow>
+          </CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
+    </template>
+    <template v-slot:footer></template>
+  </AlertModal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 import ProImprest from '@/views/proCash/Imprest/components/ProImprest.vue'
-import { mapGetters } from 'vuex'
+import AlertModal from '@/components/Modals/AlertModal.vue'
+import {mapGetters, mapState} from 'vuex'
 
 export default defineComponent({
   name: 'ProImprestList',
-  components: { ProImprest },
-  props: { project: Object },
+  components: {ProImprest, AlertModal},
+  props: {project: Object},
   computed: {
     ...mapGetters('proCash', ['proImprestPages', 'getProImprestLogs']),
+    ...mapState('proCash', ['allAccD1List', 'allAccD2List']),
   },
   methods: {
     pageSelect(page: number) {
--- applications/vue3/src/views/proCash/Manage/components/ProCashList.vue
+++ applications/vue3/src/views/proCash/Manage/components/ProCashList.vue
@@ -1,17 +1,17 @@
 <template>
   <CTable hover responsive align="middle">
     <colgroup>
-      <col width="8%" />
-      <col width="6%" />
-      <col width="7%" />
-      <col width="10%" />
-      <col width="12%" />
-      <col width="11%" />
-      <col width="11%" />
-      <col width="10%" />
-      <col width="10%" />
-      <col width="9%" />
-      <col width="6%" />
+      <col width="8%"/>
+      <col width="6%"/>
+      <col width="7%"/>
+      <col width="10%"/>
+      <col width="12%"/>
+      <col width="11%"/>
+      <col width="11%"/>
+      <col width="10%"/>
+      <col width="10%"/>
+      <col width="9%"/>
+      <col width="6%"/>
     </colgroup>
 
     <CTableHead>
@@ -19,10 +19,20 @@
         <CTableHeaderCell scope="col">거래일자</CTableHeaderCell>
         <CTableHeaderCell scope="col">구분</CTableHeaderCell>
         <CTableHeaderCell scope="col">계정</CTableHeaderCell>
-        <CTableHeaderCell scope="col">세부계정</CTableHeaderCell>
+        <CTableHeaderCell scope="col">
+          세부계정
+          <router-link to="" @click="showD1">
+            <CIcon name="cilCog" @click="$refs.DAccount.callModal()"/>
+          </router-link>
+        </CTableHeaderCell>
         <CTableHeaderCell scope="col">적요</CTableHeaderCell>
         <CTableHeaderCell scope="col">거래처</CTableHeaderCell>
-        <CTableHeaderCell scope="col">거래계좌</CTableHeaderCell>
+        <CTableHeaderCell scope="col">
+          거래계좌
+          <!--          <router-link to="" @click="showD1">-->
+          <CIcon name="cilCog"/>
+          <!--          </router-link>-->
+        </CTableHeaderCell>
         <CTableHeaderCell scope="col">입금액</CTableHeaderCell>
         <CTableHeaderCell scope="col">출금액</CTableHeaderCell>
         <CTableHeaderCell scope="col">지출증빙</CTableHeaderCell>
@@ -32,35 +42,60 @@
 
     <CTableBody>
       <ProCash
-        v-for="proCash in getProCashLogs"
-        :proCash="proCash"
-        :key="proCash.pk"
-        @multi-submit="multiSubmit"
-        @on-delete="onDelete"
+          v-for="proCash in getProCashLogs"
+          :proCash="proCash"
+          :key="proCash.pk"
+          @multi-submit="multiSubmit"
+          @on-delete="onDelete"
       />
     </CTableBody>
   </CTable>
 
   <CSmartPagination
-    :activePage="1"
-    :limit="8"
-    :pages="proCashPages(15)"
-    class="mt-3"
-    @active-page-change="pageSelect"
+      :activePage="1"
+      :limit="8"
+      :pages="proCashPages(15)"
+      class="mt-3"
+      @active-page-change="pageSelect"
   />
+
+  <AlertModal ref="DAccount" size="lg">
+    <template v-slot:header> 프로젝트 계정 분류 보기</template>
+    <template v-slot:default>
+      <CAccordion>
+        <CAccordionItem v-for="d1 in allAccD1List" :item-key="d1.pk" :key="d1.pk">
+          <CAccordionHeader>{{ `[${d1.code}] ${d1.name} (${d1.description})` }}</CAccordionHeader>
+          <CAccordionBody class="pl-3">
+            <CRow
+                v-for="d2 in allAccD2List.filter(d2 => d2.d1 === d1.pk)"
+                :key="d2.pk"
+                class="pl-2 mb-2"
+            >
+              <CCol>
+                [{{ d2.code }}] {{ d2.name }} ------ ({{ d2.description }})
+              </CCol>
+            </CRow>
+          </CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
+    </template>
+    <template v-slot:footer></template>
+  </AlertModal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 import ProCash from '@/views/proCash/Manage/components/ProCash.vue'
-import { mapGetters } from 'vuex'
+import AlertModal from '@/components/Modals/AlertModal.vue'
+import {mapGetters, mapState} from 'vuex'
 
 export default defineComponent({
   name: 'ProCashList',
-  components: { ProCash },
-  props: { project: Object },
+  components: {ProCash, AlertModal},
+  props: {project: Object},
   computed: {
     ...mapGetters('proCash', ['proCashPages', 'getProCashLogs']),
+    ...mapState('proCash', ['allAccD1List', 'allAccD2List']),
   },
   methods: {
     pageSelect(page: number) {
