f4d853efffc0fa3373fe0a04f7a85fd044ef3a01
Austin-Kho <kori.susie@gmail.com>
2022-03-20 23:01:23 +0900

    proCash Status layout  update

--- applications/vue3/src/views/proCash/Status/components/SummaryForBudget.vue
+++ applications/vue3/src/views/proCash/Status/components/SummaryForBudget.vue
@@ -1,5 +1,69 @@
 <template>
-  <h3>예산대비 집계</h3>
+  <CTable hover responsive bordered align="middle">
+    <colgroup>
+      <col width="5%" />
+      <col width="5%" />
+      <col width="10%" />
+      <col width="10%" />
+      <col width="14%" />
+      <col width="14%" />
+      <col width="14%" />
+      <col width="14%" />
+      <col width="14%" />
+    </colgroup>
+    <CTableHead>
+      <CTableRow>
+        <CTableDataCell colspan="8">
+          <strong>
+            <CIcon name="cilFolderOpen" />
+            사업예산 및 집행현황
+          </strong>
+          <small>(2022-03-20) 기준</small>
+        </CTableDataCell>
+        <CTableDataCell class="text-right">(단위: 원)</CTableDataCell>
+      </CTableRow>
+      <CTableRow color="dark" class="text-center">
+        <CTableHeaderCell colspan="4">구분</CTableHeaderCell>
+        <CTableHeaderCell>예산</CTableHeaderCell>
+        <CTableHeaderCell>전월 인출금액 누계</CTableHeaderCell>
+        <CTableHeaderCell>당월 인출금액</CTableHeaderCell>
+        <CTableHeaderCell>인출금액 합계</CTableHeaderCell>
+        <CTableHeaderCell>가용 예산합계</CTableHeaderCell>
+      </CTableRow>
+    </CTableHead>
+
+    <CTableBody>
+      <CTableRow class="text-right" v-for="i in 20" :key="i">
+        <CTableDataCell
+          color="info"
+          class="text-center"
+          rowspan="20"
+          v-if="i === 1"
+        >
+          사업비
+        </CTableDataCell>
+        <CTableDataCell class="text-center">건축비</CTableDataCell>
+        <CTableDataCell class="text-left">간접공사비</CTableDataCell>
+        <CTableDataCell class="text-left">각종 인입비</CTableDataCell>
+        <CTableDataCell>911,876,000</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+      </CTableRow>
+
+      <CTableRow color="dark" class="text-right">
+        <CTableHeaderCell colspan="4" class="text-center">
+          합계
+        </CTableHeaderCell>
+        <CTableHeaderCell>911,876,000</CTableHeaderCell>
+        <CTableHeaderCell>-</CTableHeaderCell>
+        <CTableHeaderCell>-</CTableHeaderCell>
+        <CTableHeaderCell>-</CTableHeaderCell>
+        <CTableHeaderCell>-</CTableHeaderCell>
+      </CTableRow>
+    </CTableBody>
+  </CTable>
 </template>
 
 <script lang="ts">
@@ -7,7 +71,6 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'SummaryForBudget',
-  components: {},
   props: {},
   setup() {
     return {}
