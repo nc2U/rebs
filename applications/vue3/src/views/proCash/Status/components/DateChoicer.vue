fc05c941f4bbef23c9cd49e4e59d5ae7a8645d76
Austin-Kho <kori.susie@gmail.com>
2022-03-21 13:16:31 +0900

    proCash DateChoicer update

--- applications/vue3/src/views/proCash/Status/components/DateChoicer.vue
+++ applications/vue3/src/views/proCash/Status/components/DateChoicer.vue
@@ -1,6 +1,7 @@
 <template>
   <CCallout color="success">
     <CRow>
+      <CFormLabel class="col-lg-1 col-form-label">기준일자</CFormLabel>
       <CCol md="6" lg="3">
         <DatePicker v-model="date" />
       </CCol>
@@ -10,21 +11,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import DatePicker from "@/components/DatePicker/index.vue";
+import DatePicker from '@/components/DatePicker/index.vue'
 
 export default defineComponent({
   name: 'DateChoicer',
   components: { DatePicker },
-  props: {},
-  setup() {
-    return {}
-  },
   data() {
     return {
       date: new Date(),
     }
   },
-  computed: {},
-  methods: {},
 })
 </script>
