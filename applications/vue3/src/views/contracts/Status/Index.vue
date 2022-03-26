cee9fa2371cb73c5d931aae879232cc3459950d9
Austin-Kho <kori.susie@gmail.com>
2022-03-27 00:34:53 +0900

    contract Status layout update

--- applications/vue3/src/views/contracts/Status/Index.vue
+++ applications/vue3/src/views/contracts/Status/Index.vue
@@ -5,7 +5,15 @@
     @header-select="onSelectAdd"
   />
 
-  <ContentBody></ContentBody>
+  <ContentBody>
+    <CCardBody class="pb-5">
+      <ContSummary />
+      <ExcelExport />
+      <ContractBoard />
+    </CCardBody>
+
+    <CCardFooter>&nbsp;</CCardFooter>
+  </ContentBody>
 </template>
 
 <script lang="ts">
@@ -13,6 +21,9 @@ import { defineComponent } from 'vue'
 import HeaderMixin from '@/views/contracts/_menu/headermixin'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
+import ContSummary from '@/views/contracts/Status/components/ContSummary.vue'
+import ExcelExport from '@/views/contracts/Status/components/ExcelExport.vue'
+import ContractBoard from '@/views/contracts/Status/components/ContractBoard.vue'
 import { mapState } from 'vuex'
 
 export default defineComponent({
@@ -21,6 +32,9 @@ export default defineComponent({
   components: {
     ContentHeader,
     ContentBody,
+    ContSummary,
+    ExcelExport,
+    ContractBoard,
   },
   computed: {
     ...mapState('project', ['project']),
