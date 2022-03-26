07c220c4982a11d86d26e09124b04a5abdb60321
Austin-Kho <kori.susie@gmail.com>
2022-03-27 01:05:18 +0900

    contract Status layout update

--- applications/vue3/src/views/contracts/Status/components/ContSummary.vue
+++ applications/vue3/src/views/contracts/Status/components/ContSummary.vue
@@ -1,5 +1,39 @@
 <template>
-  <h3>Hello World!</h3>
+  <CRow>
+    <CCol lg="6">
+      <CTable hover responsive bordered align="middle">
+        <colgroup>
+          <col width="25%" />
+          <col width="25%" />
+          <col width="25%" />
+          <col width="25%" />
+        </colgroup>
+        <CTableBody>
+          <CTableRow>
+            <CTableHeaderCell color="dark">총세대수</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+            <CTableHeaderCell color="dark">홀딩세대</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+          </CTableRow>
+
+          <CTableRow>
+            <CTableHeaderCell color="success">청약세대</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+            <CTableHeaderCell color="primary">계약세대</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+          </CTableRow>
+
+          <CTableRow>
+            <CTableHeaderCell color="dark">합계</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+            <CTableHeaderCell color="dark">잔여세대</CTableHeaderCell>
+            <CTableDataCell class="text-right">-</CTableDataCell>
+          </CTableRow>
+        </CTableBody>
+      </CTable>
+    </CCol>
+    <CCol lg="6">74 84A 84B</CCol>
+  </CRow>
 </template>
 
 <script lang="ts">
