f745fe7e4ccfce2a8cc01976389e9d0a319af5b3
Austin-Kho <kori.susie@gmail.com>
2022-06-09 00:32:29 +0900

    nginx settings update

--- nginx/cert-create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# cert create
-certbot certonly --manual --preferred-challenges=dns --email $ADMIN_EMAIL \
---server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d $SERVER_NAME -d *.$SERVER_NAME
