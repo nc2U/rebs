79b865ffcd3b5794198f971114ae14449dc479db
Austin-Kho <kori.susie@gmail.com>
2022-07-10 10:21:30 +0900

    pdf bill update complate

--- app/django/rebs/views.py
+++ app/django/rebs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 # --------------------------------------------------------
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.template.loader import render_to_string
