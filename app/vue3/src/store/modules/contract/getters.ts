1247418fb091420fced22e2c5d91cb5096e59a25
Austin-Kho <kori.susie@gmail.com>
2022-07-12 08:13:17 +0900

    contract store getter update

--- app/vue3/src/store/modules/contract/getters.ts
+++ app/vue3/src/store/modules/contract/getters.ts
@@ -28,6 +28,31 @@ const getters = {
     }))
   },
 
+  contractBill: (state: ContractState) => {
+    return state.contractList.map(c => ({
+      pk: c.pk,
+      ctor_pk: c.contractor?.pk,
+      serial_number: c.serial_number,
+      order_group: c.order_group,
+      unit_type: c.unit_type.name,
+      type_color: c.unit_type.color,
+      house_unit: c.keyunit?.houseunit?.__str__ || '[미정]',
+      contractor: c.contractor?.name,
+      total_paid:
+        c.payments.length !== 0
+          ? c.payments.map(o => o.income).reduce((res, item) => res + item)
+          : 0,
+      last_paid_order:
+        c.payments.length !== 0
+          ? c.payments
+              .filter(p => p.installment_order !== null)
+              .map(p => p.installment_order)
+              .pop()
+          : '-',
+      contract_date: c.contractor?.contract_date,
+    }))
+  },
+
   contractPages: (state: ContractState) => (itemsPerPage: number) => {
     return Math.ceil(state.contractsCount / itemsPerPage)
   },
