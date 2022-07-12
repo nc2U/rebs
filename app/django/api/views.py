a2e3c0be19a1149eb4413b8cf7edaf116fbebabe
Austin-Kho <kori.susie@gmail.com>
2022-07-12 15:22:10 +0900

    sales bill serialize udpate

--- app/django/api/views.py
+++ app/django/api/views.py
@@ -957,6 +957,7 @@ class BillIssueList(generics.ListCreateAPIView):
     name = 'bill_issue-list'
     queryset = SalesBillIssue.objects.all()
     serializer_class = SallesBillIssueSerializer
+    filter_fields = ('project',)
     permission_classes = (permissions.IsAuthenticated, IsStaffOrReadOnly)
 
     def perform_create(self, serializer):
