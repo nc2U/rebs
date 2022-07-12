4a21016d2b94f5728dd79c5c7ce930c0b8eea9fa
Austin-Kho <kori.susie@gmail.com>
2022-07-13 00:04:24 +0900

    salesbillissue form update

--- app/vue3/src/views/notices/Bill/components/SalesBillIssueForm.vue
+++ app/vue3/src/views/notices/Bill/components/SalesBillIssueForm.vue
@@ -16,289 +16,317 @@
     </CRow>
   </CAlert>
 
-  <CRow class="mb-1">
-    <CCol sm="4" md="2" xl="1">
-      <CFormLabel for="" class="col-form-label">발행일자</CFormLabel>
-    </CCol>
-    <CCol sm="8" md="4" xl="2">
-      <DatePicker
-        v-model="published_date"
-        v-maska="'####-##-##'"
-        placeholder="발행일자"
-        maxlength="10"
-        required
-      />
-    </CCol>
-  </CRow>
-
-  <CCollapse :visible="visible">
+  <CForm
+    class="needs-validation"
+    novalidate
+    :validated="validated"
+    @submit.prevent="onSubmit"
+  >
     <CRow class="mb-1">
       <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">발행회차</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="2">
-        <CFormSelect
-          v-model="form.now_payment_order"
-          placeholder="발행회차"
-          maxlength="10"
-          required
-        >
-          <option value="">--------</option>
-          <option v-for="po in payOrderList" :value="po.pk" :key="po.pk">
-            {{ po.__str__ }}
-          </option>
-        </CFormSelect>
-      </CCol>
-
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">당회 납부기한</CFormLabel>
+        <CFormLabel for="" class="col-form-label">발행일자</CFormLabel>
       </CCol>
       <CCol sm="8" md="4" xl="2">
         <DatePicker
-          v-model="form.now_due_date"
+          v-model="published_date"
           v-maska="'####-##-##'"
-          placeholder="당회 납부기한"
+          placeholder="발행일자"
           maxlength="10"
           required
         />
       </CCol>
     </CRow>
 
-    <CRow class="mb-1">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">시행자명</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="2">
-        <CFormInput
-          v-model="form.host_name"
-          placeholder="시행자명"
-          maxlength="20"
-          required
-        />
-      </CCol>
+    <CCollapse :visible="visible">
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">발행회차</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <CFormSelect
+            v-model="form.now_payment_order"
+            placeholder="발행회차"
+            maxlength="10"
+            required
+          >
+            <option value="">--------</option>
+            <option v-for="po in payOrderList" :value="po.pk" :key="po.pk">
+              {{ po.__str__ }}
+            </option>
+          </CFormSelect>
+        </CCol>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">시행자 전화</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="2">
-        <input
-          type="text"
-          class="form-control"
-          v-model="form.host_tel"
-          v-maska="['###-###-####', '###-####-####']"
-          placeholder="시행자 전화"
-          maxlength="13"
-          required
-        />
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">당회 납부기한</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <DatePicker
+            v-model="form.now_due_date"
+            v-maska="'####-##-##'"
+            placeholder="당회 납부기한"
+            maxlength="10"
+            required
+          />
+        </CCol>
+      </CRow>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">대행사명</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="2">
-        <CFormInput
-          v-model="form.agency"
-          placeholder="대행사명"
-          maxlength="20"
-          required
-        />
-      </CCol>
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">시행자명</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <CFormInput
+            v-model="form.host_name"
+            placeholder="시행자명"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">대행사 전화</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="2">
-        <input
-          type="text"
-          class="form-control"
-          v-model="form.agency_tel"
-          v-maska="['###-###-####', '###-####-####']"
-          placeholder="대행사 전화"
-          maxlength="13"
-          required
-        />
-      </CCol>
-    </CRow>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">시행자 전화</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <input
+            type="text"
+            class="form-control"
+            v-model="form.host_tel"
+            v-maska="['###-###-####', '###-####-####']"
+            placeholder="시행자 전화"
+            maxlength="13"
+            required
+          />
+        </CCol>
 
-    <CRow class="mb-1">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">수납은행[1]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_account1"
-          placeholder="수납은행[1]"
-          maxlength="20"
-          required
-        />
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">대행사명</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <CFormInput
+            v-model="form.agency"
+            placeholder="대행사명"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">계좌번호[1]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_number1"
-          placeholder="계좌번호[1]"
-          maxlength="20"
-          required
-        />
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">대행사 전화</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="2">
+          <input
+            type="text"
+            class="form-control"
+            v-model="form.agency_tel"
+            v-maska="['###-###-####', '###-####-####']"
+            placeholder="대행사 전화"
+            maxlength="13"
+            required
+          />
+        </CCol>
+      </CRow>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">예금주[1]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_host1"
-          placeholder="예금주[1]"
-          maxlength="20"
-          required
-        />
-      </CCol>
-    </CRow>
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">수납은행[1]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_account1"
+            placeholder="수납은행[1]"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-    <CRow class="mb-1">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">수납은행[2]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_account2"
-          placeholder="수납은행[2]"
-          maxlength="20"
-          required
-        />
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">계좌번호[1]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_number1"
+            placeholder="계좌번호[1]"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">계좌번호[2]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_number2"
-          placeholder="계좌번호[2]"
-          maxlength="20"
-          required
-        />
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">예금주[1]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_host1"
+            placeholder="예금주[1]"
+            maxlength="20"
+            required
+          />
+        </CCol>
+      </CRow>
 
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">예금주[2]</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="4" xl="3">
-        <CFormInput
-          v-model="form.bank_host2"
-          placeholder="예금주[2]"
-          maxlength="20"
-          required
-        />
-      </CCol>
-    </CRow>
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">수납은행[2]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_account2"
+            placeholder="수납은행[2]"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-    <CRow class="mb-1">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">시행자 주소</CFormLabel>
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">계좌번호[2]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_number2"
+            placeholder="계좌번호[2]"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-      <CCol xs="4" sm="8" md="2" xl="1" class="mb-1">
-        <CButton color="info">우편번호</CButton>
-      </CCol>
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">예금주[2]</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="4" xl="3">
+          <CFormInput
+            v-model="form.bank_host2"
+            placeholder="예금주[2]"
+            maxlength="20"
+            required
+          />
+        </CCol>
+      </CRow>
 
-      <CCol class="d-none d-sm-block d-md-none" sm="4" />
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">시행자 주소</CFormLabel>
+        </CCol>
 
-      <CCol xs="8" md="2" xl="1" class="mb-1">
-        <input
-          type="text"
-          class="form-control"
-          v-model="form.zipcode"
-          v-maska="'#####'"
-          placeholder="우편번호"
-          maxlength="5"
-          required
-        />
-      </CCol>
+        <CCol xs="4" sm="8" md="2" xl="1" class="mb-1">
+          <CButton color="info">우편번호</CButton>
+        </CCol>
 
-      <CCol class="d-none d-sm-block d-md-none" sm="4" />
+        <CCol class="d-none d-sm-block d-md-none" sm="4" />
 
-      <CCol sm="8" md="6" xl="4" class="mb-1">
-        <CFormInput
-          v-model="form.address1"
-          placeholder="메인 주소"
-          maxlength="20"
-          required
-        />
-      </CCol>
+        <CCol xs="8" md="2" xl="1" class="mb-1">
+          <input
+            type="text"
+            class="form-control"
+            v-model="form.zipcode"
+            v-maska="'#####'"
+            placeholder="우편번호"
+            maxlength="5"
+            required
+          />
+        </CCol>
 
-      <CCol class="d-none d-sm-block d-md-block d-xl-none" sm="4" md="2" />
+        <CCol class="d-none d-sm-block d-md-none" sm="4" />
 
-      <CCol sm="8" md="5" xl="2" class="mb-1">
-        <CFormInput
-          v-model="form.address2"
-          placeholder="상세 주소"
-          maxlngth="20"
-          required
-        />
-      </CCol>
+        <CCol sm="8" md="6" xl="4" class="mb-1">
+          <CFormInput
+            v-model="form.address1"
+            placeholder="메인 주소"
+            maxlength="20"
+            required
+          />
+        </CCol>
 
-      <CCol class="d-none d-sm-block d-md-none" sm="4" />
+        <CCol class="d-none d-sm-block d-md-block d-xl-none" sm="4" md="2" />
 
-      <CCol sm="8" md="5" xl="3" class="mb-1">
-        <CFormInput
-          v-model="form.address3"
-          placeholder="참고항목"
-          maxlength="20"
-          required
-        />
-      </CCol>
-    </CRow>
+        <CCol sm="8" md="5" xl="2" class="mb-1">
+          <CFormInput
+            v-model="form.address2"
+            placeholder="상세 주소"
+            maxlngth="20"
+            required
+          />
+        </CCol>
 
-    <CRow class="mb-1">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">고지서 제목</CFormLabel>
-      </CCol>
+        <CCol class="d-none d-sm-block d-md-none" sm="4" />
 
-      <CCol sm="8" md="10" lg="12" xl="11">
-        <CFormInput
-          v-model="form.title"
-          placeholder="고재서 제목"
-          maxlength="20"
-          required
-        />
-      </CCol>
-      <CCol class="d-none d-md-block d-lg-none" md="6" />
-    </CRow>
+        <CCol sm="8" md="5" xl="3" class="mb-1">
+          <CFormInput
+            v-model="form.address3"
+            placeholder="참고항목"
+            maxlength="20"
+            required
+          />
+        </CCol>
+      </CRow>
 
-    <CRow class="mb-3">
-      <CCol sm="4" md="2" xl="1">
-        <CFormLabel for="" class="col-form-label">고지서 내용</CFormLabel>
-      </CCol>
-      <CCol sm="8" md="10" lg="12" xl="11">
-        <CFormTextarea
-          v-model="form.content"
-          placeholder="고지서 내용"
-          rows="4"
-        />
-      </CCol>
-      <CCol class="d-none d-md-block d-lg-none" md="6" />
-    </CRow>
+      <CRow class="mb-1">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">고지서 제목</CFormLabel>
+        </CCol>
 
-    <CAlert color="secondary" class="text-right">
-      <CButton color="success">업데이트</CButton>
-    </CAlert>
-  </CCollapse>
+        <CCol sm="8" md="10" lg="12" xl="11">
+          <CFormInput
+            v-model="form.title"
+            placeholder="고재서 제목"
+            maxlength="20"
+            required
+          />
+        </CCol>
+        <CCol class="d-none d-md-block d-lg-none" md="6" />
+      </CRow>
+
+      <CRow class="mb-3">
+        <CCol sm="4" md="2" xl="1">
+          <CFormLabel for="" class="col-form-label">고지서 내용</CFormLabel>
+        </CCol>
+        <CCol sm="8" md="10" lg="12" xl="11">
+          <CFormTextarea
+            v-model="form.content"
+            placeholder="고지서 내용"
+            rows="4"
+          />
+        </CCol>
+        <CCol class="d-none d-md-block d-lg-none" md="6" />
+      </CRow>
+
+      <CAlert color="secondary" class="text-right">
+        <CButton type="submit" color="success" :disabled="formsCheck">
+          업데이트
+        </CButton>
+      </CAlert>
+    </CCollapse>
+  </CForm>
+
+  <DaumPostcode @addressPut="addressPut" ref="postCode" />
+
+  <ConfirmModal ref="confirmModal">
+    <template v-slot:header>회사정보</template>
+    <template v-slot:default>
+      회사정보 {{ confirmText }}을 진행하시겠습니까?
+    </template>
+    <template v-slot:footer>
+      <CButton :color="btnClass" @click="modalAction">저장</CButton>
+    </template>
+  </ConfirmModal>
+
+  <AlertModal ref="alertModal" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import DatePicker from '@/components/DatePicker/index.vue'
+import ConfirmModal from '@/components/Modals/ConfirmModal.vue'
+import AlertModal from '@/components/Modals/AlertModal.vue'
+import DaumPostcode from '@/components/DaumPostcode/index.vue'
+import addressMixin from '@/components/DaumPostcode/addressMixin'
 import { maska } from 'maska'
-import { mapState } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 
 export default defineComponent({
   name: 'SalesBillIssueForm',
+  components: { DatePicker, ConfirmModal, AlertModal, DaumPostcode },
+  mixins: [addressMixin],
   directives: { maska },
-  components: { DatePicker },
   props: {
     salesbillissue: Object,
   },
@@ -306,6 +334,8 @@ export default defineComponent({
     return {
       visible: false,
       published_date: new Date(),
+      now_order: null,
+      pk: '',
       form: {
         now_payment_order: '',
         now_due_date: '',
@@ -326,16 +356,46 @@ export default defineComponent({
         title: '',
         content: '',
       },
+      validated: false,
     }
   },
   computed: {
+    // formsCheck(this: any) {
+    //   const a =
+    //     this.form.now_payment_order === this.salesbillissue.now_payment_order
+    //   const b = this.form.now_due_date === this.now_order
+    //   const c = this.form.host_name === this.salesbillissue.host_name
+    //   const d = this.form.host_tel === this.salesbillissue.host_tel
+    //   const e = this.form.agency === this.salesbillissue.agency
+    //   const f = this.form.agency_tel === this.salesbillissue.agency_tel
+    //   const g = this.form.bank_account1 === this.salesbillissue.bank_account1
+    //   const h = this.form.bank_number1 === this.salesbillissue.bank_number1
+    //   const i = this.form.bank_host1 === this.salesbillissue.bank_host1
+    //   const j = this.form.bank_account2 === this.salesbillissue.bank_account2
+    //   const k = this.form.bank_number2 === this.salesbillissue.bank_number2
+    //   const l = this.form.bank_host2 === this.salesbillissue.bank_host2
+    //   const m = this.form.zipcode === this.salesbillissue.zipcode
+    //   const n = this.form.address1 === this.salesbillissue.address1
+    //   const o = this.form.address2 === this.salesbillissue.address2
+    //   const p = this.form.address3 === this.salesbillissue.address3
+    //   const q = this.form.title === this.salesbillissue.title
+    //   const r = this.form.content === this.salesbillissue.content
+    //   const sky = a && b && c && d && e && f && g && h && i
+    //   const land = j && k && l && m && n && o && p && q && r
+    //   return sky && land
+    // },
+    writeAuth() {
+      return this.superAuth || (this.staffAuth && this.staffAuth.notice === '2')
+    },
     ...mapState('payment', ['payOrderList']),
+    ...mapGetters('accounts', ['staffAuth', 'superAuth']),
   },
   watch: {
     salesbillissue(val) {
       if (val) {
         this.set_now_order(val.now_payment_order)
 
+        this.pk = val.pk
         this.form.now_payment_order = val.now_payment_order
         this.form.host_name = val.host_name
         this.form.host_tel = val.host_tel
@@ -355,14 +415,44 @@ export default defineComponent({
         this.form.content = val.content
       }
     },
+    now_order(val) {
+      if (val) {
+        this.form.now_due_date = val.pay_due_date
+      }
+    },
   },
   methods: {
     set_now_order(order: number) {
       const now_order = this.payOrderList
         .filter((o: any) => o.pk == order)
         .map((o: any) => o)[0]
-      this.form.now_due_date = now_order.pay_due_date
+      this.now_order = now_order
       this.$emit('display-order', now_order.__str__)
+    },
+    onSubmit(this: any, event: any) {
+      if (this.writeAuth) {
+        const form = event.currentTarget
+        if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+
+          this.validated = true
+        } else {
+          this.$refs.confirmModal.callModal()
+        }
+      } else {
+        this.$refs.alertModal.callModal()
+      }
+    },
+    modalAction(this: any) {
+      const { pk } = this
+      this.form.now_due_date = this.dateFormat(this.form.now_due_date)
+      if (this.update) {
+        this.$emit('to-update', { ...{ pk }, ...this.form })
+      } else {
+        this.$emit('to-create', this.form)
+      }
+      this.validated = false
     },
   },
 })
