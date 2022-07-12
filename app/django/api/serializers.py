e7b6ae15fea0bba91a794ac84ac83de4812501df
Austin-Kho <kori.susie@gmail.com>
2022-07-12 15:03:46 +0900

    serialize update

--- app/django/api/serializers.py
+++ app/django/api/serializers.py
@@ -628,7 +628,7 @@ class SallesBillIssueSerializer(serializers.ModelSerializer):
         fields = ('pk', 'project', 'now_payment_order', 'host_name', 'host_tel',
                   'agency', 'agency_tel', 'bank_account1', 'bank_number1', 'bank_host1',
                   'bank_account2', 'bank_number2', 'bank_host2', 'zipcode', 'address1',
-                  'address2', 'address3', 'title', 'content', 'user', 'updated_at')
+                  'address2', 'address3', 'title', 'content')
 
 
 # Document --------------------------------------------------------------------------
