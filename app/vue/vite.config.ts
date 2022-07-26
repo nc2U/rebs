1fb53af68a6b594f30d839204a8f1b5a4654a448
Austin-Kho <kori.susie@gmail.com>
2022-07-26 14:00:48 +0900

    outdir & indexpath update

--- app/vue/vite.config.ts
+++ app/vue/vite.config.ts
@@ -1,16 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import copy from 'rollup-plugin-copy-merge'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        dir: '../django/static/dist',
+      },
+    },
+  },
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
+    }),
+    copy({
+      targets: [
+        {
+          src: '../django/static/dist/index.html',
+          file: '../django/templates/base-vue.html',
+        },
+      ],
     }),
   ],
   define: { 'process.env': {} },
