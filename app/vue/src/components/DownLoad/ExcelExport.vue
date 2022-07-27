855ff23b46bc9e6c4ebd094cb266dda0ce625878
Austin-Kho <kori.susie@gmail.com>
2022-07-27 17:19:59 +0900

    Excel & Pdf Export Component update

--- app/vue/src/components/DownLoad/ExcelExport.vue
+++ app/vue/src/components/DownLoad/ExcelExport.vue
@@ -2,11 +2,10 @@
 import { defineProps } from 'vue'
 
 const props = defineProps({ url: String })
-const downLoad = () => (location.href = `${props.url}`)
 </script>
 
 <template>
-  <v-btn flat size="small" @click="downLoad">
+  <v-btn size="small" :href="props.url" flat width="180">
     <v-icon icon="mdi-microsoft-excel" color="green" class="mr-2" />
     Excel Export
     <v-icon icon="mdi-download" color="grey" class="ml-2" />
--- app/vue/src/components/DownLoad/PdfExport.vue
+++ app/vue/src/components/DownLoad/PdfExport.vue
@@ -2,11 +2,10 @@
 import { defineProps } from 'vue'
 
 const props = defineProps({ url: String })
-const downLoad = () => (location.href = `${props.url}`)
 </script>
 
 <template>
-  <v-btn variant="flat" size="small" @click="downLoad">
+  <v-btn size="small" :href="props.url" flat width="180">
     <v-icon icon="mdi-file-pdf-box" color="red" class="mr-2" />
     Pdf Export
     <v-icon icon="mdi-download" color="grey" class="ml-2" />
