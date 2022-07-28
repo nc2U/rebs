e3e78e59389d2721868a288289a72492a0b078f3
Austin-Kho <kori.susie@gmail.com>
2022-07-29 00:56:08 +0900

    project select update tring

--- app/vue/src/layouts/ContentHeader/ProjectSelect/Index.vue
+++ app/vue/src/layouts/ContentHeader/ProjectSelect/Index.vue
@@ -1,46 +1,54 @@
+<script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+const selected = ref()
+const store = useStore()
+const props = defineProps({ project: { type: Object, default: null } })
+const emit = defineEmits(['projSelect'])
+
+const projSelect = computed(() => store.getters['project/projSelect'])
+const initProjId = computed(() => store.getters['accounts/initProjId'])
+const projId = computed(() =>
+  props.project ? props.project.pk : initProjId.value,
+)
+
+const fetchProjectList = () => store.dispatch('project/fetchProjectList')
+
+onMounted(() => {
+  fetchProjectList()
+  selected.value = projId.value
+})
+</script>
+
 <template>
-  <CRow>
+  <CRow class="m-0">
     <CFormLabel class="col-lg-1 col-form-label">프로젝트</CFormLabel>
     <CCol md="6" lg="3">
-      <CFormSelect @change="$emit('proj-select', $event)">
+      <CFormSelect v-model="selected" @change="$emit('proj-select', $event)">
         <option value="">프로젝트선택</option>
         <option
           v-for="proj in projSelect"
-          :value="proj.value"
           :key="proj.value"
-          :selected="proj.value === projId"
+          :value="proj.value"
         >
           {{ proj.text }}
         </option>
       </CFormSelect>
     </CCol>
+    <!--    <CCol md="6" lg="4" xl="3">-->
+    <!--      <v-select-->
+    <!--        v-model="selected"-->
+    <!--        :items="projSelect"-->
+    <!--        :hint="`${projSelect.value}, ${projSelect.text}`"-->
+    <!--        item-title="text"-->
+    <!--        item-value="value"-->
+    <!--        label="프로젝트"-->
+    <!--        density="comfortable"-->
+    <!--        persistent-hint-->
+    <!--        return-object-->
+    <!--      />-->
+    <!--      &lt;!&ndash;        @update:modelValue="emit('projSelect', selected)"&ndash;&gt;-->
+    <!--    </CCol>-->
   </CRow>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { mapActions, mapGetters } from 'vuex'
-
-export default defineComponent({
-  name: 'ProjectSelect',
-  created() {
-    this.fetchProjectList()
-  },
-  props: {
-    project: {
-      type: Object,
-    },
-  },
-  computed: {
-    projId() {
-      return this.project ? this.project.pk : this.initProjId
-    },
-    ...mapGetters('project', ['projSelect']),
-    ...mapGetters('accounts', ['initProjId']),
-  },
-
-  methods: {
-    ...mapActions('project', ['fetchProjectList']),
-  },
-})
-</script>
