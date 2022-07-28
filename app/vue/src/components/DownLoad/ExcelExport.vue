df882dacbcfb713c1d9182bb32bf005767f8025e
Austin-Kho <kori.susie@gmail.com>
2022-07-28 16:46:17 +0900

    DownloadExport component update

--- app/vue/src/components/DownLoad/ExcelExport.vue
+++ app/vue/src/components/DownLoad/ExcelExport.vue
@@ -7,13 +7,17 @@ const props = defineProps({
 
 <template>
   <v-row class="justify-end my-1">
+    <v-col class="my-0 py-1">
+      <slot />
+    </v-col>
+    <slot name="near" />
     <v-btn
       size="small"
       :href="props.url"
       flat
       width="160"
       :disabled="props.disabled"
-      class="mr-3"
+      class="mx-3"
     >
       <v-icon icon="mdi-microsoft-excel" color="green" class="mr-2" />
       Excel Export
--- app/vue/src/components/DownLoad/PdfExport.vue
+++ app/vue/src/components/DownLoad/PdfExport.vue
@@ -7,13 +7,17 @@ const props = defineProps({
 
 <template>
   <v-row class="justify-end my-1">
+    <v-col class="my-0 py-1">
+      <slot />
+    </v-col>
+    <slot name="near" />
     <v-btn
       size="small"
       :href="props.url"
       flat
       width="160"
       :disabled="props.disabled"
-      class="mr-3"
+      class="mx-3"
     >
       <v-icon icon="mdi-file-pdf-box" color="red" class="mr-2" />
       Pdf Export
