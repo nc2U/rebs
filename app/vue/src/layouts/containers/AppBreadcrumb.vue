a812949433517e79a6a7076752995c8e215c4b92
Austin-Kho <kori.susie@gmail.com>
2022-07-30 12:19:34 +0900

    AppBreadcrumb.vue to composition api update

--- app/vue/src/layouts/containers/AppBreadcrumb.vue
+++ app/vue/src/layouts/containers/AppBreadcrumb.vue
@@ -1,50 +1,42 @@
-<template>
-  <CBreadcrumb class="d-md-down-none me-auto mb-0">
-    <CBreadcrumbItem
-      v-for="item in breadcrumbs"
-      :href="item.active ? '' : item.path"
-      :active="item.active"
-      :key="item"
-    >
-      {{ item.name }}
-    </CBreadcrumbItem>
-  </CBreadcrumb>
-</template>
-
-<script lang="ts">
+<script lang="ts" setup>
 import { onMounted, ref } from 'vue'
 import { RouteLocationMatched } from 'vue-router'
 import router from '@/router'
 
-export default {
-  name: 'AppBreadcrumb',
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  setup() {
-    const breadcrumbs = ref()
+const breadcrumbs = ref()
 
-    const getBreadcrumbs = () => {
-      return router.currentRoute.value.matched.map(
-        (route: RouteLocationMatched) => {
-          return {
-            active: route.path === router.currentRoute.value.fullPath,
-            name: route.name,
-            path: `${router.options.history.base}${route.path}`,
-          }
-        },
-      )
-    }
-
-    router.afterEach(() => {
-      breadcrumbs.value = getBreadcrumbs()
-    })
-
-    onMounted(() => {
-      breadcrumbs.value = getBreadcrumbs()
-    })
-
-    return {
-      breadcrumbs,
-    }
-  },
+const getBreadcrumbs = () => {
+  return router.currentRoute.value.matched.map(
+    (route: RouteLocationMatched) => {
+      return {
+        active: route.path === router.currentRoute.value.fullPath,
+        name: route.name,
+        path: `${router.options.history.base}${route.path}`,
+      }
+    },
+  )
 }
+
+router.afterEach(() => {
+  breadcrumbs.value = getBreadcrumbs()
+})
+
+onMounted(() => {
+  breadcrumbs.value = getBreadcrumbs()
+})
 </script>
+
+<template>
+  <CBreadcrumb class="d-md-down-none me-auto mb-0">
+    <TransitionGroup name="breadcrumb">
+      <CBreadcrumbItem
+        v-for="item in breadcrumbs"
+        :key="item"
+        :href="item.active ? '' : item.path"
+        :active="item.active"
+      >
+        {{ item.name }}
+      </CBreadcrumbItem>
+    </TransitionGroup>
+  </CBreadcrumb>
+</template>
