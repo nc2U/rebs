ed550382c585ef5ba0699206dacf3f22f81a9656
Austin-Kho <kori.susie@gmail.com>
2022-08-02 00:43:49 +0900

    nprogress update

--- app/vue/src/router/index.ts
+++ app/vue/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { start, close } from '@/utils/nprogress'
 import routes from '@/router/routes'
 import store from '@/store'
 
@@ -8,12 +9,14 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  start()
   store.commit('startSpinner')
   next()
 })
 
 router.afterEach(() => {
   store.commit('endSpinner')
+  close()
 })
 
 export default router
