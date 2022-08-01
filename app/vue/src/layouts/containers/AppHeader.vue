3513c80303b952cd1a89c28c8987d3f4aba80de6
Austin-Kho <kori.susie@gmail.com>
2022-08-01 19:06:27 +0900

    tagsView logic update ing

--- app/vue/src/layouts/containers/AppHeader.vue
+++ app/vue/src/layouts/containers/AppHeader.vue
@@ -5,7 +5,7 @@ import AppBreadcrumb from './AppBreadcrumb.vue'
 import AppHeaderDropdownAccnt from './AppHeaderDropdownAccnt.vue'
 import { directive as vFullscreen } from 'vue-fullscreen'
 import { logo } from '@/assets/brand/current-logo'
-// import TagsView from '@/layouts/containers/TagsView/index.vue'
+import TagsView from '@/layouts/containers/TagsView/index.vue'
 
 const store = useStore()
 const screenIcon = ref('mdi-fullscreen')
