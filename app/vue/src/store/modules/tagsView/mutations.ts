7187ed759c3896ef40d15fa634adcb222b2d0081
Austin-Kho <kori.susie@gmail.com>
2022-08-01 20:05:48 +0900

    tagsView logic update ing

--- app/vue/src/store/modules/tagsView/mutations.ts
+++ app/vue/src/store/modules/tagsView/mutations.ts
@@ -3,11 +3,11 @@ import { TagsViewState } from '@/store/modules/tagsView/state'
 const mutations = {
   ADD_VISITED_VIEW: (state: TagsViewState, view: any) => {
     if (state.visitedViews.some(v => v.name === view.name)) return
-    state.visitedViews.push(
-      Object.assign({}, view, {
-        title: view.meta.title || 'no-name',
-      }),
-    )
+    // state.visitedViews.push(
+    //   Object.assign({}, view, {
+    //     title: view.meta.title || 'no-name',
+    //   }),
+    // )
   },
   ADD_CACHED_VIEW: (state: TagsViewState, view: any) => {
     if (state.cachedViews.includes(view.name)) return
