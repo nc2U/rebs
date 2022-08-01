24df09c476814482154ea65d647a7d7a5f11abf6
Austin-Kho <kori.susie@gmail.com>
2022-08-01 19:29:15 +0900

    tagsView logic update ing

--- app/vue/src/layouts/containers/TagsView/index.vue
+++ app/vue/src/layouts/containers/TagsView/index.vue
@@ -27,29 +27,29 @@ const isActive = (currentRoute: any) => currentRoute.name === route.name
 
 const isAffix = (tag: any) => tag.meta && tag.meta.affix
 
-const filterAffixTags = (routes: any[]) => {
-  let tags: Array<VisitedViews> = []
-  routes.forEach((r: any) => {
-    if (r.meta && r.meta.affix) {
-      tags.push({
-        path: r.path,
-        name: r.name,
-        meta: { ...r.meta },
-      })
-    }
-
-    if (r.children) {
-      const tempTags = filterAffixTags(r.children)
-      if (tempTags.length >= 1) {
-        tags = [...tags, ...tempTags]
-      }
-    }
-  })
-  return tags
-}
+// const filterAffixTags = (routes: any[]) => {
+//   let tags: Array<VisitedViews> = []
+//   routes.forEach((r: any) => {
+//     if (r.meta && r.meta.affix) {
+//       tags.push({
+//         path: r.path,
+//         name: r.name,
+//         meta: { ...r.meta },
+//       })
+//     }
+//
+//     if (r.children) {
+//       const tempTags = filterAffixTags(r.children)
+//       if (tempTags.length >= 1) {
+//         tags = [...tags, ...tempTags]
+//       }
+//     }
+//   })
+//   return tags
+// }
 
 const initTags = () => {
-  affixTags = filterAffixTags(routes)
+  // affixTags = filterAffixTags(routes)
   affixTags.forEach(tag =>
     tag.name ? store.dispatch('tagsView/addVisitedView', tag) : undefined,
   )
@@ -63,95 +63,95 @@ const addTags = () => {
   return false
 }
 
-// const moveToCurrentTag = () => {
-//   const tags = tag.value
-//   nextTick(() => {
-//     for (const tag of tags) {
-//       // Todo this logic update
-//       console.log(tag)
-//       // if (tag.to.path === route.path) {
-//       //   scrollPane.value.moveToTarget(tag)
-//       //   // when query is different then update
-//       //   if (tag.to.fullPath !== route.fullPath) {
-//       //     store.dispatch('tagsView/updateVisitedView', route)
-//       //   }
-//       //   break
-//       // }
-//     }
-//   })
-// }
-//
-// const refreshSelectedTag = (view: any) => {
-//   store.dispatch('tagsView/delCachedView', view).then(() => {
-//     const { fullPath } = view
-//     nextTick(() => {
-//       router.replace({
-//         path: '/redirect' + fullPath,
-//       })
-//     })
-//   })
-// }
-//
-// const toLastView = (visitedViews: any, view: any) => {
-//   const latestView = visitedViews.slice(-1)[0]
-//   if (latestView) {
-//     router.push(latestView.fullPath)
-//   } else {
-//     // now the default is to redirect to the home page if there is no tags-view,
-//     // you can adjust it according to your needs.
-//     if (view.name === 'Dashboard') {
-//       // to reload home page
-//       router.replace({ path: '/redirect' + view.fullPath })
-//     } else {
-//       router.push('/')
-//     }
-//   }
-// }
-//
-// const closeSelectedTag = (view: any) => {
-//   store.dispatch('tagsView/delView', view).then(({ visitedViews }) => {
-//     if (isActive(view)) {
-//       toLastView(visitedViews, view)
-//     }
-//   })
-// }
-//
-// const closeOthersTags = () => {
-//   router.push(selectedTag)
-//   store.dispatch('tagsView/delOthersViews', selectedTag).then(() => {
-//     moveToCurrentTag()
-//   })
-// }
-//
-// const closeAllTags = (view: any) => {
-//   store.dispatch('tagsView/delAllViews').then(({ visitedViews }) => {
-//     if (affixTags.some(tag => tag.path === view.path)) {
-//       return
-//     }
-//     toLastView(visitedViews, view)
-//   })
-// }
-//
-// const closeMenu = () => {
-//   visible.value = false
-// }
-//
-// const handleScroll = () => {
-//   closeMenu()
-// }
-//
-// watch(route, () => {
-//   addTags()
-//   moveToCurrentTag()
-// })
-//
-// watch(visible, value => {
-//   if (value) {
-//     document.body.addEventListener('click', closeMenu)
-//   } else {
-//     document.body.removeEventListener('click', closeMenu)
-//   }
-// })
+const moveToCurrentTag = () => {
+  const tags = tag.value
+  nextTick(() => {
+    for (const tag of tags) {
+      // Todo this logic update
+      console.log(tag)
+      // if (tag.to.path === route.path) {
+      //   scrollPane.value.moveToTarget(tag)
+      //   // when query is different then update
+      //   if (tag.to.fullPath !== route.fullPath) {
+      //     store.dispatch('tagsView/updateVisitedView', route)
+      //   }
+      //   break
+      // }
+    }
+  })
+}
+
+const refreshSelectedTag = (view: any) => {
+  store.dispatch('tagsView/delCachedView', view).then(() => {
+    const { fullPath } = view
+    nextTick(() => {
+      router.replace({
+        path: '/redirect' + fullPath,
+      })
+    })
+  })
+}
+
+const toLastView = (visitedViews: any, view: any) => {
+  const latestView = visitedViews.slice(-1)[0]
+  if (latestView) {
+    router.push(latestView.fullPath)
+  } else {
+    // now the default is to redirect to the home page if there is no tags-view,
+    // you can adjust it according to your needs.
+    if (view.name === 'Dashboard') {
+      // to reload home page
+      router.replace({ path: '/redirect' + view.fullPath })
+    } else {
+      router.push('/')
+    }
+  }
+}
+
+const closeSelectedTag = (view: any) => {
+  store.dispatch('tagsView/delView', view).then(({ visitedViews }) => {
+    if (isActive(view)) {
+      toLastView(visitedViews, view)
+    }
+  })
+}
+
+const closeOthersTags = () => {
+  router.push(selectedTag)
+  store.dispatch('tagsView/delOthersViews', selectedTag).then(() => {
+    moveToCurrentTag()
+  })
+}
+
+const closeAllTags = (view: any) => {
+  store.dispatch('tagsView/delAllViews').then(({ visitedViews }) => {
+    if (affixTags.some(tag => tag.path === view.path)) {
+      return
+    }
+    toLastView(visitedViews, view)
+  })
+}
+
+const closeMenu = () => {
+  visible.value = false
+}
+
+const handleScroll = () => {
+  closeMenu()
+}
+
+watch(route, () => {
+  addTags()
+  moveToCurrentTag()
+})
+
+watch(visible, value => {
+  if (value) {
+    document.body.addEventListener('click', closeMenu)
+  } else {
+    document.body.removeEventListener('click', closeMenu)
+  }
+})
 
 onMounted(() => {
   initTags()
