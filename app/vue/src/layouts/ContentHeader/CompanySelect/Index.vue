628ab91666a8757f5fa2e6f07aa9984b713a78a9
Austin-Kho <kori.susie@gmail.com>
2022-08-02 23:56:30 +0900

    company select update

--- app/vue/src/layouts/ContentHeader/CompanySelect/Index.vue
+++ app/vue/src/layouts/ContentHeader/CompanySelect/Index.vue
@@ -1,46 +1,40 @@
+<script lang="ts" setup>
+import { ref, computed, onMounted, nextTick } from 'vue'
+import { useStore } from 'vuex'
+
+const com = ref()
+
+const props = defineProps({ company: { type: Object, default: null } })
+
+const store = useStore()
+const comSelectList = computed(() => store.getters['settings/comSelect'])
+const initComId = computed(() => store.getters['accounts/initComId'])
+
+const fetchCompanyList = () => store.dispatch('settings/fetchCompanyList')
+const emit = defineEmits(['com-select'])
+const selectCom = () => {
+  nextTick(() => {
+    alert(com.value)
+    emit('com-select', com.value)
+  })
+}
+
+onMounted(() => {
+  com.value = initComId.value
+  fetchCompanyList()
+})
+</script>
+
 <template>
   <CRow>
     <CFormLabel class="col-lg-1 col-form-label text-body">회사명</CFormLabel>
     <CCol md="6" lg="3">
-      <CFormSelect @change="$emit('com-select', $event)">
+      <CFormSelect v-model="com" @change="selectCom">
         <option value="">회사선택</option>
-        <option
-          v-for="com in comSelect"
-          :key="com.value"
-          :value="com.value"
-          :selected="com.value === comId"
-        >
-          {{ com.text }}
+        <option v-for="c in comSelectList" :key="c.value" :value="c.value">
+          {{ c.text }}
         </option>
       </CFormSelect>
     </CCol>
   </CRow>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { mapActions, mapGetters } from 'vuex'
-
-export default defineComponent({
-  name: 'CompanySelect',
-  props: {
-    company: {
-      type: Object,
-    },
-  },
-  created() {
-    this.fetchCompanyList()
-  },
-  computed: {
-    comId() {
-      return this.company ? this.company.pk : this.initComId
-    },
-    ...mapGetters('settings', ['comSelect']),
-    ...mapGetters('accounts', ['initComId']),
-  },
-
-  methods: {
-    ...mapActions('settings', ['fetchCompanyList']),
-  },
-})
-</script>
