7dec9902606a9c1e744b44a7d9a53c337decf78b
Austin-Kho <kori.susie@gmail.com>
2022-08-03 08:57:29 +0900

    proCash status update

--- app/vue/src/views/proCash/Status/Index.vue
+++ app/vue/src/views/proCash/Status/Index.vue
@@ -1,3 +1,119 @@
+<script lang="ts">
+import { defineComponent, ref, computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { pageTitle, navMenu } from '@/views/proCash/_menu/headermixin'
+import { dateFormat } from '@/utils/baseMixins'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+import DateChoicer from '@/views/proCash/Status/components/DateChoicer.vue'
+import TabSelect from '@/views/proCash/Status/components/TabSelect.vue'
+import TableTitleRow from '@/components/TableTitleRow.vue'
+import StatusByAccount from '@/views/proCash/Status/components/StatusByAccount.vue'
+import CashListByDate from '@/views/proCash/Status/components/CashListByDate.vue'
+import SummaryForBudget from '@/views/proCash/Status/components/SummaryForBudget.vue'
+
+export default defineComponent({
+  name: 'ProjectCashStatus',
+  components: {
+    ContentHeader,
+    ContentBody,
+    DateChoicer,
+    TabSelect,
+    TableTitleRow,
+    StatusByAccount,
+    CashListByDate,
+    SummaryForBudget,
+  },
+  setup() {
+    const store = useStore()
+
+    const date = ref(new Date())
+    const compName = ref('StatusByAccount')
+
+    const project = computed(() => store.state.project.project)
+    const initProjId = computed(() => store.getters['accounts/initProjId'])
+
+    const fetchProAllAccD1List = () =>
+      store.dispatch('proCash/fetchProAllAccD1List')
+    const fetchProAllAccD2List = () =>
+      store.dispatch('proCash/fetchProAllAccD2List')
+    const fetchProBankAccList = (proj: number) =>
+      store.dispatch('proCash/fetchProBankAccList', proj)
+    const fetchBalanceByAccList = (proj: { project: number }) =>
+      store.dispatch('proCash/fetchBalanceByAccList', proj)
+
+    const fetchDateCashBookList = (payload: any) =>
+      store.dispatch('proCash/fetchDateCashBookList', payload)
+    const fetchProjectBudgetList = (proj: number) =>
+      store.dispatch('proCash/fetchProjectBudgetList', proj)
+    const fetchExecAmountList = (proj: { project: number }) =>
+      store.dispatch('proCash/fetchExecAmountList', proj)
+
+    const updateState = (payload: any) =>
+      store.commit('proCash/updateState', payload)
+
+    const onSelectAdd = (target: any) => {
+      if (target !== '') {
+        fetchProBankAccList(target)
+        fetchBalanceByAccList({ project: target })
+        fetchDateCashBookList({
+          project: target,
+          date: dateFormat(date.value),
+        })
+        fetchProjectBudgetList(target)
+        fetchExecAmountList({ project: target })
+      } else {
+        updateState({
+          proBankAccountList: [],
+          balanceByAccList: [],
+          proDateCashBook: [],
+          proBudgetList: [],
+          execAmountList: [],
+        })
+      }
+    }
+
+    const showTab = (num: number) => {
+      if (num === 1) compName.value = 'StatusByAccount'
+      else if (num === 2) compName.value = 'CashListByDate'
+      else if (num === 3) compName.value = 'SummaryForBudget'
+    }
+
+    const setDate = (d: Date) => {
+      const dt = new Date(d)
+      date.value = dt
+      fetchBalanceByAccList({ project: project.value })
+      fetchDateCashBookList({ project: project.value, date: dateFormat(dt) })
+      fetchProjectBudgetList(project.value)
+      fetchExecAmountList({ project: project.value })
+    }
+
+    onMounted(() => {
+      fetchProAllAccD1List()
+      fetchProAllAccD2List()
+      fetchProBankAccList(initProjId.value)
+      fetchBalanceByAccList({ project: initProjId.value })
+      fetchDateCashBookList({
+        project: initProjId.value,
+        date: dateFormat(date.value),
+      })
+      fetchProjectBudgetList(initProjId.value)
+      fetchExecAmountList({ project: initProjId.value })
+    })
+
+    return {
+      date,
+      compName,
+      pageTitle,
+      navMenu,
+      onSelectAdd,
+      showTab,
+      setDate,
+    }
+  },
+})
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
@@ -19,103 +135,3 @@
     <CCardFooter>&nbsp;</CCardFooter>
   </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/proCash/_menu/headermixin'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import DateChoicer from '@/views/proCash/Status/components/DateChoicer.vue'
-import TabSelect from '@/views/proCash/Status/components/TabSelect.vue'
-import TableTitleRow from '@/components/TableTitleRow.vue'
-import StatusByAccount from '@/views/proCash/Status/components/StatusByAccount.vue'
-import CashListByDate from '@/views/proCash/Status/components/CashListByDate.vue'
-import SummaryForBudget from '@/views/proCash/Status/components/SummaryForBudget.vue'
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'ProjectCashStatus',
-  components: {
-    ContentHeader,
-    ContentBody,
-    DateChoicer,
-    TabSelect,
-    TableTitleRow,
-    StatusByAccount,
-    CashListByDate,
-    SummaryForBudget,
-  },
-  mixins: [HeaderMixin],
-  data() {
-    return {
-      compName: 'StatusByAccount',
-      date: new Date(),
-    }
-  },
-  created(this: any) {
-    this.fetchProAllAccD1List()
-    this.fetchProAllAccD2List()
-    this.fetchProBankAccList(this.initProjId)
-    this.fetchBalanceByAccList({ project: this.initProjId })
-    this.fetchDateCashBookList({
-      project: this.initProjId,
-      date: this.dateFormat(this.date),
-    })
-    this.fetchProjectBudgetList(this.initProjId)
-    this.fetchExecAmountList({ project: this.initProjId })
-  },
-  computed: {
-    ...mapState('project', ['project']),
-    ...mapGetters('accounts', ['initProjId']),
-  },
-  methods: {
-    onSelectAdd(this: any, target: any) {
-      if (target !== '') {
-        this.fetchProBankAccList(target)
-        this.fetchBalanceByAccList({ project: target })
-        this.fetchDateCashBookList({
-          project: target,
-          date: this.dateFormat(this.date),
-        })
-        this.fetchProjectBudgetList(target)
-        this.fetchExecAmountList({ project: target })
-      } else {
-        this.FETCH_P_BANK_ACCOUNT_LIST([])
-        this.FETCH_BALANCE_BY_ACC_LIST([])
-        this.FETCH_P_DATE_CASHBOOK([])
-        this.FETCH_P_BUDGET_LIST([])
-        this.FETCH_EXEC_AMOUNT_LIST([])
-      }
-    },
-    showTab(num: number) {
-      if (num === 1) this.compName = 'StatusByAccount'
-      else if (num === 2) this.compName = 'CashListByDate'
-      else if (num === 3) this.compName = 'SummaryForBudget'
-    },
-    setDate(this: any, date: string) {
-      this.date = date
-      const project = this.project.pk
-      this.fetchBalanceByAccList({ project, date: this.dateFormat(date) })
-      this.fetchDateCashBookList({ project, date: this.dateFormat(date) })
-      this.fetchProjectBudgetList(project)
-      this.fetchExecAmountList({ project, date: this.dateFormat(date) })
-    },
-    ...mapActions('proCash', [
-      'fetchProAllAccD1List',
-      'fetchProAllAccD2List',
-      'fetchProBankAccList',
-      'fetchBalanceByAccList',
-      'fetchDateCashBookList',
-      'fetchProjectBudgetList',
-      'fetchExecAmountList',
-    ]),
-    ...mapMutations('proCash', [
-      'FETCH_P_BANK_ACCOUNT_LIST',
-      'FETCH_BALANCE_BY_ACC_LIST',
-      'FETCH_P_DATE_CASHBOOK',
-      'FETCH_P_BUDGET_LIST',
-      'FETCH_EXEC_AMOUNT_LIST',
-    ]),
-  },
-})
-</script>
