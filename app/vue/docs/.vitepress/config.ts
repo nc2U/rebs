1d61984f239951f27b0aa8994b67580b820743c7
Austin-Kho <kori.susie@gmail.com>
2022-08-09 21:52:05 +0900

    docs update

--- app/vue/docs/.vitepress/config.ts
+++ app/vue/docs/.vitepress/config.ts
@@ -17,14 +17,14 @@ export default defineConfig({
     siteTitle: 'REBS',
     nav: [
       {text: '가이드', link: '/getting-started'},
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     {text: 'Item A', link: '/item-1'},
-      //     {text: 'Item B', link: '/item-2'},
-      //     {text: 'Item C', link: '/item-3'}
-      //   ]
-      // }
+      {
+        text: 'Dropdown',
+        items: [
+          {text: 'Item A', link: '/item-1'},
+          {text: 'Item B', link: '/item-2'},
+          {text: 'Item C', link: '/item-3'}
+        ]
+      }
     ],
     sidebar: [
       {
@@ -32,10 +32,43 @@ export default defineConfig({
         items: [
           {text: 'REBS란?', link: '/'},
           {text: '시작하기', link: '/getting-started'},
-          {text: '회사설정', link: '/company'},
+        ]
+      },
+      {
+        text: '기본 설정',
+        items: [
+          {text: '회사정보 설정', link: '/company'},
           {text: '프로젝트 설정', link: '/project'},
-          {text: '계약등록', link: '/contract'},
-          {text: '입출금 관리', link: '/cash-inout'},
+          {text: '세부정보 설정', link: '/settings'},
+          {text: '부지정보 입력', link: '/site-manage'},
+        ]
+      },
+      {
+        text: '계약 관리',
+        items: [
+          {text: '계약 등록 관리', link: '/contract'},
+          {text: '계약 해지 관리', link: '/release'},
+        ]
+      },
+      {
+        text: '분양대금 수납 관리',
+        items: [
+          {text: '수납 등록 관리', link: '/payment'},
+          {text: '고지서 발급 관리', link: '/bill-notice'},
+        ]
+      },
+      {
+        text: '일반 입출금 관리',
+        items: [
+          {text: '설정 관리', link: '/cash-settings'},
+          {text: '입출금 관리', link: '/cash-manage'},
+        ]
+      },
+      {
+        text: '권한 관리',
+        items: [
+          {text: '사용자 권한 관리', link: '/auth-manage'},
+          {text: 'admin 페이지', link: '/admin-page'},
         ]
       }
     ],
