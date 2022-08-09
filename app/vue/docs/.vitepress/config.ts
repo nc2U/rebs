9a25b85204be105998c4e1110d33546df49aa53b
Austin-Kho <kori.susie@gmail.com>
2022-08-09 17:54:38 +0900

    docs update

--- app/vue/docs/.vitepress/config.ts
+++ app/vue/docs/.vitepress/config.ts
@@ -1,3 +1,16 @@
 import {defineConfig} from "vitepress";
 
-export default defineConfig({})
+export default defineConfig({
+  lang: 'ko-KR',
+  title: 'REBS',
+  description: '부동산 개발관리 프로그램',
+  base: '/docs/',
+  lastUpdated: true,
+  markdown: {
+    theme: 'material-palenight',
+    lineNumbers: true
+  },
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  }
+})
