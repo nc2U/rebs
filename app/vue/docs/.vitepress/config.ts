818e7bddbf55ea7eaa0b15c9e6e3218d5f56c2bc
Austin-Kho <kori.susie@gmail.com>
2022-08-10 15:23:43 +0900

    docs update

--- app/vue/docs/.vitepress/config.ts
+++ app/vue/docs/.vitepress/config.ts
@@ -49,31 +49,30 @@ export default defineConfig({
       {
         text: '계약 수납 관리',
         items: [
-          {text: '계약 등록 관리', link: '/contract/'},
-          {text: '계약 해지 관리', link: '/contract/release'},
-          {text: '수납 등록 관리', link: '/contract/payment'},
-          {text: '고지서 발급 관리', link: '/contract/bill-notice'},
+          {text: '계약 관리', link: '/contract/'},
+          {text: '수납 관리', link: '/contract/payment'},
+          {text: '고지서 관리', link: '/contract/bill-notice'},
         ]
       },
       {
         text: '일반 입출금 관리',
         items: [
-          {text: '기본 설정 관리', link: '/cashes/settings'},
-          {text: '입출금 정보 관리', link: '/cashes/manage'},
+          {text: '기본 설정', link: '/cashes/settings'},
+          {text: '입출금 관리', link: '/cashes/manage'},
         ]
       },
       {
         text: '문서 관리',
         items: [
-          {text: '일반 문서 관리', link: '/document/'},
-          {text: '소송 기록 관리', link: '/document/legal-case'},
+          {text: '일반 문서', link: '/document/'},
+          {text: '소송 기록', link: '/document/legal-case'},
         ]
       },
       {
         text: '권한 관리',
         items: [
-          {text: '사용자 권한 관리', link: '/authority/'},
-          {text: '관리자(admin) 페이지', link: '/authority/admin-page'},
+          {text: '사용자 권한', link: '/authority/'},
+          {text: '관리자 페이지', link: '/authority/admin-page'},
         ]
       }
     ],
