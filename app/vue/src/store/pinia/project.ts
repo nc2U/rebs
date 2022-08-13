752ae9ffcb179f9ea0c476da289d0a6ed853746b
Austin-Kho <kori.susie@gmail.com>
2022-08-14 07:24:38 +0900

    project pinia update

--- app/vue/src/store/pinia/project.ts
+++ app/vue/src/store/pinia/project.ts
@@ -1,13 +1,89 @@
-import { ref } from 'vue'
+import api from '@/api'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
+import { useAccount } from '@/store/pinia/account'
+import { Project } from '@/store/modules/project/state'
+import { errorHandle, message } from '@/utils/helper'
 
 export const useProject = defineStore('project', () => {
   // states
   const projectList = ref([])
   const project = ref(null)
 
+  // getters
+  const allowed_projects = computed(() => {
+    const account = useAccount()
+    return account.userInfo && account.userInfo.staffauth
+      ? account.userInfo.staffauth.allowed_projects
+      : []
+  })
+
+  const projSelect = computed(() =>
+    projectList.value
+      ? projectList.value
+          .filter((p: Project) => allowed_projects.value.includes(p.pk))
+          .map((p: Project) => ({ value: p.pk, text: p.name }))
+      : [],
+  )
+
+  // actions
+  const fetchProjectList = () => {
+    api
+      .get('/project/')
+      .then(res => (projectList.value = res.data.results))
+      .catch(err => errorHandle(err.response.data))
+  }
+
+  const fetchProject = (pk: string) => {
+    api
+      .get(`/project/${pk}/`)
+      .then(res => (project.value = res.data))
+      .catch(err => errorHandle(err.response.data))
+  }
+
+  const createProject = (payload: Project) => {
+    api
+      .post('/project/', payload)
+      .then(res => {
+        fetchProject(res.data.pk)
+        fetchProjectList()
+        message()
+      })
+      .catch(err => errorHandle(err.response.data))
+  }
+
+  const updateProject = (payload: { pk: string } & Project) => {
+    const { pk, ...projectData } = payload
+    api
+      .put(`/project/${pk}/`, projectData)
+      .then(res => {
+        fetchProject(res.data.pk)
+        fetchProjectList()
+        message()
+      })
+      .catch(err => errorHandle(err.response.data))
+  }
+
+  const deleteProject = (pk: string) => {
+    api
+      .delete(`/project/${pk}/`)
+      .then(() => {
+        fetchProjectList()
+        message('warning', '', '삭제되었습니다.')
+      })
+      .catch(err => errorHandle(err.response.data))
+  }
+
   return {
     projectList,
     project,
+    allowed_projects,
+    projSelect,
+
+    fetchProjectList,
+    fetchProject,
+    createProject,
+    updateProject,
+    deleteProject,
   }
 })
