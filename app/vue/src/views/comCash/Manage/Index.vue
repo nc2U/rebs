681ad6478d4afbf72c5bbcd800dbdbf7978c5441
Austin-Kho <kori.susie@gmail.com>
2022-08-14 10:31:43 +0900

    comCash manage index update

--- app/vue/src/views/comCash/Manage/Index.vue
+++ app/vue/src/views/comCash/Manage/Index.vue
@@ -1,3 +1,142 @@
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useCompany } from '@/store/pinia/company'
+import { pageTitle, navMenu } from '@/views/comCash/_menu/headermixin'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+import ListController from '@/views/comCash/Manage/components/ListController.vue'
+import AddCash from '@/views/comCash/Manage/components/AddCash.vue'
+import TableTitleRow from '@/components/TableTitleRow.vue'
+import CashesList from '@/views/comCash/Manage/components/CashesList.vue'
+
+const listControl = ref()
+const dataFilter = ref({
+  page: 1,
+  from_date: '',
+  to_date: '',
+  sort: '',
+  account_d1: '',
+  account_d2: '',
+  account_d3: '',
+  bank_account: '',
+  search: '',
+})
+
+const store = useStore()
+const companyStore = useCompany()
+
+const initComId = computed(() => companyStore.initComId.toString())
+const company = computed(() => companyStore.company)
+
+//     ...mapActions('settings', ['fetchCompany']),
+//     ...mapActions('comCash', [
+//       'fetchAccSortList',
+//       'fetchAllAccD1List',
+//       'fetchAllAccD2List',
+//       'fetchAllAccD3List',
+//       'fetchFormAccD1List',
+//       'fetchFormAccD2List',
+//       'fetchFormAccD3List',
+//       'fetchComBankAccList',
+//       'fetchCashBookList',
+//       'createCashBook',
+//       'updateCashBook',
+//       'deleteCashBook',
+//     ]),
+
+const fetchCompany = (pk: string) => companyStore.fetchCompany(pk)
+const fetchAccSortList = () => store.dispatch('comCash/fetchAccSortList')
+const fetchAllAccD1List = () => store.dispatch('comCash/fetchAllAccD1List')
+const fetchAllAccD2List = () => store.dispatch('comCash/fetchAllAccD2List')
+const fetchAllAccD3List = () => store.dispatch('comCash/fetchAllAccD3List')
+const fetchFormAccD1List = (payload: { sort: string }) =>
+  store.dispatch('comCash/fetchFormAccD1List', payload)
+const fetchFormAccD2List = (payload: { sort: string; d1: string }) =>
+  store.dispatch('comCash/fetchFormAccD2List', payload)
+const fetchFormAccD3List = (payload: {
+  sort: string
+  d1: string
+  d2: string
+}) => store.dispatch('comCash/fetchFormAccD3List', payload)
+const fetchComBankAccList = (pk: string) =>
+  store.dispatch('comCash/fetchComBankAccList', pk)
+const fetchCashBookList = (payload: any) =>
+  store.dispatch('comCash/fetchCashBookList', payload)
+const createCashBook = (payload: any) =>
+  store.dispatch('comCash/createCashBook', payload)
+const updateCashBook = (payload: any) =>
+  store.dispatch('comCash/updateCashBook', payload)
+const deleteCashBook = (payload: any) =>
+  store.dispatch('comCash/deleteCashBook', payload)
+
+const onSelectAdd = (target: any) => {
+  if (!!target) {
+    fetchCompany(target)
+    fetchComBankAccList(target)
+    fetchCashBookList({ company: target })
+  } else {
+    store.state.settings.company = null
+    store.state.comCash.comBankList = []
+    store.state.comCash.cashBookList = []
+    store.state.comCash.cashBookCount = 0
+  }
+}
+
+const pageSelect = (page: number) => {
+  dataFilter.value.page = page
+  listControl.value.listFiltering(page)
+}
+
+const listFiltering = (payload: any) => {
+  dataFilter.value = payload
+  const pk = company.value?.pk.toString()
+  const sort = payload.sort ? payload.sort : ''
+  const d1 = payload.account_d1 ? payload.account_d1 : ''
+  const d2 = payload.account_d2 ? payload.account_d2 : ''
+  fetchFormAccD1List({ sort })
+  fetchFormAccD2List({ sort, d1 })
+  fetchFormAccD3List({ sort, d1, d2 })
+  fetchCashBookList({ ...{ company: pk }, ...payload })
+}
+
+const onCreate = (payload: any) => {
+  payload.company = company.value?.pk
+  if (payload.sort === '3' && payload.bank_account_to) {
+    const { bank_account_to, income, ...outData } = payload
+
+    createCashBook(outData)
+
+    const { bank_account, outlay, ...incData } = outData
+
+    createCashBook({
+      ...{ bank_account: bank_account_to, income },
+      ...incData,
+    })
+  } else createCashBook(payload)
+}
+
+const onUpdate = (payload: any) => {
+  updateCashBook({ ...{ filters: dataFilter.value }, ...payload })
+}
+
+const onDelete = (payload: any) => {
+  deleteCashBook({ ...{ filters: dataFilter.value }, ...payload })
+}
+
+onBeforeMount(() => {
+  fetchAccSortList()
+  fetchAllAccD1List()
+  fetchAllAccD2List()
+  fetchAllAccD3List()
+  fetchFormAccD1List({ sort: '' })
+  fetchFormAccD2List({ sort: '', d1: '' })
+  fetchFormAccD3List({ sort: '', d1: '', d2: '' })
+  fetchComBankAccList(initComId.value)
+  fetchCashBookList({ company: initComId.value })
+})
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
@@ -21,120 +160,3 @@
     <CCardFooter>&nbsp;</CCardFooter>
   </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/comCash/_menu/headermixin'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import ListController from '@/views/comCash/Manage/components/ListController.vue'
-import AddCash from '@/views/comCash/Manage/components/AddCash.vue'
-import TableTitleRow from '@/components/TableTitleRow.vue'
-import CashesList from '@/views/comCash/Manage/components/CashesList.vue'
-import { mapActions, mapGetters, mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'ComCashManage',
-  components: {
-    ContentHeader,
-    ContentBody,
-    ListController,
-    AddCash,
-    TableTitleRow,
-    CashesList,
-  },
-  mixins: [HeaderMixin],
-  data() {
-    return {
-      dataFilter: {
-        page: 1,
-        from_date: '',
-        to_date: '',
-        sort: '',
-        account_d1: '',
-        account_d2: '',
-        account_d3: '',
-        bank_account: '',
-        search: '',
-      },
-    }
-  },
-  created() {
-    this.fetchAccSortList()
-    this.fetchAllAccD1List()
-    this.fetchAllAccD2List()
-    this.fetchAllAccD3List()
-    this.fetchFormAccD1List()
-    this.fetchFormAccD2List()
-    this.fetchFormAccD3List()
-    this.fetchComBankAccList(this.initComId)
-    this.fetchCashBookList({ company: this.initComId })
-  },
-  computed: {
-    ...mapState('settings', ['company']),
-    ...mapGetters('accounts', ['initComId']),
-  },
-  methods: {
-    onSelectAdd(this: any, target: any) {
-      if (target !== '') {
-        this.fetchCompany(target)
-        this.fetchComBankAccList(target)
-        this.fetchCashBookList({ company: target })
-      } else {
-        this.$store.state.settings.company = null
-        this.$store.state.comCash.comBankList = []
-        this.$store.state.comCash.cashBookList = []
-        this.$store.state.comCash.cashBookCount = 0
-      }
-    },
-    pageSelect(this: any, page: number) {
-      this.dataFilter.page = page
-      this.$refs.listControl.listFiltering(page)
-    },
-    listFiltering(payload: any) {
-      this.dataFilter = payload
-      const company = this.company.pk
-      const sort = payload.sort ? payload.sort : ''
-      const d1 = payload.account_d1 ? payload.account_d1 : ''
-      const d2 = payload.account_d2 ? payload.account_d2 : ''
-      this.fetchFormAccD1List({ sort })
-      this.fetchFormAccD2List({ sort, d1 })
-      this.fetchFormAccD3List({ sort, d1, d2 })
-      this.fetchCashBookList({ ...{ company }, ...payload })
-    },
-    onCreate(payload: any) {
-      payload.company = this.company.pk
-      if (payload.sort === '3' && payload.bank_account_to) {
-        const { bank_account_to, income, ...outData } = payload
-        this.createCashBook(outData)
-        const { bank_account, outlay, ...incData } = outData
-        this.createCashBook({
-          ...{ bank_account: bank_account_to, income },
-          ...incData,
-        })
-      } else this.createCashBook(payload)
-    },
-    onUpdate(payload: any) {
-      this.updateCashBook({ ...{ filters: this.dataFilter }, ...payload })
-    },
-    onDelete(payload: any) {
-      this.deleteCashBook({ ...{ filters: this.dataFilter }, ...payload })
-    },
-    ...mapActions('settings', ['fetchCompany']),
-    ...mapActions('comCash', [
-      'fetchAccSortList',
-      'fetchAllAccD1List',
-      'fetchAllAccD2List',
-      'fetchAllAccD3List',
-      'fetchFormAccD1List',
-      'fetchFormAccD2List',
-      'fetchFormAccD3List',
-      'fetchComBankAccList',
-      'fetchCashBookList',
-      'createCashBook',
-      'updateCashBook',
-      'deleteCashBook',
-    ]),
-  },
-})
-</script>
