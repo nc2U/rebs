1085fd539f635a971ed48eb2e53670fa26133de8
Austin-Kho <kori.susie@gmail.com>
2022-08-14 15:01:21 +0900

    project list update

--- app/vue/src/views/projects/List/Index.vue
+++ app/vue/src/views/projects/List/Index.vue
@@ -1,3 +1,50 @@
+<script lang="ts" setup>
+import { computed, ref, watch } from 'vue'
+import { useStore } from 'vuex'
+import { useAccount } from '@/store/pinia/account'
+import { useProject } from '@/store/pinia/project'
+import { pageTitle, navMenu } from '@/views/projects/_menu/headermixin1'
+import { Project } from '@/store/pinia/project'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+import IndexForm from '@/views/projects/List/components/IndexForm.vue'
+import IndexDetail from '@/views/projects/List/components/IndexDetail.vue'
+
+const compName = ref('IndexDetail')
+const update = ref(false)
+
+const store = useStore()
+const accountStore = useAccount()
+const projectStore = useProject()
+
+const userInfo = computed(() => accountStore.userInfo)
+const project = computed(() => projectStore.project)
+
+watch(project, () => (compName.value = 'IndexDetail'))
+
+const createProject = (payload: Project) =>
+  store.dispatch('project/createProject', payload)
+const updateProject = (payload: { pk: string } & Project) =>
+  store.dispatch('project/updateProject', payload)
+
+const createForm = () => {
+  update.value = false
+  compName.value = 'IndexForm'
+}
+
+const updateForm = () => {
+  update.value = true
+  compName.value = 'IndexForm'
+}
+
+const resetForm = () => {
+  update.value = false
+  compName.value = 'IndexDetail'
+}
+const toCreate = (payload: Project) => createProject(payload)
+const toUpdate = (payload: { pk: string } & Project) => updateProject(payload)
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
@@ -6,8 +53,8 @@
   />
 
   <ContentBody>
-    <component
-      :is="compName"
+    <IndexDetail
+      v-if="compName === 'IndexDetail'"
       :user-info="userInfo"
       :project="project"
       :update="update"
@@ -17,62 +64,7 @@
       @create-form="createForm"
       @update-form="updateForm"
     />
+
+    <IndexForm v-if="compName === 'IndexForm'" />
   </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/projects/_menu/headermixin1'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import IndexForm from '@/views/projects/List/components/IndexForm.vue'
-import IndexDetail from '@/views/projects/List/components/IndexDetail.vue'
-import { mapActions, mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'ProjectsIndex',
-  components: {
-    ContentHeader,
-    ContentBody,
-    IndexForm,
-    IndexDetail,
-  },
-  mixins: [HeaderMixin],
-  data() {
-    return {
-      compName: 'IndexDetail',
-      update: false,
-    }
-  },
-  computed: {
-    ...mapState('accounts', ['userInfo']),
-    ...mapState('project', ['project']),
-  },
-  watch: {
-    project() {
-      this.compName = 'IndexDetail'
-    },
-  },
-  methods: {
-    createForm() {
-      this.update = false
-      this.compName = 'IndexForm'
-    },
-    updateForm() {
-      this.update = true
-      this.compName = 'IndexForm'
-    },
-    resetForm() {
-      this.update = false
-      this.compName = 'IndexDetail'
-    },
-    toCreate(payload: any) {
-      this.createProject(payload)
-    },
-    toUpdate(payload: any) {
-      this.updateProject(payload)
-    },
-    ...mapActions('project', ['createProject', 'updateProject']),
-  },
-})
-</script>
