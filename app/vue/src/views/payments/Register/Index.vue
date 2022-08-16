44a7e4b0cf269091afca84ad658447121c263e50
Austin-Kho <kori.susie@gmail.com>
2022-08-17 00:45:21 +0900

    payment Register setup update ing

--- app/vue/src/views/payments/Register/Index.vue
+++ app/vue/src/views/payments/Register/Index.vue
@@ -1,3 +1,128 @@
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref, watch } from 'vue'
+import { useStore } from 'vuex'
+import { useProject } from '@/store/pinia/project'
+import { useRouter, useRoute, onBeforeRouteLeave } from 'vue-router'
+import { pageTitle, navMenu } from '@/views/payments/_menu/headermixin'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+import ContChoicer from '@/views/payments/Register/components/ContChoicer.vue'
+import PaymentListAll from '@/views/payments/Register/components/PaymentListAll.vue'
+import OrdersBoard from '@/views/payments/Register/components/OrdersBoard.vue'
+import CreateButton from '@/views/payments/Register/components/CreateButton.vue'
+
+const paymentId = ref('') as any
+
+const store = useStore()
+const projectStore = useProject()
+
+const project = computed(() => projectStore.project?.pk)
+const initProjId = computed(() => projectStore.initProjId)
+
+const contract = computed(() => store.state.contract.contract)
+const AllPaymentList = computed(() => store.state.payment.AllPaymentList)
+
+const fetchTypeList = (projId: number) =>
+  store.dispatch('project/fetchTypeList', projId)
+const fetchAllPaymentList = (payload: any) =>
+  store.dispatch('payment/fetchAllPaymentList', payload)
+const fetchPayOrderList = (projId: number) =>
+  store.dispatch('payment/fetchPayOrderList', projId)
+const fetchDownPayList = (payload: any) =>
+  store.dispatch('payment/fetchDownPayList', payload)
+const fetchPriceList = (payload: any) =>
+  store.dispatch('payment/fetchPriceList', payload)
+
+const fetchProBankAccList = (projId: number) =>
+  store.dispatch('proCash/fetchProBankAccList', projId)
+const createPrCashBook = (payload: any) =>
+  store.dispatch('proCash/createPrCashBook', payload)
+const updatePrCashBook = (payload: any) =>
+  store.dispatch('proCash/updatePrCashBook', payload)
+const deletePrCashBook = (payload: any) =>
+  store.dispatch('proCash/deletePrCashBook', payload)
+
+const fetchContractList = (payload: any) =>
+  store.dispatch('contract/fetchContractList', payload)
+const fetchContract = (cont: any) =>
+  store.dispatch('contract/fetchContract', cont)
+
+//     ...mapMutations('payment', ['FETCH_PAYMENT_ID']),
+
+const route = useRoute()
+const router = useRouter()
+
+onBeforeMount(() => {
+  if (route.query.contract) {
+    router.push({ name: '건별수납 관리' })
+    getContract(route.query.contract)
+  }
+  if (route.query.payment) {
+    router.push({ name: '건별수납 관리' })
+    paymentId.value = route.query.payment
+  }
+  fetchTypeList(initProjId.value)
+  fetchPayOrderList(initProjId.value)
+  fetchProBankAccList(initProjId.value)
+})
+
+watch(contract, newVal => {
+  if (newVal) {
+    const order_group = newVal.order_group.pk
+    const unit_type = newVal.unit_type.pk
+    fetchPriceList({ project: project.value, order_group, unit_type })
+    fetchDownPayList({ project: project.value, order_group, unit_type })
+    fetchAllPaymentList({
+      project: project.value,
+      contract: newVal.pk,
+      ordering: 'deal_date',
+    })
+  } else {
+    store.commit('payment/updateState', {
+      priceList: [],
+      downPayList: [],
+      AllPaymentList: [],
+    })
+  }
+})
+
+const onSelectAdd = (target: any) => {
+  if (target !== '') {
+    fetchTypeList(target)
+    fetchPayOrderList(target)
+    fetchProBankAccList(target)
+  } else {
+    store.commit('contract/updateState', { contract: null, contractList: [] })
+    store.commit('project/updateState', { unitTypeList: [] })
+    store.commit('payment/updateState', {
+      AllPaymentList: [],
+      payOrderList: [],
+    })
+    store.commit('proCash/updateState', { proBankAccountList: [] })
+  }
+}
+
+const onContFiltering = (payload: any) =>
+  fetchContractList({ ...{ project: project.value }, ...payload })
+
+const getContract = (cont: any) => fetchContract(cont)
+
+const onCreate = (payload: any) => {
+  payload.project = project.value
+  createPrCashBook(payload)
+}
+const onUpdate = (payload: any) => {
+  payload.project = project.value
+  updatePrCashBook(payload)
+}
+const onDelete = (pk: number) =>
+  deletePrCashBook({ project: project.value, pk, contract: contract.value.pk })
+
+onBeforeRouteLeave(() => {
+  store.commit('contract/updateState', { contract: null })
+})
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
@@ -16,7 +141,7 @@
       />
       <CRow>
         <CCol lg="7">
-          <AllPaymentList
+          <PaymentListAll
             :contract="contract"
             :payment-id="paymentId"
             :payment-list="AllPaymentList"
@@ -35,126 +160,3 @@
     <CCardFooter>&nbsp;</CCardFooter>
   </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/payments/_menu/headermixin'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import ContChoicer from '@/views/payments/Register/components/ContChoicer.vue'
-import AllPaymentList from '@/views/payments/Register/components/AllPaymentList.vue'
-import OrdersBoard from '@/views/payments/Register/components/OrdersBoard.vue'
-import CreateButton from '@/views/payments/Register/components/CreateButton.vue'
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'PaymentsRegister',
-  components: {
-    ContentHeader,
-    ContentBody,
-    ContChoicer,
-    AllPaymentList,
-    CreateButton,
-    OrdersBoard,
-  },
-  mixins: [HeaderMixin],
-  beforeRouteLeave(this: any) {
-    this.$store.state.contract.contract = null
-  },
-  data() {
-    return {
-      paymentId: null,
-    }
-  },
-  created(this: any) {
-    if (this.$route.query.contract) {
-      this.$router.push({ name: '건별수납 관리' })
-      this.getContract(this.$route.query.contract)
-    }
-    if (this.$route.query.payment) {
-      this.$router.push({ name: '건별수납 관리' })
-      this.paymentId = this.$route.query.payment
-    }
-    this.fetchTypeList(this.initProjId)
-    this.fetchPayOrderList(this.initProjId)
-    this.fetchProBankAccList(this.initProjId)
-  },
-  computed: {
-    ...mapState('project', ['project']),
-    ...mapGetters('accounts', ['initProjId']),
-    ...mapState('contract', ['contract']),
-    ...mapState('payment', ['AllPaymentList']),
-  },
-  watch: {
-    contract(this: any, newVal) {
-      if (newVal) {
-        const project = this.project.pk
-        const order_group = newVal.order_group.pk
-        const unit_type = newVal.unit_type.pk
-        this.fetchPriceList({ project, order_group, unit_type })
-        this.fetchDownPayList({ project, order_group, unit_type })
-        this.fetchAllPaymentList({
-          project,
-          contract: newVal.pk,
-          ordering: 'deal_date',
-        })
-      } else {
-        this.$store.state.payment.priceList = []
-        this.$store.state.payment.downPayList = []
-        this.$store.state.payment.AllPaymentList = []
-      }
-    },
-  },
-  methods: {
-    onSelectAdd(this: any, target: any) {
-      if (target !== '') {
-        this.fetchTypeList(target)
-        this.fetchPayOrderList(target)
-        this.fetchProBankAccList(target)
-      } else {
-        this.$store.state.contract.contract = null
-        this.$store.state.contract.contractList = []
-        this.$store.state.project.unitTypeList = []
-        this.$store.state.payment.AllPaymentList = []
-        this.$store.state.payment.payOrderList = []
-        this.$store.state.proCash.proBankAccountList = []
-      }
-    },
-    onContFiltering(payload: any) {
-      const project = this.project.pk
-      this.fetchContractList({ ...{ project }, ...payload })
-    },
-    getContract(cont: number) {
-      this.fetchContract(cont)
-    },
-    onCreate(payload: any) {
-      payload.project = this.project.pk
-      this.createPrCashBook(payload)
-    },
-    onUpdate(payload: any) {
-      payload.project = this.project.pk
-      this.updatePrCashBook(payload)
-    },
-    onDelete(pk: number) {
-      const project = this.project.pk
-      const contract = this.contract.pk
-      this.deletePrCashBook({ project, pk, contract })
-    },
-    ...mapActions('project', ['fetchTypeList']),
-    ...mapActions('payment', [
-      'fetchAllPaymentList',
-      'fetchPayOrderList',
-      'fetchDownPayList',
-      'fetchPriceList',
-    ]),
-    ...mapMutations('payment', ['FETCH_PAYMENT_ID']),
-    ...mapActions('proCash', [
-      'fetchProBankAccList',
-      'createPrCashBook',
-      'updatePrCashBook',
-      'deletePrCashBook',
-    ]),
-    ...mapActions('contract', ['fetchContractList', 'fetchContract']),
-  },
-})
-</script>
