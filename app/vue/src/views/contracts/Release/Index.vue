ee63c9ff0cf6b58a4b48b61e3634e0ff205589a5
Austin-Kho <kori.susie@gmail.com>
2022-08-19 23:20:21 +0900

    contract release setup update ing

--- app/vue/src/views/contracts/Release/Index.vue
+++ app/vue/src/views/contracts/Release/Index.vue
@@ -1,3 +1,98 @@
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref, watch } from 'vue'
+import { useStore } from 'vuex'
+import { useProject } from '@/store/pinia/project'
+import { useRoute, useRouter, onBeforeRouteLeave } from 'vue-router'
+import { pageTitle, navMenu } from '@/views/contracts/_menu/headermixin'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+import ReleasetButton from '@/views/contracts/Release/components/ReleasetButton.vue'
+import ContNavigation from '@/views/contracts/Release/components/ContNavigation.vue'
+import ContractorAlert from '@/views/contracts/Release/components/ContractorAlert.vue'
+import ContController from '@/views/contracts/Release/components/ContController.vue'
+import TableTitleRow from '@/components/TableTitleRow.vue'
+import ReleaseList from '@/views/contracts/Release/components/ReleaseList.vue'
+
+const page = ref(1)
+
+const projectStore = useProject()
+const project = computed(() => projectStore.project?.pk)
+const initProjId = computed(() => projectStore.initProjId)
+const downloadUrl = computed(() => `/excel/releases/?project=${project.value}`)
+
+const store = useStore()
+const contractor = computed(() => store.state.contract.contractor)
+
+const fetchContractor = (contor: any) =>
+  store.dispatch('contract/fetchContractor', contor)
+const fetchContractorList = (payload: any) =>
+  store.dispatch('contract/fetchContractorList', payload)
+const fetchContRelease = (payload: any) =>
+  store.dispatch('contract/fetchContRelease', payload)
+const fetchContReleaseList = (payload: { project: number } & any) =>
+  store.dispatch('contract/fetchContReleaseList', payload)
+const createRelease = (payload: any) =>
+  store.dispatch('contract/createRelease', payload)
+const updateRelease = (payload: any) =>
+  store.dispatch('contract/updateRelease', payload)
+
+const route = useRoute()
+
+watch(route, val => {
+  if (val.query.contractor) fetchContractor(val.query.contractor)
+  else store.commit('contract/updateState', { contractor: null })
+})
+
+watch(contractor, val => {
+  if (val && val.contractorrelease) fetchContRelease(val.contractorrelease)
+})
+
+const router = useRouter()
+
+const onSelectAdd = (target: any) => {
+  if (target !== '') {
+    fetchContReleaseList({ project: target })
+  } else {
+    store.commit('contract/updateState', {
+      contractor: null,
+      contractorList: [],
+      contRelease: null,
+      contReleaseList: [],
+      contReleaseCount: 0,
+    })
+  }
+  router.push({ name: '계약해지 관리' })
+}
+const searchContractor = (search: string) => {
+  if (search !== '') {
+    fetchContractorList({ project: project.value, search })
+  } else store.commit('contract/updateState', { contractorList: [] })
+}
+
+const getRelease = (release: number) => fetchContRelease(release)
+
+const pageSelect = (p: number) => {
+  page.value = p
+  fetchContReleaseList({ project: project.value, page: p })
+}
+
+const onSubmit = (payload: any) => {
+  if (payload.pk)
+    updateRelease({ project: project.value, page: page.value, ...payload })
+  else createRelease({ project: project.value, ...payload })
+}
+
+onBeforeMount(() => {
+  fetchContReleaseList({ project: initProjId.value })
+  if (route.query.contractor) fetchContractor(route.query.contractor)
+  else store.commit('contract/updateState', { contractor: null }) // this.FETCH_CONTRACTOR(null)
+})
+
+onBeforeRouteLeave(() =>
+  store.commit('contract/updateState', { contractor: null }),
+)
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
@@ -35,112 +130,3 @@
     <CCardFooter>&nbsp;</CCardFooter>
   </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/contracts/_menu/headermixin'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import ReleasetButton from '@/views/contracts/Release/components/ReleasetButton.vue'
-import ContNavigation from '@/views/contracts/Release/components/ContNavigation.vue'
-import ContractorAlert from '@/views/contracts/Release/components/ContractorAlert.vue'
-import ContController from '@/views/contracts/Release/components/ContController.vue'
-import TableTitleRow from '@/components/TableTitleRow.vue'
-import ReleaseList from '@/views/contracts/Release/components/ReleaseList.vue'
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'ReleaseIndex',
-  components: {
-    ContentHeader,
-    ContentBody,
-    ContNavigation,
-    ContractorAlert,
-    ContController,
-    ReleasetButton,
-    TableTitleRow,
-    ReleaseList,
-  },
-  mixins: [HeaderMixin],
-  beforeRouteLeave() {
-    this.FETCH_CONTRACTOR(null)
-  },
-  data() {
-    return {
-      page: 1,
-    }
-  },
-  created(this: any) {
-    this.fetchContReleaseList({ project: this.initProjId })
-    if (this.$route.query.contractor)
-      this.fetchContractor(this.$route.query.contractor)
-    else this.FETCH_CONTRACTOR(null)
-  },
-  computed: {
-    downloadUrl() {
-      return `/excel/releases/?project=${this.project.pk}`
-    },
-    ...mapState('project', ['project']),
-    ...mapState('contract', ['contractor']),
-    ...mapGetters('accounts', ['initProjId']),
-  },
-  watch: {
-    $route(val) {
-      if (val.query.contractor) this.fetchContractor(val.query.contractor)
-      else this.FETCH_CONTRACTOR(null)
-    },
-    contractor(val) {
-      if (val && val.contractorrelease) {
-        this.fetchContRelease(val.contractorrelease)
-      }
-    },
-  },
-  methods: {
-    onSelectAdd(this: any, target: any) {
-      if (target !== '') {
-        this.fetchContReleaseList({ project: target })
-      } else {
-        this.FETCH_CONTRACTOR(null)
-        this.FETCH_CONTRACTOR_LIST([])
-        this.FETCH_CONT_RELEASE(null)
-        this.FETCH_CONT_RELEASE_LIST([])
-      }
-      this.$router.push({ name: '계약해지 관리' })
-    },
-    searchContractor(search: string) {
-      if (search !== '') {
-        const project = this.project.pk
-        this.fetchContractorList({ project, search })
-      } else this.FETCH_CONTRACTOR_LIST([])
-    },
-    getRelease(release: number) {
-      this.fetchContRelease(release)
-    },
-    pageSelect(page: number) {
-      this.page = page
-      const project = this.project.pk
-      this.fetchContReleaseList({ project, page })
-    },
-    onSubmit(payload: any) {
-      const page = this.page
-      const project = this.project.pk
-      if (payload.pk) this.updateRelease({ project, page, ...payload })
-      else this.createRelease({ project, ...payload })
-    },
-    ...mapActions('contract', [
-      'fetchContractor',
-      'fetchContractorList',
-      'fetchContRelease',
-      'fetchContReleaseList',
-      'createRelease',
-      'updateRelease',
-    ]),
-    ...mapMutations('contract', [
-      'FETCH_CONTRACTOR',
-      'FETCH_CONTRACTOR_LIST',
-      'FETCH_CONT_RELEASE',
-      'FETCH_CONT_RELEASE_LIST',
-    ]),
-  },
-})
-</script>
