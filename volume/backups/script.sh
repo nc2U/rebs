1987d213d6b30ebfb96cea252994649f722bd34a
Austin-Kho <kori.susie@gmail.com>
2022-08-28 02:29:15 +0900

    db backup workflow update

--- volume/backups/script.sh
+++ volume/backups/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-DATE=`date +"%Y-%m-%d"`
-SQLFILE=/var/backups/db-${MYSQL_DATABASE}-${DATE}.sql
+DATE=$(date +"%Y-%m-%d")
+SQL_FILE=/var/backups/db-${MYSQL_DATABASE}-${DATE}.sql
 DATABASE=${MYSQL_DATABASE}
 USER=${MYSQL_USER}
 PASSWORD=${MYSQL_PASSWORD}
 
 # (2) in case you run this more than once a day,
 # remove the previous version of the file
-rm $(find . -mtime +2 -name "*.sql")
+# shellcheck disable=SC2046
+rm -rf $(find . -mtime +2)
 
 # (3) do the mysql database backup (dump)
-mysqldump -u${USER} -p${PASSWORD} ${DATABASE} --ignore-table=${DATABASE}.django_migrations > ${SQLFILE}
+mysqldump -u"${USER}" -p"${PASSWORD}" "${DATABASE}" --ignore-table="${DATABASE}".django_migrations > "${SQL_FILE}"
