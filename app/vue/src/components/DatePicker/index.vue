39fcb2832a98b53a06bbb51f2630e2c1ddf7734e
Austin-Kho <kori.susie@gmail.com>
2022-09-03 19:36:51 +0900

    DatePicker component update

--- app/vue/src/components/DatePicker/index.vue
+++ app/vue/src/components/DatePicker/index.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
-import Datepicker from '@vuepic/vue-datepicker'
 import { dateFormat } from '@/utils/baseMixins'
 import { maska as vMaska } from 'maska'
+import Datepicker from '@vuepic/vue-datepicker'
 
 const store = useStore()
 
-const textInputOptions = computed(() => ({
+const textInputOptions = ref({
   enterSubmit: true,
   tabSubmit: true,
   openMenu: true,
   format: 'yyyy-MM-dd',
-}))
+})
 
 const isDark = computed(() => store.state.theme === 'dark')
 </script>
@@ -31,6 +31,5 @@ const isDark = computed(() => store.state.theme === 'dark')
     teleport="#app"
     :dark="isDark"
     locale="ko"
-    required
   />
 </template>
