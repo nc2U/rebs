de317f41fc512995ec0895f30f4838b46b3b332d
Austin-Kho <kori.susie@gmail.com>
2022-09-04 14:50:59 +0900

    project store type update

--- app/vue/src/store/modules/project/state.ts
+++ app/vue/src/store/modules/project/state.ts
@@ -1,74 +1,16 @@
 import { State } from '@/store'
-import { Project } from '@/store/pinia/project'
 import {
+  Project,
+  UnitType,
+  UnitFloorType,
+  BuildingUnit,
+  HouseUnit,
+  ProjectBudget,
   Site,
   SiteOwner,
-  SiteOwnshipRelationship,
+  Relation,
   SiteContract,
-} from '@/store/pinia/project_site'
-
-interface UnitType {
-  pk: number
-  project: number
-  name: string
-  color: string
-  actual_area: number | null
-  supply_area: number | null
-  contract_area: number | null
-  average_price: number
-  num_unit: number
-}
-
-interface UnitFloorType {
-  pk: number
-  project: number
-  start_floor: number
-  end_floor: number
-  extra_cond: string
-  alias_name: string
-}
-
-interface Contract {
-  pk: number
-  contractor: {
-    status: string
-    name: string
-  }
-}
-
-export interface KeyUnit {
-  pk: number
-  contract: Contract | null
-}
-
-export interface BuildingUnit {
-  pk: number
-  project: number
-  name: string
-}
-
-export interface HouseUnit {
-  pk: number
-  project: number
-  unit_type: number
-  floor_type: number
-  __str__: string
-  building_unit: number
-  name: string
-  key_unit: KeyUnit
-  bldg_line: number
-  floor_no: number
-  is_hold: boolean
-  hold_reason: string
-}
-
-export interface ProjectBudget {
-  pk: number
-  project: number
-  account_d1: number
-  account_d2: number
-  budget: number
-}
+} from '@/store/types/project'
 
 export interface ProjectState extends State {
   projectList: Project[]
@@ -82,7 +24,7 @@ export interface ProjectState extends State {
   projectBudgetList: ProjectBudget[]
   siteList: Site[]
   siteOwnerList: SiteOwner[]
-  siteOwnerRelationList: SiteOwnshipRelationship[]
+  siteOwnerRelationList: Relation[]
   siteContractList: SiteContract[]
 }
 
--- app/vue/src/store/types/project.ts
+++ app/vue/src/store/types/project.ts
@@ -29,6 +29,69 @@ export interface Project {
   num_planed_parking: number | null
 }
 
+export interface UnitType {
+  pk: number
+  project: number
+  name: string
+  color: string
+  actual_area: number | null
+  supply_area: number | null
+  contract_area: number | null
+  average_price: number
+  num_unit: number
+}
+
+export interface UnitFloorType {
+  pk: number
+  project: number
+  start_floor: number
+  end_floor: number
+  extra_cond: string
+  alias_name: string
+}
+
+export interface BuildingUnit {
+  pk: number
+  project: number
+  name: string
+}
+
+export interface KeyUnit {
+  pk: number
+  contract: Contract | null
+}
+
+interface Contract {
+  pk: number
+  contractor: {
+    status: string
+    name: string
+  }
+}
+
+export interface HouseUnit {
+  pk: number
+  project: number
+  unit_type: number
+  floor_type: number
+  __str__: string
+  building_unit: number
+  name: string
+  key_unit: KeyUnit
+  bldg_line: number
+  floor_no: number
+  is_hold: boolean
+  hold_reason: string
+}
+
+export interface ProjectBudget {
+  pk: number
+  project: number
+  account_d1: number
+  account_d2: number
+  budget: number
+}
+
 export interface AllSite {
   pk: number
   __str__: string
