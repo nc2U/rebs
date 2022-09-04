8a19b77daaa5bcf59a44572caa5f0888f5acd135
Austin-Kho <kori.susie@gmail.com>
2022-09-04 21:39:26 +0900

    api url to api/v1 update

--- app/django/_config/urls.py
+++ app/django/_config/urls.py
@@ -30,11 +30,11 @@ admin.site.site_title = 'Rebs 사이트 관리'  # default: "Django site admin"
 
 url = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('accounts/', include('allauth.urls')),  # path('accounts/', include('accounts.urls')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
