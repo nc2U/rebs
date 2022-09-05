e0af4f9675153cd3481de0e86fa97b01f41c5b9c
Austin-Kho <kori.susie@gmail.com>
2022-09-05 21:34:45 +0900

    notice types update

--- app/vue/src/store/modules/notice/state.ts
+++ app/vue/src/store/modules/notice/state.ts
@@ -1,28 +1,7 @@
 import { State } from '@/store'
+import { SalesBillIssue } from '@/store/types/notice'
 
-export declare interface SalesBillIssue {
-  pk: number
-  project: number
-  now_payment_order: number
-  host_name: string
-  host_tel: string
-  agency: string
-  agency_tel: string
-  bank_account1: string
-  bank_number1: string
-  bank_host1: string
-  bank_account2: string
-  bank_number2: string
-  bank_host2: string
-  zipcode: string
-  address1: string
-  address2: string
-  address3: string
-  title: string
-  content: string
-}
-
-export declare interface NoticeState extends State {
+export interface NoticeState extends State {
   billIssue: SalesBillIssue | null
 }
 
