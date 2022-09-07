950f64d11151e79890c564a622368d3c88e4769c
Austin-Kho <kori.susie@gmail.com>
2022-09-07 16:40:50 +0900

    contract pinia update

--- app/vue/src/store/pinia/contract.ts
+++ app/vue/src/store/pinia/contract.ts
@@ -14,6 +14,10 @@ import {
   DownPayment,
   ContractRelease,
 } from '@/store/types/contract'
+import {
+  FETCH_CONTRACTOR,
+  FETCH_CONTRACTOR_LIST,
+} from '@/store/modules/contract/mutations-types'
 
 export const useContract = defineStore('contract', () => {
   // state & getters
@@ -46,9 +50,6 @@ export const useContract = defineStore('contract', () => {
     })),
   )
   const contractsCount = ref<number>(0)
-
-  const contractor = ref<Contractor | null>(null)
-  const contractorList = ref<Contractor[]>([])
 
   // actions
   const contractPages = (itemsPerPage: number) =>
@@ -101,6 +102,23 @@ export const useContract = defineStore('contract', () => {
         fetchContractList({ project: res.data.project }).then(() => message())
       })
       .catch(err => errorHandle(err.response.data))
+
+  const contractor = ref<Contractor | null>(null)
+  const contractorList = ref<Contractor[]>([])
+
+  // actions
+  const fetchContractor = (pk: number) =>
+    api
+      .get(`/contractor/${pk}/`)
+      .then(res => (contractor.value = res.data))
+      .catch(err => errorHandle(err.response.data))
+
+  const fetchContractorList = (project: number, search: number) => {
+    api
+      .get(`/contractor/?project=${project}&search=${search}`)
+      .then(res => (contractorList.value = res.data.results))
+      .catch(err => errorHandle(err.response.data))
+  }
 
   // state & getters
   const subsSummaryList = ref<SubsSummary[]>([])
@@ -226,6 +244,54 @@ export const useContract = defineStore('contract', () => {
       .then(res => (contReleaseList.value = res.data.results))
       .catch(err => errorHandle(err.response.data))
 
+  const createRelease = (payload: any) => {
+    // 1. 해지 정보 테이블 데이터 생성
+    const { refund_amount, ...restData } = payload
+    const createData = !refund_amount ? restData : payload
+
+    api
+      .post(`/contractor-release/`, createData)
+      .then(res => {
+        fetchContRelease(res.data.pk)
+        fetchContReleaseList(payload.project)
+      })
+      .catch(err => errorHandle(err.response.data))
+
+    if (payload.status >= '4') {
+      // releaseSet(payload)
+    }
+    message()
+  }
+
+  const updateRelease = (payload: any) => {
+    const { pk, refund_amount, ...updateData } = payload
+
+    api.get(`/contractor-release/${pk}/`).then(res => {
+      const status = res.data.status < '4' && payload.status >= '4'
+      const completion_date =
+        !res.data.completion_date && payload.completion_date
+      if (status || completion_date) {
+        // releaseSet(payload)
+      }
+
+      // 1. 해지 정보 테이블 데이터 업데이트
+      const page = payload.page
+      const project = payload.project
+      const fixedData =
+        refund_amount !== ''
+          ? { refund_amount, ...updateData }
+          : { ...{ refund_amount: null }, ...updateData }
+      api
+        .put(`/contractor-release/${pk}/`, fixedData)
+        .then(res => {
+          fetchContRelease(res.data.pk)
+          fetchContReleaseList(project, page)
+        })
+        .catch(err => errorHandle(err.response.data))
+    })
+    message()
+  }
+
   const contBillIndex = computed(() =>
     contractList.value.map(c => ({
       pk: c.pk,
@@ -260,14 +326,17 @@ export const useContract = defineStore('contract', () => {
     contractIndex,
     contractsCount,
 
-    contractor,
-    contractorList,
-
     contractPages,
     fetchContract,
     fetchContractList,
     createContractSet,
     updateContractSet,
+
+    contractor,
+    contractorList,
+
+    fetchContractor,
+    fetchContractorList,
 
     subsSummaryList,
     contSummaryList,
@@ -299,6 +368,8 @@ export const useContract = defineStore('contract', () => {
     releasePages,
     fetchContRelease,
     fetchContReleaseList,
+    createRelease,
+    updateRelease,
 
     contBillIndex,
   }
