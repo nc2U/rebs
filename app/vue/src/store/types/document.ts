458bbf9cbd93e34c26787e8c398d930917e8a656
Austin-Kho <kori.susie@gmail.com>
2022-09-15 17:13:15 +0900

    document types update

---
+++ app/vue/src/store/types/document.ts
@@ -0,0 +1,24 @@
+export interface Post {
+  pk: number
+  board: number
+  is_notice: boolean
+  project: number
+  category: number | null
+  lawsuit: number | null
+  title: string
+  execution_date: string
+  content: string
+  is_hide_comment: boolean
+  hit: number
+  like: number
+  dislike: number
+  blame: number
+  ip: string | null
+  device: string
+  secret: boolean
+  password: string
+  user: number | null
+  soft_delete: string | null
+  created: string
+  updated: string
+}
