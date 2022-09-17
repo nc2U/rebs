579038370126a33925cd8575699fa00c9c8f90e8
Austin-Kho <kori.susie@gmail.com>
2022-09-17 21:57:07 +0900

    post api update

--- app/django/apiV1/serializers/document.py
+++ app/django/apiV1/serializers/document.py
@@ -33,13 +33,14 @@ class LawSuitCaseSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    project_name = serializers.SlugField(source='project', read_only=True)
+    proj_name = serializers.SlugField(source='project', read_only=True)
+    cate_name = serializers.SlugField(source='category', read_only=True)
     user = serializers.SlugRelatedField(read_only=True, slug_field='username')
 
     class Meta:
         model = Post
-        fields = ('pk', 'board', 'is_notice', 'project', 'project_name', 'category', 'lawsuit', 'title',
-                  'execution_date', 'content', 'is_hide_comment', 'hit', 'like', 'dislike', 'blame',
+        fields = ('pk', 'board', 'is_notice', 'project', 'proj_name', 'category', 'cate_name', 'lawsuit',
+                  'title', 'execution_date', 'content', 'is_hide_comment', 'hit', 'like', 'dislike', 'blame',
                   'ip', 'device', 'secret', 'password', 'user', 'soft_delete', 'created', 'updated')
 
 
