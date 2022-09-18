d1b698dec576337cc977bb5bdec61013377a4e83
Austin-Kho <kori.susie@gmail.com>
2022-09-18 21:58:53 +0900

    editor component updajte

---
+++ app/vue/src/components/TinyMce/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import Editor from '@tinymce/tinymce-vue'
+import { computed } from 'vue'
+
+const store = useStore()
+const isDark = computed(() => store.state.theme)
+const editorSkin = computed(() =>
+  isDark.value === 'dark' ? 'oxide-dark' : 'oxide',
+)
+</script>
+
+<template>
+  <Editor
+    api-key="11ntkikesp2cxo74r2lzk7b76i5oajmeixas0of64z4ugj7k"
+    :init="{
+      // selector: 'textarea', // change this value according to your HTML
+      language: 'ko_KR',
+      plugins:
+        'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap mentions quickbars linkchecker emoticons advtable export',
+      mobile: {
+        plugins:
+          'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker textpattern noneditable help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable',
+      },
+      menubar: false,
+      toolbar:
+        'undo redo | bold italic underline strikethrough | formatselect fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist checklist | pagebreak | image media codesample | fullscreen  preview save print | charmap emoticons',
+      skin: editorSkin,
+      content_css: isDark,
+      statusbar: false,
+    }"
+  />
+</template>
---
+++ app/vue/src/components/TipTap/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: '',
+  extensions: [StarterKit],
+  autofocus: true,
+  editable: true,
+  injectCSS: false,
+})
+</script>
+
+<template>
+  <EditorContent :editor="editor" class="form-control" />
+</template>
