f44e66e1532bee36811f8277c24de191dc051e13
Austin-Kho <kori.susie@gmail.com>
2022-09-19 18:04:22 +0900

    comDocs update

--- app/vue/src/views/comDocs/GeneralDocs/components/DocsForm.vue
+++ app/vue/src/views/comDocs/GeneralDocs/components/DocsForm.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup="">
+<script lang="ts" setup>
 import { ref, reactive, computed, onBeforeMount, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useDocument } from '@/store/pinia/document'
--- app/vue/src/views/comDocs/GeneralDocs/components/DocsView.vue
+++ app/vue/src/views/comDocs/GeneralDocs/components/DocsView.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup="">
+<script lang="ts" setup>
 import { computed, onBeforeMount, onMounted } from 'vue'
 import { timeFormat } from '@/utils/baseMixins'
 import { useDocument } from '@/store/pinia/document'
