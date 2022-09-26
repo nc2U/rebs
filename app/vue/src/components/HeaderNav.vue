6ec36635af480dc497a4305a7388745a0ca60f9a
Austin-Kho <kori.susie@gmail.com>
2022-09-26 17:10:54 +0900

    HeaderNav component update

--- app/vue/src/components/HeaderNav.vue
+++ app/vue/src/components/HeaderNav.vue
@@ -1,8 +1,12 @@
+<script lang="ts" setup>
+defineProps({ menus: { type: Array, required: true } })
+</script>
+
 <template>
   <CNav variant="tabs" class="mb-5">
     <CNavItem v-for="(menu, i) in menus" :key="i">
       <CNavLink
-        :active="$route.name == menu"
+        :active="$route.name === menu || $route.meta.title === menu"
         @click="$router.push({ name: menu })"
       >
         {{ menu }}
@@ -10,17 +14,3 @@
     </CNavItem>
   </CNav>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'HeadNav',
-  props: {
-    menus: {
-      type: Array,
-      required: true,
-    },
-  },
-})
-</script>
