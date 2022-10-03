d5f426304ad96bd311f7950388ab17fa9ccea26d
Austin-Kho <kori.susie@gmail.com>
2022-10-03 15:43:50 +0900

    Quill editor component update

--- app/vue/src/components/QuillEditor/index.vue
+++ app/vue/src/components/QuillEditor/index.vue
@@ -1,7 +1,30 @@
 <script lang="ts" setup>
 import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+  [{ direction: 'rtl' }], // text direction
+
+  ['blockquote', 'code-block'],
+
+  // [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+
+  ['clean'], // remove formatting button
+]
 </script>
 
 <template>
-  <QuillEditor />
+  <QuillEditor theme="snow" :toolbar="toolbarOptions" />
 </template>
