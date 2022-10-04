7f434331b828aca1269db00b032f0fe06fe49fe6
Austin-Kho <kori.susie@gmail.com>
2022-10-04 22:29:00 +0900

    comDocs pinia update

--- app/vue/src/store/pinia/document.ts
+++ app/vue/src/store/pinia/document.ts
@@ -182,23 +182,21 @@ export const useDocument = defineStore('document', () => {
       .catch(err => errorHandle(err.response.data))
   }
 
-  const createPost = (payload: { form: Post }) =>
+  const createPost = (payload: { form: FormData }) =>
     api
-      .post(`/post/`, payload.form)
-      // .post(`/post/`, payload, {
-      //   headers: { 'Content-Type': 'multipart/form-data' },
-      // })
+      .post(`/post/`, payload.form, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
       .then(res =>
         fetchPostList({ board: res.data.board }).then(() => message()),
       )
       .catch(err => errorHandle(err.response.data))
 
-  const updatePost = (payload: { pk: number; form: Post }) =>
+  const updatePost = (payload: { pk: number; form: FormData }) =>
     api
-      .put(`/post/${payload.pk}/`, payload.form)
-      // .put(`/post/${payload.pk}/`, payload, {
-      //   headers: { 'Content-Type': 'multipart/form-data' },
-      // })
+      .put(`/post/${payload.pk}/`, payload.form, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
       .then(() => message())
       .catch(err => errorHandle(err.response.data))
 
