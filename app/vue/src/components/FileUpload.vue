f1e137e51d363472a8958c1ca58654fd5783277d
Austin-Kho <kori.susie@gmail.com>
2022-10-04 22:15:18 +0900

    FileUpload component update

---
+++ app/vue/src/components/FileUpload.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: false,
+    default: '',
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const onChange = (event: { target: { files: File[] } }) =>
+  emit('update:modelValue', event.target.files[0])
+</script>
+
+<template>
+  <input type="file" class="form-control" @change="onChange" />
+</template>
