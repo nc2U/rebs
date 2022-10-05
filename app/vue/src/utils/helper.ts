c8db542b1e4bb1e51f26672cc62e8076c377e0b5
Austin-Kho <kori.susie@gmail.com>
2022-10-05 13:44:47 +0900

    formutility update

--- app/vue/src/utils/helper.ts
+++ app/vue/src/utils/helper.ts
@@ -55,3 +55,32 @@ export const isValidate = (event: Event) => {
     return true
   } else return false
 }
+
+export const formUtility = {
+  getFormData(val: any, formData = new FormData(), namespace = '') {
+    if (typeof val !== 'undefined' && val !== null) {
+      if (val instanceof Date) {
+        formData.append(namespace, val.toISOString())
+      } else if (val instanceof Array) {
+        for (let i = 0; i < val.length; i++) {
+          this.getFormData(val[i], formData, namespace + '[' + i + ']')
+        }
+      } else if (typeof val === 'object' && !(val instanceof File)) {
+        for (const propertyName in val) {
+          if (val.hasOwnProperty(propertyName)) {
+            this.getFormData(
+              val[propertyName],
+              formData,
+              namespace ? `${namespace}[${propertyName}]` : propertyName,
+            )
+          }
+        }
+      } else if (val instanceof File) {
+        formData.append(namespace, val)
+      } else {
+        formData.append(namespace, val.toString())
+      }
+    }
+    return formData
+  },
+}
