f6bc8c18294b50aafa52db9565af16585aed1c22
Austin-Kho <kori.susie@gmail.com>
2022-10-17 11:16:50 +0900

    document api update ing

--- app/django/apiV1/views/document.py
+++ app/django/apiV1/views/document.py
@@ -50,6 +50,9 @@ class LawSuitCaseViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_queryset(self):
+        return LawsuitCase.objects.all()
+
 
 class AllLawSuitCaseViewSet(LawSuitCaseViewSet):
     serializer_class = SimpleLawSuitCaseSerializer
