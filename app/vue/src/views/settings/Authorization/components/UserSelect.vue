bc967a3c911a5bae6a9ea5e05222b9e812b4c170
Austin-Kho <kori.susie@gmail.com>
2023-01-25 22:22:30 +0900

    authorization user-select component update

--- app/vue/src/views/settings/Authorization/components/UserSelect.vue
+++ app/vue/src/views/settings/Authorization/components/UserSelect.vue
@@ -1,11 +1,25 @@
 <script lang="ts" setup="">
-import { computed, reactive } from 'vue'
+import { ref, computed, nextTick, onBeforeMount } from 'vue'
 import { useStore } from 'vuex'
+import { useAccount } from '@/store/pinia/account'
 import Multiselect from '@vueform/multiselect'
+
+const emit = defineEmits(['select-user'])
+
+const userId = ref<number | null>(null)
 
 const store = useStore()
 const isDark = computed(() => store.state.theme === 'dark')
-const getUsers = reactive([{ value: 1, label: '고창균' }])
+
+const accountStore = useAccount()
+const userInfo = computed(() => accountStore.userInfo)
+const getUsers = computed(() => accountStore.getUsers)
+
+const selectUser = () => nextTick(() => emit('select-user', userId.value))
+
+onBeforeMount(() => {
+  if (userInfo.value) userId.value = userInfo.value.pk as number
+})
 </script>
 
 <template>
@@ -16,12 +30,14 @@ const getUsers = reactive([{ value: 1, label: '고창균' }])
           <CFormLabel class="col-md-4 col-form-label"> 사용자 선택</CFormLabel>
           <CCol>
             <Multiselect
+              v-model="userId"
               :options="getUsers"
               placeholder="사용자"
               autocomplete="label"
               :classes="{ search: 'form-control multiselect-search' }"
               :add-option-on="['enter' | 'tab']"
               searchable
+              @change="selectUser"
             />
           </CCol>
         </CRow>
