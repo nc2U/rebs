5484358fa3cc9a058c76d5c1fdafdbc96e7897d0
Austin-Kho <kori.susie@gmail.com>
2023-01-25 23:36:17 +0900

    authorization index update ing

--- app/vue/src/views/settings/Authorization/Index.vue
+++ app/vue/src/views/settings/Authorization/Index.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
-// import { computed } from 'vue'
+import { computed, onBeforeMount } from 'vue'
 import { pageTitle, navMenu } from '@/views/settings/_menu/headermixin'
-import { useStore } from 'vuex'
+import { useAccount } from '@/store/pinia/account'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
 import UserSelect from './components/UserSelect.vue'
 import ProjectManageAuth from './components/ProjectManageAuth.vue'
 import SideBarManageAuth from './components/SideBarManageAuth.vue'
 
-const store = useStore()
+const accountStore = useAccount()
+const user = computed(() => accountStore.user)
+const userInfo = computed(() => accountStore.userInfo)
+
+const selectUser = (pk: number | null) => {
+  if (pk === null) accountStore.user = null
+  else accountStore.fetchUser(pk)
+}
+const getAllowed = (payload: any) => console.log(payload)
+const getAssigned = (payload: any) => console.log(payload)
+const selectAuth = (payload: any) => console.log(payload)
+
+onBeforeMount(() => {
+  accountStore.fetchUsersList()
+  if (userInfo.value) selectUser(userInfo.value.pk as number)
+})
 </script>
 
 <template>
@@ -19,9 +34,13 @@ const store = useStore()
   />
   <ContentBody>
     <CCardBody>
-      <UserSelect />
-      <ProjectManageAuth />
-      <SideBarManageAuth />
+      <UserSelect @select-user="selectUser" />
+      <ProjectManageAuth :user="user" @get-allowed="getAllowed" />
+      <SideBarManageAuth
+        :user="user"
+        @get-assigned="getAssigned"
+        @select-auth="selectAuth"
+      />
     </CCardBody>
 
     <CCardFooter>&nbsp;</CCardFooter>
