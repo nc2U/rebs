fba56d9a84a56a63e77eaa0a3019cc9834ce5b39
Austin-Kho <kori.susie@gmail.com>
2023-01-26 15:21:23 +0900

    authorization index update

--- app/vue/src/views/settings/Authorization/Index.vue
+++ app/vue/src/views/settings/Authorization/Index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ref, computed, onBeforeMount, watch } from 'vue'
 import { pageTitle, navMenu } from '@/views/settings/_menu/headermixin'
+import { useCompany } from '@/store/pinia/company'
 import { useAccount } from '@/store/pinia/account'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
@@ -8,14 +9,19 @@ import UserSelect from './components/UserSelect.vue'
 import ProjectManageAuth from './components/ProjectManageAuth.vue'
 import SideBarManageAuth from './components/SideBarManageAuth.vue'
 
-const projectAuth = ref({
+const comInfo = ref<{ company: number | null; is_staff: boolean }>({
+  company: null,
   is_staff: true,
+})
+
+const projectAuth = ref({
   assigned_project: null as number | null,
   allowed_projects: [] as number[],
 })
 
 type Auth = '0' | '1' | '2'
-export type MenuType = {
+export type UserAuth = {
+  pk?: number
   contract: Auth
   payment: Auth
   notice: Auth
@@ -28,7 +34,8 @@ export type MenuType = {
   company_docs: Auth
   auth_manage: Auth
 }
-const menuAuth = ref<MenuType>({
+const menuAuth = ref<UserAuth>({
+  pk: undefined,
   contract: '0',
   payment: '0',
   notice: '0',
@@ -41,11 +48,6 @@ const menuAuth = ref<MenuType>({
   company_settings: '0',
   auth_manage: '0',
 })
-
-const accountStore = useAccount()
-const user = computed(() => accountStore.user)
-const userInfo = computed(() => accountStore.userInfo)
-const isStaffAuth = computed(() => !!userInfo.value?.staffauth)
 
 const formsCheck = computed(() => {
   if (userInfo.value && isStaffAuth) {
@@ -75,41 +77,67 @@ const formsCheck = computed(() => {
   } else return false
 })
 
+const companyStore = useCompany()
+const comId = computed(() => companyStore.company?.pk || null)
+
+const accountStore = useAccount()
+const user = computed(() => accountStore.user)
+const userInfo = computed(() => accountStore.userInfo)
+const isStaffAuth = computed(() => !!userInfo.value?.staffauth)
+
 const selectUser = (pk: number | null) => {
   if (pk === null) accountStore.user = null
   else accountStore.fetchUser(pk)
 }
+
 const getAllowed = (payload: number[]) =>
   (projectAuth.value.allowed_projects = payload)
 const getAssigned = (payload: number | null) =>
   (projectAuth.value.assigned_project = payload)
-const selectAuth = (payload: MenuType) => (menuAuth.value = payload)
+const selectAuth = (payload: UserAuth) => (menuAuth.value = payload)
 
-const formSubmit = () => alert('ok!!')
+const formSubmit = () => {
+  const authData = { ...comInfo.value, ...projectAuth.value, ...menuAuth.value }
+  console.log(authData)
+  // accountStore.patchAuth(authData, user.value?.pk)
+}
 
 onBeforeMount(() => {
   accountStore.fetchUsersList()
   if (userInfo.value) selectUser(userInfo.value.pk as number)
+  if (companyStore.company) comInfo.value.company = companyStore.company.pk
 })
 
 watch(
+  () => comId.value,
+  nVal => {
+    if (!!nVal) comInfo.value.company = nVal
+    else {
+      comInfo.value.company = null
+      accountStore.user = null
+    }
+  },
+)
+
+watch(
   () => user.value,
-  nv => {
-    const au = nv?.staffauth
-    if (nv && au) {
-      projectAuth.value.assigned_project = au.assigned_project
-      projectAuth.value.allowed_projects = au.allowed_projects
-      menuAuth.value.contract = au.contract
-      menuAuth.value.payment = au.payment
-      menuAuth.value.notice = au.notice
-      menuAuth.value.project_cash = au.project_cash
-      menuAuth.value.project_docs = au.project_docs
-      menuAuth.value.project = au.project
-      menuAuth.value.company_cash = au.company_cash
-      menuAuth.value.company_docs = au.company_docs
-      menuAuth.value.human_resource = au.human_resource
-      menuAuth.value.company_settings = au.company_settings
-      menuAuth.value.auth_manage = au.auth_manage
+  nVal => {
+    const sa = nVal?.staffauth
+    if (nVal && sa) {
+      projectAuth.value.assigned_project = sa.assigned_project
+      projectAuth.value.allowed_projects = sa.allowed_projects
+      menuAuth.value.pk = sa.pk
+      menuAuth.value.contract = sa.contract
+      menuAuth.value.payment = sa.payment
+      menuAuth.value.notice = sa.notice
+      menuAuth.value.project_cash = sa.project_cash
+      menuAuth.value.project_docs = sa.project_docs
+      menuAuth.value.project = sa.project
+      menuAuth.value.company_cash = sa.company_cash
+      menuAuth.value.company_docs = sa.company_docs
+      menuAuth.value.human_resource = sa.human_resource
+      menuAuth.value.company_settings = sa.company_settings
+      menuAuth.value.auth_manage = sa.auth_manage
     }
   },
 )
