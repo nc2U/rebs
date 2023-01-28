52e48d93f7edafec688a4383c3e4beb595f908a4
Austin-Kho <kori.susie@gmail.com>
2023-01-28 13:51:06 +0900

    hrManage layout update

--- app/vue/src/views/hrManage/Department/Index.vue
+++ app/vue/src/views/hrManage/Department/Index.vue
@@ -1,29 +1,23 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { pageTitle, navMenu } from '@/views/hrManage/_menu/headermixin'
+import ContentHeader from '@/layouts/ContentHeader/Index.vue'
+import ContentBody from '@/layouts/ContentBody/Index.vue'
+
+const msg = ref(pageTitle)
+</script>
+
 <template>
   <ContentHeader
     :page-title="pageTitle"
     :nav-menu="navMenu"
     :selector="'CompanySelect'"
-    @header-select="onSelectAdd"
   />
-  <ContentBody></ContentBody>
+  <ContentBody>
+    <CCardBody>
+      {{ msg }}
+    </CCardBody>
+
+    <CCardFooter class="text-right">&nbsp;</CCardFooter>
+  </ContentBody>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HeaderMixin from '@/views/hrManage/_menu/headermixin'
-import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import ContentBody from '@/layouts/ContentBody/Index.vue'
-import { mapState } from 'vuex'
-
-export default defineComponent({
-  name: 'Department',
-  components: {
-    ContentHeader,
-    ContentBody,
-  },
-  mixins: [HeaderMixin],
-  computed: {
-    ...mapState('settings', ['company']),
-  },
-})
-</script>
--- app/vue/src/views/hrManage/_menu/headermixin.ts
+++ app/vue/src/views/hrManage/_menu/headermixin.ts
@@ -1,11 +1,2 @@
 export const pageTitle = '본사인사 관리'
 export const navMenu = ['직원정보 관리', '부서정보 관리']
-
-export default {
-  data() {
-    return {
-      pageTitle,
-      navMenu,
-    }
-  },
-}
