ba9e81a04af7afe8840a5e7e8c998493c0f7347a
Austin-Kho <kori.susie@gmail.com>
2023-01-29 22:59:59 +0900

    comapny types update

---
+++ app/vue/src/store/types/company.ts
@@ -0,0 +1,30 @@
+export interface Department {
+  pk?: number
+  company: string
+  name: string
+  task: string
+  staffs: []
+}
+
+export interface Rank {
+  pk?: number
+  company: string
+  rank: string
+  title: string
+  description: string
+}
+
+export interface Staff {
+  pk?: number
+  department: number | null
+  rank: number | null
+  name: string
+  birth_date: string | null
+  gender: 'M' | 'F'
+  gender_desc: '남성' | '여성'
+  entered_date: string
+  personal_phone: string
+  email: string
+  status: '1' | '2' | '3' | '4'
+  status_desc: '근무 중' | '정직 중' | '퇴사신청' | '퇴사처리'
+}
