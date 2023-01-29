437eb12c4aa1abb902f077f5866a19250619b7b9
Austin-Kho <kori.susie@gmail.com>
2023-01-29 19:14:45 +0900

    rank list component update

--- app/vue/src/views/hrManage/Rank/components/RankList.vue
+++ app/vue/src/views/hrManage/Rank/components/RankList.vue
@@ -9,18 +9,20 @@ const msg = ref('부서리스트')
 <template>
   <CTable hover responsive bordered align="middle">
     <colgroup>
-      <col width="10%" />
+      <col width="5%" />
       <col width="20%" />
       <col width="20%" />
-      <col width="50%" />
+      <col width="20%" />
+      <col width="35%" />
     </colgroup>
 
     <CTableHead :color="headerSecondary">
       <CTableRow class="text-center" align="middle">
-        <CTableHeaderCell rowspan="2" scope="col">No</CTableHeaderCell>
-        <CTableHeaderCell rowspan="2" scope="col">상위부서</CTableHeaderCell>
-        <CTableHeaderCell rowspan="2" scope="col">부서명</CTableHeaderCell>
-        <CTableHeaderCell rowspan="2" scope="col">주요업무</CTableHeaderCell>
+        <CTableHeaderCell scope="col">No</CTableHeaderCell>
+        <CTableHeaderCell scope="col">구분</CTableHeaderCell>
+        <CTableHeaderCell scope="col">직책</CTableHeaderCell>
+        <CTableHeaderCell scope="col">직함</CTableHeaderCell>
+        <CTableHeaderCell scope="col">설명</CTableHeaderCell>
       </CTableRow>
     </CTableHead>
 
