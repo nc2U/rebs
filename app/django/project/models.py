28e708aeff9f9d918a072a61091e5b208a426e2f
Austin-Kho <kori.susie@gmail.com>
2023-02-28 15:10:03 +0900

    project model update

--- app/django/project/models.py
+++ app/django/project/models.py
@@ -96,8 +96,8 @@ class ProjectOutBudget(models.Model):
     account_d1 = models.ForeignKey('rebs.ProjectAccountD1', on_delete=models.PROTECT, verbose_name='예산항목1')
     account_d2 = models.ForeignKey('rebs.ProjectAccountD2', on_delete=models.SET_NULL, null=True, blank=True,
                                    verbose_name='예산항목2')
-    item_name = models.CharField('항목명', max_length=20, null=True, blank=True)
-    basis_calc = models.CharField('산출근거', max_length=255, null=True, blank=True)
+    item_name = models.CharField('항목명', max_length=20, blank=True, default='')
+    basis_calc = models.CharField('산출근거', max_length=255, blank=True, default='')
     budget = models.PositiveBigIntegerField(verbose_name='지출 예산')
 
     class Meta:
