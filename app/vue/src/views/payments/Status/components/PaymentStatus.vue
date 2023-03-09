4addffb7e9902065ab96e881970824ce355abddf
Austin-Kho <kori.susie@gmail.com>
2023-03-09 13:39:48 +0900

    payment status layout update

--- app/vue/src/views/payments/Status/components/PaymentStatus.vue
+++ app/vue/src/views/payments/Status/components/PaymentStatus.vue
@@ -14,13 +14,14 @@ defineProps({
 const proStore = useProject()
 const budgetList = computed(() => proStore.proIncBudgetList)
 
-const prDataStore = useProjectData()
-const unitType = computed(() => prDataStore.unitTypeList)
-
 const contStore = useContract()
 const orderGroup = computed(() => contStore.orderGroupList)
 const contSum = computed(() => contStore.contSummaryList)
 
+const prDataStore = useProjectData()
+const unitType = computed(() => prDataStore.unitTypeList)
+
+// ---------------------------------------------------------------
 const getTotalQua = (og: number, ut: number) => {
   const unit = budgetList.value.filter(
     b => b.order_group === og && b.unit_type === ut,
@@ -45,17 +46,16 @@ const getUnitPrice = (og: number, ut: number) => {
   )[0]
   return unit ? unit.average_price : 0
 }
+// ---------------------------------------------------------------
 </script>
 
 <template>
-  {{ contSum[0] }}
   <CTable hover responsive bordered align="middle">
     <colgroup>
-      <col width="6%" />
-      <col width="9%" />
-      <col width="9%" />
-      <col width="8%" />
-      <col width="8%" />
+      <col width="10%" />
+      <col width="10%" />
+      <col width="10%" />
+      <col width="10%" />
       <col width="14%" />
       <col width="15%" />
       <col width="15%" />
@@ -63,7 +63,7 @@ const getUnitPrice = (og: number, ut: number) => {
     </colgroup>
     <CTableHead>
       <CTableRow>
-        <CTableDataCell colspan="8">
+        <CTableDataCell colspan="7">
           <strong>
             <CIcon name="cilFolderOpen" />
             차수 및 타입별 수납 현황
@@ -76,7 +76,7 @@ const getUnitPrice = (og: number, ut: number) => {
       </CTableRow>
 
       <CTableRow :color="TableSecondary" class="text-center">
-        <CTableHeaderCell colspan="2">구분</CTableHeaderCell>
+        <CTableHeaderCell>구분</CTableHeaderCell>
         <CTableHeaderCell>타입</CTableHeaderCell>
         <CTableHeaderCell>계획 세대수</CTableHeaderCell>
         <CTableHeaderCell>계약 세대수</CTableHeaderCell>
@@ -88,102 +88,107 @@ const getUnitPrice = (og: number, ut: number) => {
     </CTableHead>
 
     <CTableBody>
-      <template v-if="sort !== '0'">
-        <template v-for="(order, oi) in orderGroup" :key="oi">
-          <CTableRow
-            v-for="(type, ti) in unitType"
-            :key="ti"
-            class="text-right"
-          >
-            <CTableHeaderCell
-              v-if="oi === 0 && ti === 0"
-              class="text-center"
-              :color="TableSecondary"
-              :rowspan="orderGroup.length * unitType.length"
-            >
-              계약
-            </CTableHeaderCell>
-            <CTableDataCell
-              v-if="ti === 0"
-              class="text-center"
-              :color="TableSecondary"
-              :rowspan="unitType.length"
-            >
-              {{ order.order_group_name }}
-            </CTableDataCell>
-            <CTableDataCell class="text-left pl-4">
-              <v-icon icon="mdi mdi-square" :color="type.color" size="sm" />
-              {{ type.name }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{ numFormat(getTotalQua(order.pk, type.pk)) }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{ numFormat(getContNum(order.pk, type.pk)) }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{ numFormat(getUnitPrice(order.pk, type.pk)) }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{
-                numFormat(
-                  getTotalQua(order.pk, type.pk) *
-                    getUnitPrice(order.pk, type.pk),
-                )
-              }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{
-                numFormat(
-                  getContNum(order.pk, type.pk) *
-                    getUnitPrice(order.pk, type.pk) -
-                    getTotalQua(order.pk, type.pk) *
-                      getUnitPrice(order.pk, type.pk) || 0,
-                )
-              }}
-            </CTableDataCell>
-            <CTableDataCell>
-              {{
-                numFormat(
-                  getContNum(order.pk, type.pk) *
-                    getUnitPrice(order.pk, type.pk) || 0,
-                )
-              }}
-            </CTableDataCell>
-          </CTableRow>
-        </template>
-      </template>
+      <CTableRow v-for="bg in budgetList" :key="bg.pk" class="text-right">
+        <CTableDataCell class="text-center">
+          {{ bg.order_group }}
+        </CTableDataCell>
+        <CTableDataCell class="text-left pl-4">
+          {{ bg.unit_type }}
+        </CTableDataCell>
+        <CTableDataCell>{{ numFormat(bg.quantity) }}</CTableDataCell>
+        <CTableDataCell>a</CTableDataCell>
+        <CTableDataCell>{{ numFormat(bg.average_price) }}</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+        <CTableDataCell>-</CTableDataCell>
+        <CTableDataCell>{{ numFormat(bg.budget) }}</CTableDataCell>
+      </CTableRow>
+      <!--      <template v-if="sort !== '0'">-->
+      <!--        <template v-for="(order, oi) in orderGroup" :key="oi">-->
+      <!--          <CTableRow-->
+      <!--            v-for="(type, ti) in unitType"-->
+      <!--            :key="ti"-->
+      <!--            class="text-right"-->
+      <!--          >-->
+      <!--            <CTableDataCell-->
+      <!--              v-if="ti === 0"-->
+      <!--              class="text-center"-->
+      <!--              :color="TableSecondary"-->
+      <!--              :rowspan="unitType.length"-->
+      <!--            >-->
+      <!--              {{ order.order_group_name }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell class="text-left pl-4">-->
+      <!--              <v-icon icon="mdi mdi-square" :color="type.color" size="sm" />-->
+      <!--              {{ type.name }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{ numFormat(getTotalQua(order.pk, type.pk)) }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{ numFormat(getContNum(order.pk, type.pk)) }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{ numFormat(getUnitPrice(order.pk, type.pk)) }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{-->
+      <!--                numFormat(-->
+      <!--                  getTotalQua(order.pk, type.pk) *-->
+      <!--                    getUnitPrice(order.pk, type.pk),-->
+      <!--                )-->
+      <!--              }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{-->
+      <!--                numFormat(-->
+      <!--                  getContNum(order.pk, type.pk) *-->
+      <!--                    getUnitPrice(order.pk, type.pk) - -->
+      <!--                    getTotalQua(order.pk, type.pk) *-->
+      <!--                      getUnitPrice(order.pk, type.pk) || 0,-->
+      <!--                )-->
+      <!--              }}-->
+      <!--            </CTableDataCell>-->
+      <!--            <CTableDataCell>-->
+      <!--              {{-->
+      <!--                numFormat(-->
+      <!--                  getContNum(order.pk, type.pk) *-->
+      <!--                    getUnitPrice(order.pk, type.pk) || 0,-->
+      <!--                )-->
+      <!--              }}-->
+      <!--            </CTableDataCell>-->
+      <!--          </CTableRow>-->
+      <!--        </template>-->
+      <!--      </template>-->
 
-      <template v-if="sort !== '1'">
-        <CTableRow v-for="(type, ti) in unitType" :key="ti" class="text-right">
-          <CTableHeaderCell
-            v-if="ti === 0"
-            class="text-center"
-            :color="TableSecondary"
-            :rowspan="unitType.length"
-            colspan="2"
-          >
-            미계약
-          </CTableHeaderCell>
-          <CTableDataCell class="text-left pl-4">
-            <v-icon icon="mdi mdi-square" :color="type.color" size="sm" />
-            {{ type.name }}
-          </CTableDataCell>
-          <CTableDataCell></CTableDataCell>
-          <CTableDataCell>
-            {{ numFormat(type.num_unit - typeRemainNum(type.pk)) }}
-          </CTableDataCell>
-          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>
-          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>
-          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>
-          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>
-        </CTableRow>
-      </template>
+      <!--      <template v-if="sort !== '1'">-->
+      <!--        <CTableRow v-for="(type, ti) in unitType" :key="ti" class="text-right">-->
+      <!--          <CTableHeaderCell-->
+      <!--            v-if="ti === 0"-->
+      <!--            class="text-center"-->
+      <!--            :color="TableSecondary"-->
+      <!--            :rowspan="unitType.length"-->
+      <!--          >-->
+      <!--            미계약-->
+      <!--          </CTableHeaderCell>-->
+      <!--          <CTableDataCell class="text-left pl-4">-->
+      <!--            <v-icon icon="mdi mdi-square" :color="type.color" size="sm" />-->
+      <!--            {{ type.name }}-->
+      <!--          </CTableDataCell>-->
+      <!--          <CTableDataCell></CTableDataCell>-->
+      <!--          <CTableDataCell>-->
+      <!--            {{ numFormat(type.num_unit - typeRemainNum(type.pk)) }}-->
+      <!--          </CTableDataCell>-->
+      <!--          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>-->
+      <!--          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>-->
+      <!--          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>-->
+      <!--          <CTableDataCell>{{ numFormat(0) }}</CTableDataCell>-->
+      <!--        </CTableRow>-->
+      <!--      </template>-->
     </CTableBody>
 
     <CTableHead>
       <CTableRow class="text-right" :color="TableSecondary">
-        <CTableHeaderCell colspan="3" class="text-center">
+        <CTableHeaderCell colspan="2" class="text-center">
           합계
         </CTableHeaderCell>
         <CTableHeaderCell>{{ numFormat(625) }}</CTableHeaderCell>
