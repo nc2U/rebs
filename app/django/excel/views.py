a7c94673f97bb0dd32bb1d299e982ee9ad7ccd5c
Austin-Kho <kori.susie@gmail.com>
2023-03-10 14:52:34 +0900

    payment status excel layout update

--- app/django/excel/views.py
+++ app/django/excel/views.py
@@ -1642,9 +1642,11 @@ class ExportSites(View):
                 bf = workbook.add_format(body_format)
 
                 if int(col_num) == 5:
-                    worksheet.write(row_num, col_num, float(row[col_num - 1]) * 0.3025, bf)
+                    # worksheet.write(row_num, col_num, float(row[col_num - 1]) * 0.3025, bf)
+                    worksheet.write(row_num, col_num, 500, bf)
                 elif int(col_num) == 7:
-                    worksheet.write(row_num, col_num, float(row[col_num - 2]) * 0.3025, bf)
+                    # worksheet.write(row_num, col_num, float(row[col_num - 2]) * 0.3025, bf)
+                    worksheet.write(row_num, col_num, row[col_num - 2], bf)
 
                 if int(col_num) < 5:
                     worksheet.write(row_num, col_num, row[col_num], bf)
