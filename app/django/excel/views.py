79def841af4a2c8ea38ec3869aeac99d756d559f
Austin-Kho <kori.susie@gmail.com>
2023-03-10 20:23:27 +0900

    payment status excel update ing

--- app/django/excel/views.py
+++ app/django/excel/views.py
@@ -941,9 +941,9 @@ class ExportPaymentStatus(View):
 
                 # if col_num == 0 and row[1] == get_value(row[col_num], unit_type, 0):
                 if col_num == 0:
-                    # worksheet.write(row_num, col_num, get_value(row[col_num], order_group, 1), bformat)
-                    worksheet.merge_range(row_num, col_num, row_num, col_num,
-                                          f'{row[1]} - {get_value(row[col_num], order_group, 0)}', bformat)
+                    worksheet.write(row_num, col_num, get_value(row[col_num], order_group, 1), bformat)
+                    # worksheet.merge_range(row_num, col_num, row_num, col_num,
+                    #                       f'{row[1]} - {get_value(row[col_num], order_group, 0)}', bformat)
                 elif col_num == 1:
                     worksheet.write(row_num, col_num, get_value(row[col_num], unit_type, 1), bformat)
                 elif col_num == 2 or col_num == 3:
