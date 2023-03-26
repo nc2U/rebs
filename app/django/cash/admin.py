a920beb104ca52e755522fb295fb1febf64674f5
Austin-Kho <kori.susie@gmail.com>
2023-03-26 21:30:51 +0900

    cash admin update

--- app/django/cash/admin.py
+++ app/django/cash/admin.py
@@ -59,44 +59,7 @@ class ProjectCashBookAdmin(ImportExportMixin, admin.ModelAdmin):
     formatted_outlay.short_description = '출금액'
 
 
-# class SalesPriceByGTAdmin(ImportExportMixin, admin.ModelAdmin):
-#     list_display = (
-#         'id', 'project', 'order_group', 'unit_type', 'unit_floor_type', 'price_build', 'price_land', 'price_tax',
-#         'price')
-#     list_display_links = ('project', 'unit_type', 'unit_floor_type')
-#     list_editable = ('price_build', 'price_land', 'price_tax', 'price')
-#     list_filter = ('project', 'order_group', 'unit_type')
-#
-#
-# class InstallmentPaymentOrderAdmin(ImportExportMixin, admin.ModelAdmin):
-#     list_display = (
-#         'id', 'project', 'pay_name', 'pay_sort', 'pay_code', 'pay_time',
-#         'pay_ratio', 'alias_name', 'is_pm_cost', 'pay_due_date', 'extra_due_date')
-#     search_fields = ('pay_name', 'alias_name',)
-#     list_editable = ('alias_name', 'is_pm_cost', 'pay_due_date', 'extra_due_date')
-#     list_display_links = ('project', 'pay_name',)
-#
-#
-# class DownPaymentAdmin(ImportExportMixin, admin.ModelAdmin):
-#     list_display = ('id', 'project', 'order_group', 'unit_type', 'number_payments', 'payment_amount')
-#     list_display_links = ('project', 'order_group', 'unit_type')
-#     list_editable = ('number_payments', 'payment_amount')
-#     list_filter = ('order_group', 'unit_type')
-#
-#
-# class OverDueRuleAdmin(ImportExportMixin, admin.ModelAdmin):
-#     list_display = ('id', 'project', '__str__', 'term_start', 'term_end', 'rate_year')
-#     list_display_links = ('__str__',)
-#     list_editable = ('term_start', 'term_end', 'rate_year')
-#     list_filter = ('project',)
-
-
 admin.site.register(CompanyBankAccount, CompanyBankAccountAdmin)
 admin.site.register(ProjectBankAccount, ProjectBankAccountAdmin)
 admin.site.register(CashBook, CashBookAdmin)
 admin.site.register(ProjectCashBook, ProjectCashBookAdmin)
-
-# admin.site.register(SalesPriceByGT, SalesPriceByGTAdmin)
-# admin.site.register(InstallmentPaymentOrder, InstallmentPaymentOrderAdmin)
-# admin.site.register(DownPayment, DownPaymentAdmin)
-# admin.site.register(OverDueRule, OverDueRuleAdmin)
