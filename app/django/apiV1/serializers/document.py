89fae0e98dcca7fa30ada548a263c2dbde038538
Austin-Kho <kori.susie@gmail.com>
2023-03-27 23:53:15 +0900

    lawsuitCase api update

--- app/django/apiV1/serializers/document.py
+++ app/django/apiV1/serializers/document.py
@@ -36,9 +36,10 @@ class LawSuitCaseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = LawsuitCase
-        fields = ('pk', 'project', 'proj_name', 'sort', 'sort_desc', 'level', 'level_desc', 'related_case',
-                  'related_case_name', 'court', 'court_desc', 'other_agency', 'case_number', 'case_name',
-                  'plaintiff', 'defendant', 'related_debtor', 'case_start_date', 'summary', 'user', 'created')
+        fields = ('pk', 'company', 'project', 'proj_name', 'sort', 'sort_desc', 'level',
+                  'level_desc', 'related_case', 'related_case_name', 'court', 'court_desc',
+                  'other_agency', 'case_number', 'case_name', 'plaintiff', 'defendant',
+                  'related_debtor', 'case_start_date', 'summary', 'user', 'created')
 
 
 class SimpleLawSuitCaseSerializer(serializers.ModelSerializer):
