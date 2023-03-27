eb2a0165667fb7a4dfbfe1b9a4b5c38e1cfe7d8d
Austin-Kho <kori.susie@gmail.com>
2023-03-27 22:52:49 +0900

    type error debug

--- app/vue/src/views/comDocs/LawsuitCase/components/CaseForm.vue
+++ app/vue/src/views/comDocs/LawsuitCase/components/CaseForm.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-import { ref, reactive, computed, onBeforeMount, watch } from 'vue'
-import { onBeforeRouteLeave, useRoute } from 'vue-router'
-import { useDocument } from '@/store/pinia/document'
-import { SuitCase } from '@/store/types/document'
-import { write_company_docs } from '@/utils/pageAuth'
-import { dateFormat } from '@/utils/baseMixins'
-import { courtChoices } from '@/views/comDocs/LawsuitCase/components/court'
+import {ref, reactive, computed, onBeforeMount, watch} from 'vue'
+import {onBeforeRouteLeave, useRoute} from 'vue-router'
+import {useDocument} from '@/store/pinia/document'
+import {SuitCase} from '@/store/types/document'
+import {write_company_docs} from '@/utils/pageAuth'
+import {dateFormat} from '@/utils/baseMixins'
+import {courtChoices} from '@/views/comDocs/LawsuitCase/components/court'
 import Multiselect from '@vueform/multiselect'
 import DatePicker from '@/components/DatePicker/index.vue'
 import ConfirmModal from '@/components/Modals/ConfirmModal.vue'
@@ -19,6 +19,7 @@ const confirmModal = ref()
 
 const form = reactive<SuitCase>({
   pk: null,
+  company: null,
   project: null,
   sort: '',
   level: '',
@@ -63,7 +64,7 @@ const formsCheck = computed(() => {
 })
 
 const sortName = computed(() =>
-  suitcase.value && suitcase.value.project ? suitcase.value.proj_name : '본사',
+    suitcase.value && suitcase.value.project ? suitcase.value.proj_name : '본사',
 )
 
 const fetchSuitCase = (pk: number) => documentStore.fetchSuitCase(pk)
@@ -84,7 +85,7 @@ const onSubmit = (event: Event) => {
 }
 
 const modalAction = () => {
-  emit('on-submit', { ...form })
+  emit('on-submit', {...form})
   validated.value = false
   confirmModal.value.close()
 }
@@ -132,20 +133,20 @@ onBeforeRouteLeave(() => {
     <CCol>
       <h5>
         {{ sortName }}
-        <v-icon icon="mdi-chevron-double-right" size="xs" />
+        <v-icon icon="mdi-chevron-double-right" size="xs"/>
         소송 사건
       </h5>
     </CCol>
   </CRow>
 
-  <hr />
+  <hr/>
 
   <CForm
-    enctype="multipart/form-data"
-    class="needs-validation"
-    novalidate
-    :validated="validated"
-    @submit.prevent="onSubmit"
+      enctype="multipart/form-data"
+      class="needs-validation"
+      novalidate
+      :validated="validated"
+      @submit.prevent="onSubmit"
   >
     <CRow class="mb-3">
       <CFormLabel for="sort" class="col-md-2 col-form-label">유형</CFormLabel>
@@ -179,13 +180,13 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <Multiselect
-          v-model="form.related_case"
-          :options="getSuitCase"
-          placeholder="관련 사건"
-          autocomplete="label"
-          :classes="{ search: 'form-control multiselect-search' }"
-          :add-option-on="['enter' | 'tab']"
-          searchable
+            v-model="form.related_case"
+            :options="getSuitCase"
+            placeholder="관련 사건"
+            autocomplete="label"
+            :classes="{ search: 'form-control multiselect-search' }"
+            :add-option-on="['enter' | 'tab']"
+            searchable
         />
         <small class="text-blue-grey-lighten-2">
           본안 사건인 경우 원심 사건, 신청/집행 사건인 경우 관련 본안 사건 지정
@@ -199,14 +200,14 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <Multiselect
-          v-model="form.court"
-          :options="courtChoices"
-          placeholder="법원 선택"
-          autocomplete="label"
-          :classes="{ search: 'form-control multiselect-search' }"
-          :attrs="form.court || form.other_agency ? {} : { required: true }"
-          :add-option-on="['enter' | 'tab']"
-          searchable
+            v-model="form.court"
+            :options="courtChoices"
+            placeholder="법원 선택"
+            autocomplete="label"
+            :classes="{ search: 'form-control multiselect-search' }"
+            :attrs="form.court || form.other_agency ? {} : { required: true }"
+            :add-option-on="['enter' | 'tab']"
+            searchable
         />
       </CCol>
 
@@ -215,9 +216,9 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="other_agency"
-          v-model="form.other_agency"
-          placeholder="기타 처리기관"
+            id="other_agency"
+            v-model="form.other_agency"
+            placeholder="기타 처리기관"
         />
         <small class="text-blue-grey-lighten-2">
           사건 유형이 기소 전 형사 사건인 경우 해당 수사기관을 기재
@@ -231,10 +232,10 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="case_number"
-          v-model="form.case_number"
-          placeholder="사건번호"
-          required
+            id="case_number"
+            v-model="form.case_number"
+            placeholder="사건번호"
+            required
         />
       </CCol>
 
@@ -243,10 +244,10 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="case_name"
-          v-model="form.case_name"
-          placeholder="사건명"
-          required
+            id="case_name"
+            v-model="form.case_name"
+            placeholder="사건명"
+            required
         />
       </CCol>
     </CRow>
@@ -257,10 +258,10 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="plaintiff"
-          v-model="form.plaintiff"
-          placeholder="원고(신청인)"
-          required
+            id="plaintiff"
+            v-model="form.plaintiff"
+            placeholder="원고(신청인)"
+            required
         />
       </CCol>
 
@@ -269,10 +270,10 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="defendant"
-          v-model="form.defendant"
-          placeholder="피고(피신청인)"
-          required
+            id="defendant"
+            v-model="form.defendant"
+            placeholder="피고(피신청인)"
+            required
         />
       </CCol>
     </CRow>
@@ -283,9 +284,9 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <CFormInput
-          id="related_debtor"
-          v-model="form.related_debtor"
-          placeholder="제3채무자"
+            id="related_debtor"
+            v-model="form.related_debtor"
+            placeholder="제3채무자"
         />
       </CCol>
 
@@ -294,9 +295,9 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol md="4">
         <DatePicker
-          id="case_start_date"
-          v-model="form.case_start_date"
-          placeholder="사건개시(결정)일"
+            id="case_start_date"
+            v-model="form.case_start_date"
+            placeholder="사건개시(결정)일"
         />
       </CCol>
     </CRow>
@@ -307,10 +308,10 @@ onBeforeRouteLeave(() => {
       </CFormLabel>
       <CCol>
         <CFormTextarea
-          id="summary"
-          v-model="form.summary"
-          rows="4"
-          placeholder="개요 및 경과"
+            id="summary"
+            v-model="form.summary"
+            rows="4"
+            placeholder="개요 및 경과"
         />
       </CCol>
     </CRow>
@@ -318,15 +319,15 @@ onBeforeRouteLeave(() => {
     <CRow>
       <CCol class="text-right">
         <CButton
-          color="light"
-          @click="$router.push({ name: '본사 소송 사건' })"
+            color="light"
+            @click="$router.push({ name: '본사 소송 사건' })"
         >
           목록으로
         </CButton>
         <CButton
-          v-if="route.params.caseId"
-          color="light"
-          @click="$router.go(-1)"
+            v-if="route.params.caseId"
+            color="light"
+            @click="$router.go(-1)"
         >
           뒤로
         </CButton>
@@ -353,5 +354,5 @@ onBeforeRouteLeave(() => {
     </template>
   </ConfirmModal>
 
-  <AlertModal ref="alertModal" />
+  <AlertModal ref="alertModal"/>
 </template>
