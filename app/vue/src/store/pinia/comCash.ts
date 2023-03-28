6cb6e04ead59f26e1145c887bd457db1405b0b26
Austin-Kho <kori.susie@gmail.com>
2023-03-28 21:47:21 +0900

    comCash accd3 patch update

--- app/vue/src/store/pinia/comCash.ts
+++ app/vue/src/store/pinia/comCash.ts
@@ -104,7 +104,7 @@ export const useComCash = defineStore('comCash', () => {
     const uD1 = d1 ? `&d2__d1=${d1}` : ''
     const uD2 = d2 ? `&d2=${d2}` : ''
     return api
-      .get(`/account-depth3/?${uSort}${uD1}${uD2}`)
+      .get(`/account-depth3/?${uSort}${uD1}${uD2}&is_hide=false`)
       .then(res => (formAccD3List.value = res.data.results))
       .catch(err => errorHandle(err.response.data))
   }
