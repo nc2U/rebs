246bf1a2bd08c54dedd93bc4db44cc00c9b63c4a
Austin-Kho <kori.susie@gmail.com>
2023-03-28 20:23:45 +0900

    rebs admin update

--- app/django/rebs/admin.py
+++ app/django/rebs/admin.py
@@ -37,8 +37,9 @@ class AccountSubD2Admin(ImportExportMixin, admin.ModelAdmin):
 
 
 class AccountSubD3Admin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ('id', 'd2', 'name', 'code', 'description', 'is_special')
+    list_display = ('id', 'd2', 'name', 'code', 'description', 'is_hide', 'is_special')
     list_display_links = ('name',)
+    list_editable = ('is_hide', 'is_special')
     search_fields = ('name', 'description')
     list_filter = ('d2__d1', 'd2')
 
