7828b125e1da3c866a19c1a8f9f76ad3ab6c01a5
Austin-Kho <kori.susie@gmail.com>
2023-04-05 23:42:20 +0900

    setup page layout update ing

--- app/django/accounts/urls.py
+++ app/django/accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from django.views.generic import RedirectView
-from .views import superuser_check, create_superuser, create_company
+from .views import superuser_check, create_superuser, create_company, create_project
 
 app_name = 'install'
 
@@ -8,6 +7,7 @@ urlpatterns = [
     path('', superuser_check),
     path('create/superuser/', create_superuser, name='create_superuser'),
     path('create/company/', create_company, name='create_company'),
+    path('create/project/', create_project, name='create_project'),
     # path('', include('django.contrib.auth.urls')),
     # path('register/', UserCreateView.as_view(), name='register'),
     # path('register/done/', UserCreateDoneTV.as_view(), name='register_done'),
--- app/django/accounts/views.py
+++ app/django/accounts/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from .models import User
+from company.models import Company
+from project.models import Project
 
 from .forms import UserCreationForm
 
@@ -41,12 +43,42 @@ def create_superuser(request):
 
 def create_company(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        User.objects.create_superuser(username=username,
-                                      email=email,
-                                      password=password)
-        return redirect('/')
+        name = request.POST.get('name')
+        tax_number = request.POST.get('tax_number')
+        ceo = request.POST.get('ceo')
+        org_number = request.POST.get('org_number')
+        Company.objects.create(name=name,
+                               tax_number=tax_number,
+                               ceo=ceo,
+                               org_number=org_number)
+        return redirect('/install/create/project/')
     else:
         return render(request, 'install/create_company.html')
+
+
+def create_project(request):
+    if request.method == 'POST':
+        company = request.POST.get('company')
+        name = request.POST.get('name')
+        kind = request.POST.get('kind')
+        start_year = request.POST.get('start_year')
+        local_zipcode = request.POST.get('local_zipcode')
+        local_address1 = request.POST.get('local_address1')
+        local_address2 = request.POST.get('local_address2')
+        local_address3 = request.POST.get('local_address3')
+        area_usage = request.POST.get('area_usage')
+        build_size = request.POST.get('build_size')
+        Project.objects.create(company=company,
+                               name=name,
+                               kind=kind,
+                               start_year=start_year,
+                               local_zipcode=local_zipcode,
+                               local_address1=local_address1,
+                               local_address2=local_address2,
+                               local_address3=local_address3,
+                               area_usage=area_usage,
+                               build_size=build_size)
+        return redirect('/')
+    else:
+        companies = Company.objects.all()
+        return render(request, 'install/create_project.html', {'companies': companies})
