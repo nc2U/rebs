b527d851ecaa28bfa1a9fa060c5c5eed0e6d5ecd
Austin-Kho <kori.susie@gmail.com>
2023-04-09 17:34:38 +0900

    router update

--- app/vue/src/router/index.ts
+++ app/vue/src/router/index.ts
@@ -1,16 +1,23 @@
+import { computed } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { start, close } from '@/utils/nprogress'
 import routes from '@/router/routes'
 import store from '@/store'
+import { useAccount } from '@/store/pinia/account'
+
+const isAuth = computed(() => useAccount().isAuthorized)
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   start()
   store.commit('startSpinner')
+  if (!isAuth.value && to.name !== 'Login') {
+    return { name: 'Login', query: { redirect: to.fullPath } }
+  }
   next()
 })
 
