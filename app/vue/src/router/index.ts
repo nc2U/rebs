2be080e509c6a07cc6e2812d2060a605ad1aa53f
Austin-Kho <kori.susie@gmail.com>
2023-07-02 19:36:35 +0900

    router index update

--- app/vue/src/router/index.ts
+++ app/vue/src/router/index.ts
@@ -13,12 +13,14 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  start()
   // store.commit('startSpinner')
   const token = await accessToken.value
   if (to.matched.some(r => r.meta.auth))
     if (!token) next({ name: 'Login', query: { redirect: to.fullPath } })
-    else next()
+    else {
+      next()
+      start()
+    }
   else next()
 })
 
