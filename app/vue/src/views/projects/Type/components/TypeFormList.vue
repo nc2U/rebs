1a9febe9cbd27545a10b5b0fc50c897866458f4f
Austin-Kho <kori.susie@gmail.com>
2023-07-05 22:19:39 +0900

    api update

--- app/vue/src/views/projects/Type/components/TypeFormList.vue
+++ app/vue/src/views/projects/Type/components/TypeFormList.vue
@@ -52,7 +52,7 @@ const onDeleteType = (pk: number) => emit('on-delete', pk)
 
     <CTableBody v-else>
       <CTableRow>
-        <CTableDataCell colspan="8" class="text-center p-5 text-danger">
+        <CTableDataCell colspan="9" class="text-center p-5 text-danger">
           등록된 데이터가 없습니다.
         </CTableDataCell>
       </CTableRow>
