aa2bca07fe6da0f299930e7d82876fd33ba948fd
Austin-Kho <kori.susie@gmail.com>
2023-07-20 23:22:38 +0900

    @vue/eslint-config-prettier version upgrade

--- app/vue/.eslintrc.js
+++ app/vue/.eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:prettier/recommended',
     'plugin:vue/vue3-recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
