1b43a6ea58d7e69eacf2a3a57141a6683da58e84
Austin Kho <kori.susie@gmail.com>
2023-08-12 11:33:11 +0900

    jest config update

--- app/vue/tests/unit/example.spec.ts
+++ app/vue/tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import NotFound from '@/components/NotFound.vue'
 
 describe('NotFound.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders NotFound component', () => {
     const msg = "But if you don't change your direction"
     const wrapper = mount(NotFound)
     expect(wrapper.text()).toMatch(msg)
