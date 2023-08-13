cd1f3bd5932edd5fd75a4ce1ff0089daa79ceee3
Austin Kho <kori.susie@gmail.com>
2023-08-14 01:04:46 +0900

    router update

--- app/vue/src/router/index.ts
+++ app/vue/src/router/index.ts
@@ -1,10 +1,10 @@
 import { computed } from 'vue'
 import { useAccount } from '@/store/pinia/account'
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from './routes'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from '@/router/routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
