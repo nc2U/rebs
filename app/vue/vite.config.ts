8bf33f021eadded0944957167180d7fffd0b43e8
Austin Kho <kori.susie@gmail.com>
2023-08-15 00:30:23 +0900

    vite.config update

--- app/vue/vite.config.ts
+++ app/vue/vite.config.ts
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/static/dist' : '/',
   build: {
     outDir: '../django/static/dist',
     emptyOutDir: true,
