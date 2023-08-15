be35929b29f5fd0e68ff5b5bd6c2981a8e394294
Austin Kho <kori.susie@gmail.com>
2023-08-15 19:56:23 +0900

    vite config update

--- app/vue/vite.config.ts
+++ app/vue/vite.config.ts
@@ -1,9 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
 import vuetify from 'vite-plugin-vuetify'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,13 +18,13 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
-    tsconfigPaths(),
   ],
   define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    // extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
   server: {
     proxy: {
