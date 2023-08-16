311a79ba2ec3d66551b2fcfee81db480fe89fdee
austin kho <kori.susie@gmail.com>
2023-08-16 17:53:36 +0900

    company test update

--- app/vue/src/views/settings/Company/__tests__/Company.spec.ts
+++ app/vue/src/views/settings/Company/__tests__/Company.spec.ts
@@ -3,6 +3,8 @@ import { shallowMount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 
 import Company from '../Index.vue'
+import CompanyDetail from '../components/CompanyDetail.vue'
+import CompanyForm from '../components/CompanyForm.vue'
 
 describe('Company app', () => {
   it('Company header title check', () => {
@@ -15,5 +17,51 @@ describe('Company app', () => {
     expect(wrapper.find('content-header-stub').attributes('navmenu')).toBe(
       '회사 정보 관리,권한 설정 관리,프로필 관리',
     )
+  })
+
+  const company = {
+    pk: 1,
+    name: '코리아 주식회사',
+    ceo: '홍길동',
+    tax_number: '1234',
+    org_number: '5678',
+    business_cond: 'abc',
+    business_even: 'def',
+    es_date: '2020-01-01',
+    op_date: '2020-01-02',
+    zipcode: '12345',
+    address1: '주소1',
+    address2: '주소2',
+    address3: '주소3',
+  }
+
+  it('Company Detail company date check...', () => {
+    const wrapper = shallowMount(CompanyDetail, {
+      props: {
+        company,
+      },
+    })
+    expect(wrapper.html()).toContain('코리아 주식회사')
+    expect(wrapper.html()).toContain('홍길동')
+    expect(wrapper.html()).toContain('주소1')
+    expect(wrapper.html()).toContain('주소2')
+    expect(wrapper.html()).toContain('주소3')
+  })
+
+  it('Company Form check', () => {
+    const wrapper = shallowMount(CompanyForm, {
+      props: {
+        company,
+      },
+    })
+    const inputs = wrapper.findAll('cforminput')
+    expect(inputs[0].attributes('modelvalue')).toBe(company.name)
+    expect(inputs[1].attributes('modelvalue')).toBe(company.ceo)
+    expect(inputs[2].attributes('modelvalue')).toBe(company.business_cond)
+    expect(inputs[3].attributes('modelvalue')).toBe(company.business_even)
+    expect(inputs[4].attributes('modelvalue')).toBe(company.zipcode)
+    expect(inputs[5].attributes('modelvalue')).toBe(company.address1)
+    expect(inputs[6].attributes('modelvalue')).toBe(company.address2)
+    expect(inputs[7].attributes('modelvalue')).toBe(company.address3)
   })
 })
