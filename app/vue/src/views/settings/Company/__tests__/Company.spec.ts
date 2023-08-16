dc28102b5083936c2df2cb973a04470e4bc3e425
austin kho <kori.susie@gmail.com>
2023-08-16 14:44:27 +0900

    company test update

--- app/vue/src/views/settings/Company/__tests__/Company.spec.ts
+++ app/vue/src/views/settings/Company/__tests__/Company.spec.ts
@@ -5,12 +5,15 @@ import { createTestingPinia } from '@pinia/testing'
 import Company from '../Index.vue'
 
 describe('Company app', () => {
-  it('Company Index', () => {
+  it('Company header title check', () => {
     const wrapper = shallowMount(Company, {
       global: {
         plugins: [createTestingPinia()],
       },
     })
     expect(wrapper.find('content-header-stub').attributes('pagetitle')).toBe('환경 설정')
+    expect(wrapper.find('content-header-stub').attributes('navmenu')).toBe(
+      '회사 정보 관리,권한 설정 관리,프로필 관리',
+    )
   })
 })
