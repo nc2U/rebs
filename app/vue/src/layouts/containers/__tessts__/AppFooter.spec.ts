4a5b1d2f9e86b3c9839aed6eab8ed2e2af1c9fc9
Austin Kho <kori.susie@gmail.com>
2023-09-03 17:38:29 +0900

    AppFooter update

---
+++ app/vue/src/layouts/containers/__tessts__/AppFooter.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import CoreuiVue from '@coreui/vue'
+
+import AppFooter from '@/layouts/containers/AppFooter.vue'
+
+describe('AppFooter Component Test', () => {
+  it('should ', () => {
+    const wrapper = mount(AppFooter, {
+      global: {
+        plugins: [CoreuiVue],
+      },
+    })
+
+    expect(wrapper.find('.footer').exists()).toBeTruthy()
+    expect(wrapper.find('.footer').findAll('div')).toHaveLength(2)
+  })
+})
