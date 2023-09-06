0cb4d53f41d050df2c3615e0eefaa80168b3936d
Austin Kho <kori.susie@gmail.com>
2023-09-06 18:48:52 +0900

    file updoad testing

--- app/vue/src/components/__tests__/FileUpload.spec.ts
+++ app/vue/src/components/__tests__/FileUpload.spec.ts
@@ -1,30 +1,12 @@
-import { describe, it, expect } from 'vitest'
-import { mount, shallowMount } from '@vue/test-utils'
-import { createTestingPinia } from '@pinia/testing'
-import { createVuetify } from 'vuetify'
-import CoreuiVue from '@coreui/vue'
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
 
 import FileUpload from '../FileUpload.vue'
 
-const vuetify = createVuetify()
-
-describe('App Component Test', () => {
+describe('FileUpload Component Test', () => {
   it('should ', () => {
-    const wrapper = mount(App, {
-      global: {
-        plugins: [createTestingPinia(), vuetify, CoreuiVue],
-        mocks: {},
-        provide: {},
-        components: {},
-        directives: {},
-        stubs: [],
-      },
-      attrs: {},
-      props: {},
-      slots: {},
-      shallow: true,
-    })
+    const wrapper = mount(FileUpload)
 
-    console.log(wrapper.html())
+    expect(wrapper.find('input[type=file]').exists()).toBeTruthy()
   })
 })
