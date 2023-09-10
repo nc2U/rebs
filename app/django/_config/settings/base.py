984ceb92ed99abfcb8af6eb5862c422b0c34c504
Austin Kho <kori.susie@gmail.com>
2023-09-11 02:58:04 +0900

    django settings repair

--- app/django/_config/settings/base.py
+++ app/django/_config/settings/base.py
@@ -95,7 +95,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    # 'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = '_config.urls'
