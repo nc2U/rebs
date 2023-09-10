bab5344e76b0ebd1ccf680ab2a62160f91064e7d
Austin Kho <kori.susie@gmail.com>
2023-09-11 02:27:51 +0900

    dirchk.sh update

--- app/django/_config/settings/base.py
+++ app/django/_config/settings/base.py
@@ -95,6 +95,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = '_config.urls'
--- volume/backups/dir_chk.sh
+++ volume/backups/dir_chk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mysql_name="./volume/mysql/"
 if ! test -d $mysql_name
-then mkdir $mysql_name
+then mkdir "./volume/mysql/"
 
 media_name="./app/django/media/"
 if ! test -d $media_name
-then mkdir $media_name
+then mkdir "./app/django/media/"
 fi
