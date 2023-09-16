b41c52587898291348f86d7f26e770e8d0288682
Austin Kho <kori.susie@gmail.com>
2023-09-16 15:16:57 +0900

    proCash d2 & d3 logic update

--- app/vue/src/views/proCash/Imprest/Index.vue
+++ app/vue/src/views/proCash/Imprest/Index.vue
@@ -19,12 +19,12 @@ import { cutString } from '@/utils/baseMixins'
 
 const listControl = ref()
 
-const bankFees = ref([11, 46]) // 은행수수료 d2(id), d3(id)
-const transferD3 = ref([71, 72]) // 대체 출금(id), 입금(id)
-const cancelD3 = ref([73, 74]) // 취소 출금(id), 입금(id)
+const bankFees = ref([14, 53]) // 은행수수료 d2(id), d3(id)
+const transferD3 = ref([65, 66]) // 대체 출금(id), 입금(id)
+const cancelD3 = ref([67, 68]) // 취소 출금(id), 입금(id)
 
-provide('transfers', [14, 71]) // 대체 d2(id), d3(id)
-provide('cancels', [15, 73]) // 취소 d2(id), d3(id)
+provide('transfers', [17, 65]) // 대체 d2(id), d3(id)
+provide('cancels', [18, 67]) // 취소 d2(id), d3(id)
 
 const dataFilter = ref<CashBookFilter>({
   page: 1,
