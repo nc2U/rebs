4e668911d59b9021c3566d43b354ba45a5eb0639
austin kho <kori.susie@gmail.com>
2023-09-22 17:05:01 +0900

    docs backend update

--- app/django/apiV1/serializers/document.py
+++ app/django/apiV1/serializers/document.py
@@ -156,23 +156,19 @@ class PostSerializer(serializers.ModelSerializer):
         if old_files:
             cng_pks = self.initial_data.getlist('cngPks')
             cng_files = self.initial_data.getlist('cngFiles')
-            cng_maps = [(pk, cng_files[i]) for i, pk in cng_pks]  # 아래 로직에서 파일 변경 로직 작성 요망
+            cng_maps = [(pk, cng_files[i]) for i, pk in enumerate(cng_pks)]
+
             for json_file in old_files:
                 file = json.loads(json_file)
-                pk = file.get('pk')
-                file_object = File.objects.get(pk=pk)
-                for m in cng_maps:
-                    if pk in m:
-                        file_object.file = m[1]
-                        file_object.save()
-                # nf = [nf.get('file') for nf in cng_files if nf.get('pk') == file.get('pk')]
-                # if nf:
-                #     file['newFile'] = nf[0]
+                file_object = File.objects.get(pk=file.get('pk'))
+
                 if file.get('del'):
                     file_object.delete()
-                elif file.get('newFile'):
-                    file_object.file = file.get('newFile')
-                    file_object.save()
+
+                for cng_map in cng_maps:
+                    if int(file.get('pk')) == int(cng_map[0]):
+                        file_object.file = cng_map[1]
+                        file_object.save()
 
         new_files = self.initial_data.getlist('newFiles')
         if new_files:
