5332563215d0f4a3233ef78318d4a0ddadbd7014
austin kho <kori.susie@gmail.com>
2023-09-25 15:48:36 +0900

    suitcase model update

--- app/django/document/models.py
+++ app/django/document/models.py
@@ -53,9 +53,9 @@ class LawsuitCase(models.Model):
     company = models.ForeignKey('company.Company', on_delete=models.CASCADE, verbose_name='회사')
     project = models.ForeignKey('project.Project', on_delete=models.SET_NULL, null=True, blank=True,
                                 verbose_name='프로젝트')
-    SORT_CHOICES = (('1', '민사'), ('2', '형사'), ('3', '행정'), ('4', '가사'), ('5', '신청/집행'))
+    SORT_CHOICES = (('1', '민사'), ('2', '형사'), ('3', '행정'), ('4', '신청'), ('5', '집행'))
     sort = models.CharField('유형', max_length=1, choices=SORT_CHOICES)
-    LEVEL_CHOICES = (('1', '1심'), ('2', '2심'), ('3', '3심'), ('4', '수사'), ('5', '신청/집행'))
+    LEVEL_CHOICES = (('1', '1심'), ('2', '2심'), ('3', '3심'), ('4', '고소/수사'), ('5', '신청'), ('6', '항고/이의'), ('7', '집행'))
     level = models.CharField('심급', max_length=1, choices=LEVEL_CHOICES, blank=True)
     related_case = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='관련사건',
                                      help_text='본안 사건인 경우 원심 사건, 신청/집행 사건인 경우 관련 본안 사건 지정')
