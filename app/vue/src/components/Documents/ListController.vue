79a0540aff5c32eec1413b97e9dc34dfcbfc8e98
Austin Kho <kori.susie@gmail.com>
2023-10-04 09:31:31 +0900

    post ordering remove

--- app/vue/src/components/Documents/ListController.vue
+++ app/vue/src/components/Documents/ListController.vue
@@ -93,16 +93,16 @@ onBeforeMount(() => {
             </CFormSelect>
           </CCol>
 
-          <CCol md="6" lg="5" xl="4" class="mb-3">
-            <CFormSelect v-model="form.ordering" @change="listFiltering(1)">
-              <option value="created">작성일자 오름차순</option>
-              <option value="-created">작성일자 내림차순</option>
-              <option value="execution_date">시행일자 오름차순</option>
-              <option value="-execution_date">시행일자 내림차순</option>
-              <option value="-hit">조회수 오름차순</option>
-              <option value="hit">조회수 내림차순</option>
-            </CFormSelect>
-          </CCol>
+          <!--          <CCol md="6" lg="5" xl="4" class="mb-3">-->
+          <!--            <CFormSelect v-model="form.ordering" @change="listFiltering(1)">-->
+          <!--              <option value="created">작성일자 오름차순</option>-->
+          <!--              <option value="-created">작성일자 내림차순</option>-->
+          <!--              <option value="execution_date">시행일자 오름차순</option>-->
+          <!--              <option value="-execution_date">시행일자 내림차순</option>-->
+          <!--              <option value="-hit">조회수 오름차순</option>-->
+          <!--              <option value="hit">조회수 내림차순</option>-->
+          <!--            </CFormSelect>-->
+          <!--          </CCol>-->
         </CRow>
       </CCol>
 
