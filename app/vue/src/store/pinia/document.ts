2dbdec5858893aeb13577c1453293c55cbadc885
austin kho <kori.susie@gmail.com>
2023-10-06 14:26:35 +0900

    post create return logic update ing

--- app/vue/src/store/pinia/document.ts
+++ app/vue/src/store/pinia/document.ts
@@ -217,8 +217,9 @@ export const useDocument = defineStore('document', () => {
       .catch(err => errorHandle(err.response.data))
   }
 
-  const createPost = (payload: { form: FormData } & { isProject?: boolean }) =>
-    api
+  const createPost = async (payload: { form: FormData } & { isProject?: boolean }) => {
+    const is_com: boolean = !payload.isProject
+    return await api
       .post(`/post/`, payload.form, {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
@@ -227,9 +228,12 @@ export const useDocument = defineStore('document', () => {
           company: res.data.company,
           project: res.data.project,
           board: res.data.board,
+          is_com,
+          page: 1,
         }).then(() => message()),
       )
       .catch(err => errorHandle(err.response.data))
+  }
 
   const updatePost = (payload: { pk: number; form: FormData }) =>
     api
