6023e109d9662ffc9a6a377ad73f288a59a52b56
Austin Kho <kori.susie@gmail.com>
2023-11-06 17:44:29 +0900

    rebs pinia update

---
+++ app/vue/src/store/pinia/rebs.ts
@@ -0,0 +1,42 @@
+import api from '@/api'
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { errorHandle } from '@/utils/helper'
+
+export type WiseWord = {
+  pk: number
+  saying_ko: string
+  saying_en: string
+  spoked_by: string
+}
+
+export const useRebs = defineStore('rebs', () => {
+  // states
+  const wiseWordsList = ref<WiseWord[]>([])
+  const wiseWordsCount = ref<number>(0)
+  const wiseWord = ref<WiseWord | null>(null)
+
+  // actions
+  const fetchWiseWordsList = () =>
+    api
+      .get('wise-say')
+      .then(res => {
+        wiseWordsList.value = res.data.results
+        wiseWordsCount.value = res.data.count
+      })
+      .catch(err => errorHandle(err.response.data))
+
+  const fetchWiseWord = (pk: number) =>
+    api
+      .get(`/wise-say/${pk}`)
+      .then(res => (wiseWord.value = res.data))
+      .catch(err => errorHandle(err.response.data))
+
+  return {
+    wiseWordsList,
+    wiseWordsCount,
+    wiseWord,
+    fetchWiseWordsList,
+    fetchWiseWord,
+  }
+})
