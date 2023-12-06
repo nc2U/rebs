d33c9769b55c659021d44079b5bde67d40f98946
austin kho <kori.susie@gmail.com>
2023-12-06 18:31:49 +0900

    notice index route name update

--- app/vue/src/views/_Dashboard/components/NoticeBoard/Index.vue
+++ app/vue/src/views/_Dashboard/components/NoticeBoard/Index.vue
@@ -8,7 +8,7 @@ import NoticeView from './components/NoticeView.vue'
 import NoticeForm from '@/views/_Dashboard/components/NoticeBoard/components/NoticeForm.vue'
 
 const route = useRoute() as { name: string }
-const msg = ref('공지 사항')
+const routeName = ref('공지 사항')
 
 const postFilter = ref({
   ordering: '-created',
@@ -21,13 +21,17 @@ const postList = ref([])
   <CContainer fluid>
     <CCard>
       <CCardBody>
-        <h5>{{ msg }}</h5>
+        <h5>{{ routeName }}</h5>
         <hr />
-        <ListController v-if="route.name === '공지 사항'" :post-filter="postFilter" />
-        <CategoryTabs v-if="route.name === '공지 사항'" />
-        <NoticeList v-if="route.name === '공지 사항'" :post-list="postList" />
-        <NoticeView v-else-if="route.name.includes('보기')" view-route="공지 사항" :curr-page="1" />
-        <NoticeForm v-else-if="route.name.includes('작성')" view-route="공지 사항" />
+        <ListController v-if="route.name === routeName" :post-filter="postFilter" />
+        <CategoryTabs v-if="route.name === routeName" />
+        <NoticeList v-if="route.name === routeName" :post-list="postList" />
+        <NoticeView
+          v-else-if="route.name.includes('보기')"
+          :view-route="routeName"
+          :curr-page="1"
+        />
+        <NoticeForm v-else-if="route.name.includes('작성')" :view-route="routeName" />
       </CCardBody>
     </CCard>
   </CContainer>
