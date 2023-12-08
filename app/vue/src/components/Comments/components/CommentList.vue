64ada3676f80fe39a081df37e43af2254fc4af51
Austin Kho <kori.susie@gmail.com>
2023-12-09 01:53:24 +0900

    postView update

--- app/vue/src/components/Comments/components/CommentList.vue
+++ app/vue/src/components/Comments/components/CommentList.vue
@@ -1,30 +1,18 @@
 <script lang="ts" setup="">
 import { ref } from 'vue'
 
-const msg = ref('Hello world')
+const commentLength = ref(0)
 </script>
 
 <template>
-  <div v-if="1 == 1">
-    <h5 class="my-4 ml-4">1 Comments</h5>
-    <ul class="comments mx-5 mb-4">
+  <div v-if="commentLength">
+    <h5 class="my-4 ml-4">{{ commentLength }} Comments</h5>
+    <ul v-for="i in commentLength" :key="i" class="comments mx-5 mb-4">
       <li>
-        <div class="comment-item">
-          <strong>홍길동</strong> <small>2023-12-09</small>
-          <p>댓글에 대한 테스트 컴포넌트입니다.</p>
-        </div>
-      </li>
-
-      <li>
-        <div class="comment-item">
-          <strong>홍길동</strong> <small>2023-12-09</small>
-          <p>댓글에 대한 테스트 컴포넌트입니다.</p>
-        </div>
-      </li>
-
-      <li>
-        <div class="comment-item">
-          <strong>홍길동</strong> <small>2023-12-09</small>
+        <div class="comment-item border-bottom-1">
+          <strong>홍길동</strong>
+          <small class="ml-2">2023-12-09 09:00:00</small>
+          <small class="ml-2">추천 0 비추 0 신고</small>
           <p>댓글에 대한 테스트 컴포넌트입니다.</p>
         </div>
       </li>
