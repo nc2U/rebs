a865f2ecd48ee85ea16127b08cc6217e261a3d8c
Austin Kho <kori.susie@gmail.com>
2023-12-10 20:17:21 +0900

    comment app update ing

--- app/vue/src/components/Comments/Index.vue
+++ app/vue/src/components/Comments/Index.vue
@@ -1,10 +1,22 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import { computed } from 'vue'
+import { useDocument } from '@/store/pinia/document'
 import CommentList from './components/CommentList.vue'
 import CommentForm from './components/CommentForm.vue'
+import type { Comment } from '@/store/types/document'
+
+const props = defineProps({ post: { type: Number, required: true } })
+
+const docStore = useDocument()
+const commentList = computed(() => docStore.commentList)
+
+const onSubmit = (payload: Comment) => {
+  payload.post = props.post
+  console.log(payload)
+}
 </script>
 
 <template>
-  <CommentList />
-  <CommentForm />
+  <CommentList :comment-list="commentList" />
+  <CommentForm @on-submit="onSubmit" />
 </template>
--- app/vue/src/components/Comments/components/CommentList.vue
+++ app/vue/src/components/Comments/components/CommentList.vue
@@ -2,14 +2,18 @@
 import { ref } from 'vue'
 import Comment from './Comment.vue'
 
+defineProps({
+  commentList: { type: Object, default: () => [] },
+})
+
 const commentLength = ref(3)
 </script>
 
 <template>
-  <div v-if="commentLength">
-    <h5 class="my-4 ml-4">{{ commentLength }} Comments</h5>
+  <div v-if="commentList?.length">
+    <h5 class="my-4 ml-4">{{ commentList?.length }} Comments</h5>
     <ul class="comments mx-5 mb-4">
-      <li v-for="i in commentLength" :key="i">
+      <li v-for="i in commentList" :key="i">
         <Comment />
       </li>
     </ul>
