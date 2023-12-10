cbeb4c2a6c7c846416fd3221304972cae179d321
Austin Kho <kori.susie@gmail.com>
2023-12-11 01:02:40 +0900

    comment app update ing

--- app/vue/src/components/Comments/components/Comment.vue
+++ app/vue/src/components/Comments/components/Comment.vue
@@ -14,7 +14,7 @@ const userInfo = inject<User>('userInfo')
     <small class="ml-2">2023-12-09 09:00:00</small>
     <small class="ml-2">추천 0 비추 0 신고</small>
     <small class="ml-2">답변</small>
-    <template v-if="userInfo.pk === props.comment?.user.pk">
+    <template v-if="userInfo?.pk === props.comment?.user.pk">
       <!--    해당 본인 작성글이고 댓글에 대댓글이 없을 경우 수정/삭제 활성-->
       <small class="ml-1">수정</small>
       <small class="ml-1">삭제</small>
