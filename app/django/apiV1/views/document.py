53200e87aaf63680d24135e8b070809b13d8b340
Austin Kho <kori.susie@gmail.com>
2023-12-12 21:26:12 +0900

    comment api update

--- app/django/apiV1/views/document.py
+++ app/django/apiV1/views/document.py
@@ -118,11 +118,19 @@ class FileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, IsProjectStaffOrReadOnly)
 
 
+class CommentFilterSet(FilterSet):
+    is_comment = BooleanFilter(field_name='parent', lookup_expr='isnull', label='댓글')
+
+    class Meta:
+        model = Comment
+        fields = ('user', 'post', 'is_comment')
+
+
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.filter(parent=None)
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (permissions.IsAuthenticated, IsProjectStaffOrReadOnly)
-    filterset_fields = ('user', 'post')
+    filterset_class = CommentFilterSet
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
