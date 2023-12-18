cfd206ca864de25b127f51134c0b0fa16a71e6e6
Austin Kho <kori.susie@gmail.com>
2023-12-18 18:29:55 +0900

    Calculated component update

--- app/vue/src/views/comCash/Status/components/Calculated.vue
+++ app/vue/src/views/comCash/Status/components/Calculated.vue
@@ -17,15 +17,15 @@ const modalAction = () => {
 
 <template>
   <CAlert :color="isCalculated ? 'success' : 'warning'" class="text-right">
-    <v-btn v-if="isCalculated" color="success" size="x-large" disabled>잔고정산 확인완료</v-btn>
-    <v-btn v-else color="warning" size="x-large" @click="toCalculated">당일잔고 정산확인</v-btn>
+    <v-btn v-if="isCalculated" color="success" size="x-large" disabled> 잔고정산 확인완료</v-btn>
+    <v-btn v-else color="warning" size="x-large" @click="toCalculated"> 당일잔고 정산확인</v-btn>
   </CAlert>
 
   <ConfirmModal ref="refConfirmModal">
     <template #header> 알림</template>
     <template #default>
-      계좌별 잔고 데이터가 실제 계좌 잔고와 일치 하는 경우 확인을 클릭 하십시요. 확인을 클릭한 이후
-      현재 등록된 최종 거래일자 이전의 거래를 수정 및 삭제할 수 없습니다.
+      계좌별 잔고 데이터가 실제 계좌 잔고와 일치 하는지 체크 후 확인을 클릭 하십시요. 확인 후 현재
+      등록된 최종 거래 일자로부터 10일이 경과한 거래 건은 수정 및 삭제할 수 없습니다.
     </template>
     <template #footer>
       <CButton color="warning" @click="modalAction">확인</CButton>
