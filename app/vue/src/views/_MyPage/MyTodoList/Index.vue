6bd80c2e7cc7649b7ddeb403621b36d1bb57b7cb
Austin Kho <kori.susie@gmail.com>
2023-12-19 22:54:29 +0900

    todo-list app update

--- app/vue/src/views/_MyPage/MyTodoList/Index.vue
+++ app/vue/src/views/_MyPage/MyTodoList/Index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
 import { pageTitle, navMenu } from '@/views/_MyPage/_menu/headermixin'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
@@ -12,11 +11,9 @@ import TodoListApp from '@/components/TodoListApp/index.vue'
   <ContentBody>
     <CCardBody>
       <CRow>
-        <CCol class="text-left pt-5" lg="12" xl="4">
+        <CCol class="text-left p-5">
           <TodoListApp />
         </CCol>
-        <CCol lg="12" xl="4" />
-        <CCol lg="12" xl="4" />
       </CRow>
     </CCardBody>
   </ContentBody>
