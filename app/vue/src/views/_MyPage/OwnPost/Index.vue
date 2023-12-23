596a334ab04ad5f56c995b21f2280827e39492e7
Austin Kho <kori.susie@gmail.com>
2023-12-23 18:03:43 +0900

    ownPost update ing

--- app/vue/src/views/_MyPage/OwnPost/Index.vue
+++ app/vue/src/views/_MyPage/OwnPost/Index.vue
@@ -7,21 +7,36 @@ import { type PostFilter, useDocument } from '@/store/pinia/document'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
 import PostList from '@/views/_MyPage/OwnPost/components/PostList.vue'
-import PostView from '@/views/_MyPage/OwnPost/components/PostView.vue'
-import PostForm from '@/views/_MyPage/OwnPost/components/PostForm.vue'
 
 const mainViewName = ref('내 작성글')
 const userInfo = inject<ComputedRef<User>>('userInfo')
 
-const route = useRoute() as { name: string }
+const postFilter = ref<PostFilter>({
+  user: '',
+  search: '',
+  page: 1,
+})
+
+const listFiltering = (payload: PostFilter) => {
+  postFilter.value.user = payload.user
+  postFilter.value.search = payload.search
+}
 
 const docStore = useDocument()
-const post = computed(() => docStore.post)
 const postList = computed(() => docStore.postList)
 
+const fetchPost = (pk: number) => docStore.fetchPost(pk)
 const fetchPostList = (payload: PostFilter) => docStore.fetchPostList(payload)
 
-onBeforeMount(() => fetchPostList({ user: userInfo?.value.pk }))
+const route = useRoute()
+
+const dataSetup = (pk: number, postId?: string | string[]) => {
+  postFilter.value.user = pk
+  fetchPostList(postFilter.value)
+  if (postId) fetchPost(Number(postId))
+}
+
+onBeforeMount(() => dataSetup(userInfo?.value.pk as number, route.params?.postId))
 </script>
 
 <template>
@@ -29,21 +44,7 @@ onBeforeMount(() => fetchPostList({ user: userInfo?.value.pk }))
 
   <ContentBody>
     <CCardBody class="pb-5">
-      <div v-if="route.name === mainViewName" class="pt-3">
-        <PostList :post-list="postList" :view-route="mainViewName" />
-      </div>
-
-      <div v-else-if="route.name.includes('보기')">
-        <PostView :curr-page="1" :view-route="mainViewName" />
-      </div>
-
-      <div v-else-if="route.name.includes('작성')">
-        <PostForm :view-route="mainViewName" :category-list="[]" />
-      </div>
-
-      <div v-else-if="route.name.includes('수정')">
-        <PostForm :view-route="mainViewName" :category-list="[]" />
-      </div>
+      <PostList :post-list="postList" :view-route="mainViewName" />
     </CCardBody>
   </ContentBody>
 </template>
