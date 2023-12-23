45ae56674b109bf50bbd34afab640f37facf6819
Austin Kho <kori.susie@gmail.com>
2023-12-23 16:54:27 +0900

    mypage layout update ing

--- app/vue/src/views/_MyPage/OwnScrap/Index.vue
+++ app/vue/src/views/_MyPage/OwnScrap/Index.vue
@@ -6,9 +6,9 @@ import type { User } from '@/store/types/accounts'
 import { type PostFilter, useDocument } from '@/store/pinia/document'
 import ContentBody from '@/layouts/ContentBody/Index.vue'
 import ContentHeader from '@/layouts/ContentHeader/Index.vue'
-import DocsList from '@/components/Documents/DocsList.vue'
-import DocsView from '@/components/Documents/DocsView.vue'
-import DocsForm from '@/components/Documents/DocsForm.vue'
+import PostList from '@/views/_MyPage/OwnScrap/components/PostList.vue'
+import PostView from '@/views/_MyPage/OwnScrap/components/PostView.vue'
+import PostForm from '@/views/_MyPage/OwnScrap/components/PostForm.vue'
 
 const mainViewName = ref('스크랩')
 const userInfo = inject<ComputedRef<User>>('userInfo')
@@ -30,19 +30,19 @@ onBeforeMount(() => fetchPostList({ user: userInfo?.value.pk }))
   <ContentBody>
     <CCardBody class="pb-5">
       <div v-if="route.name === mainViewName" class="pt-3">
-        <DocsList :post-list="postList" :view-route="mainViewName" />
+        <PostList :post-list="postList" :view-route="mainViewName" />
       </div>
 
       <div v-else-if="route.name.includes('보기')">
-        <DocsView :curr-page="1" :view-route="mainViewName" />
+        <PostView :curr-page="1" :view-route="mainViewName" />
       </div>
 
       <div v-else-if="route.name.includes('작성')">
-        <DocsForm :view-route="mainViewName" :category-list="[]" />
+        <PostForm :view-route="mainViewName" :category-list="[]" />
       </div>
 
       <div v-else-if="route.name.includes('수정')">
-        <DocsForm :view-route="mainViewName" :category-list="[]" />
+        <PostForm :view-route="mainViewName" :category-list="[]" />
       </div>
     </CCardBody>
   </ContentBody>
---
+++ app/vue/src/views/_MyPage/OwnScrap/components/Post.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed, type PropType } from 'vue'
+import type { Post } from '@/store/types/document'
+import { cutString, timeFormat } from '@/utils/baseMixins'
+
+const props = defineProps({
+  post: { type: Object as PropType<Post>, default: null },
+  viewRoute: { type: String, required: true },
+  isLawsuit: { type: Boolean, default: false },
+})
+
+const sortName = computed(() => props.post?.proj_name || '본사 문서')
+const sortColor = computed(() => (props.post?.project ? 'success' : 'info'))
+</script>
+
+<template>
+  <CTableRow v-if="post" class="text-center">
+    <CTableDataCell>{{ post.pk }}</CTableDataCell>
+    <CTableDataCell class="text-left">
+      <v-badge :color="sortColor" :content="sortName" offset-x="-5" offset-y="-7" />
+    </CTableDataCell>
+    <CTableDataCell>{{ post.execution_date }}</CTableDataCell>
+    <CTableDataCell v-if="isLawsuit" class="text-left">
+      {{ cutString(post.lawsuit_name ?? '', 26) }}
+    </CTableDataCell>
+    <CTableDataCell class="text-left">
+      <router-link :to="{ name: `${viewRoute} - 보기`, params: { postId: post.pk } }">
+        {{ cutString(post.title, 32) }}
+      </router-link>
+      <CBadge v-if="post.is_new" color="warning" size="sm" class="ml-2">new</CBadge>
+    </CTableDataCell>
+    <CTableDataCell>{{ post.user?.username }}</CTableDataCell>
+    <CTableDataCell>{{ timeFormat(post.created ?? '') }}</CTableDataCell>
+    <CTableDataCell>{{ post.hit }}</CTableDataCell>
+  </CTableRow>
+</template>
