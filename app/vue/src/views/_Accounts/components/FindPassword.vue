1f338a0941f67bfe6155c7db949676d0c401bfea
Austin Kho <kori.susie@gmail.com>
2024-01-01 19:13:09 +0900

    accounts app update

--- app/vue/src/views/_Accounts/components/FindPassword.vue
+++ app/vue/src/views/_Accounts/components/FindPassword.vue
@@ -70,7 +70,7 @@ const toLogin = () => emit('to-login')
     <template #header>비밀번호 재설정 확인</template>
     <template #default> 비밀번호 재설정을 위한 이메일을 전송하시겠습니까?</template>
     <template #footer>
-      <CButton color="primary" @click="modalAction"> 확인</CButton>
+      <CButton color="danger" @click="modalAction"> 확인</CButton>
     </template>
   </ConfirmModal>
 </template>
