c2cb8633c4408c7eec527439b05b73b46a7a85fe
Austin Kho <kori.susie@gmail.com>
2024-01-05 20:59:32 +0900

    postView update

--- app/vue/src/components/__tests__/Documents.spec.ts
+++ app/vue/src/components/__tests__/Documents.spec.ts
@@ -1,14 +1,10 @@
-import { describe, it, expect } from 'vitest'
-import { mount, shallowMount } from '@vue/test-utils'
+import { describe, it } from 'vitest'
+import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import { createVuetify } from 'vuetify'
 import CoreuiVue from '@coreui/vue'
 
 import CategoryTabs from '../Documents/CategoryTabs.vue'
-import DocsForm from '../Documents/DocsForm.vue'
-import DocsList from '../Documents/DocsList.vue'
-import DocsView from '../Documents/DocsView.vue'
-import ListController from '../Documents/ListController.vue'
 
 const vuetify = createVuetify()
 
