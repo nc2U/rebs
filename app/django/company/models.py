3d502519ead1f768dd331a3132856ba6429fcc25
austin kho <kori.susie@gmail.com>
2024-02-13 17:56:12 +0900

    company model update ing

--- app/django/company/models.py
+++ app/django/company/models.py
@@ -2,6 +2,7 @@ import hashlib
 from django.db import models
 
 from accounts.models import User
+from project.models import Project
 
 
 class Company(models.Model):
@@ -132,15 +133,56 @@ class Staff(models.Model):
         verbose_name = '06. 직원 정보'
         verbose_name_plural = '06. 직원 정보'
 
+# class Role(models.Model):
+#     name = models.CharField('이름')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Permission(models.Model):
+#     name = models.CharField('이름')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class WorkType(models.Model):
+#     company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='회사')
+#     name = models.CharField('유형', max_length=100)
+#     order = models.PositiveSmallIntegerField('정렬 순서', default=0)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class WorkProject(models.Model):
+#     company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='회사')
+#     title = models.CharField('이름', max_length=100)
+#     desc = models.TextField('설명', null=True, blank=True)
+#     identifier = models.CharField('식별자', max_length=20)
+#     is_public = models.BooleanField('공개여부', default=False)
+#     work_types = models.ManyToManyField(WorkType, verbose_name='작업 유형', blank=True)
+#
+#
+# class WorkCate(models.Model):
+#     company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='회사')
+#     name = models.CharField('범주', max_length=100)
+#     order = models.PositiveSmallIntegerField('정렬 순서', default=0)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
 # class Work(models.Model):
-#     WORK_TYPE = (('1', '통상업무'), ('2', '민원처리'), ('3', '기타작업'))
-#     type = models.CharField('작업유형', max_length=1, choices=WORK_TYPE)
-#     is_secret = models.BooleanField('비공개', default=False)
+#     type = models.ForeignKey(WorkType, on_delete=models.PROTECT, verbose_name='유형')
 #     title = models.CharField(max_length=100, verbose_name='업무 제목')
 #     desc = models.TextField(verbose_name='설명', blank=True, default='')
-#     upper_work = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, verbose_name='상위 업무')
 #     WORK_STATUS = (('0', '신규'), ('1', '진행'), ('2', '해결'), ('3', '의견'), ('4', '완료'), ('5', '거절'))
 #     status = models.CharField('상태', max_length=1, choices=WORK_STATUS, default='0')
+#     upper_work = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, verbose_name='상위 업무')
+#     cate = models.ForeignKey(WorkCate, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='범주')
+#     is_secret = models.BooleanField('비공개', default=False)
 #     WORK_PRIORITY = (('1', '낮음'), ('2', '보통'), ('3', '높음'), ('4', '긴급'), ('5', '즉시'))
 #     priority = models.CharField('우선 순위', max_length=1, choices=WORK_PRIORITY)
 #     worker = models.OneToOneField('accounts.User', on_delete=models.PROTECT, verbose_name='담당자')
