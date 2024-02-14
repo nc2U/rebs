4d737d7d3a22b7bf57be30dab81cf075c6e2a1e1
Austin Kho <kori.susie@gmail.com>
2024-02-15 02:02:47 +0900

    work app update

--- app/django/work/apps.py
+++ app/django/work/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class WorkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'work'
-    verbose_name = '업무 프로젝트 관리'
+    verbose_name = '업무 협업 관리'
