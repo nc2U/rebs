5f3f607a29848031c97b251ab828be4ce774d178
austin kho <kori.susie@gmail.com>
2024-02-16 17:33:44 +0900

    default layout update

---
+++ app/vue/src/views/_WorkManage/_common/ContentHeader/Index.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import HeaderNav from '@/layouts/ContentHeader/components/HeaderNav.vue'
+import { computed } from 'vue'
+import { useStore } from '@/store'
+
+defineProps({
+  pageTitle: {
+    type: String,
+    default: 'Page Title',
+  },
+  navMenu: {
+    type: Array,
+    default: () => ['Base Menu'],
+  },
+})
+
+const isDark = computed(() => useStore().theme === 'dark')
+const backGround = computed(() =>
+  isDark.value ? 'bg-blue-grey-darken-3' : 'bg-blue-grey-lighten-3',
+)
+</script>
+
+<template>
+  <div class="mb-4 text-body" :class="backGround">
+    <div class="mb-4 p-3 text-white">
+      <v-icon icon="mdi mdi-text-box-check-outline" size="small" />
+      <strong class="pl-1"> {{ pageTitle }}</strong>
+    </div>
+
+    <div class="pl-5">
+      <HeaderNav :menus="navMenu" :query="$route?.query" />
+
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ app/vue/src/views/_WorkManage/_common/ContentHeader/components/CompanySelect.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { computed, inject, onBeforeMount } from 'vue'
+import Multiselect from '@vueform/multiselect'
+
+const emit = defineEmits(['com-select'])
+
+const comStore = inject('comStore') as any
+const company = computed(() => comStore?.company?.pk)
+const comSelectList = computed(() => comStore?.comSelect)
+
+const comSelect = (e: { originalEvent: Event; value: any; option: any }) => emit('com-select', e)
+const comClear = () => emit('com-select', null)
+
+onBeforeMount(() => comStore?.fetchCompanyList())
+</script>
+
+<template>
+  <CRow class="m-0 align-items-center">
+    <CFormLabel class="col-lg-1 col-form-label text-body">회사명</CFormLabel>
+    <CCol md="6" lg="3">
+      <Multiselect
+        :value="company"
+        :options="comSelectList"
+        placeholder="회사선택"
+        autocomplete="label"
+        :classes="{ search: 'form-control multiselect-search' }"
+        :add-option-on="['enter', 'tab']"
+        searchable
+        @select="comSelect"
+        @clear="comClear"
+      />
+    </CCol>
+    <CCol v-if="!comSelectList?.length" class="pl-0 align-middle">
+      <v-icon
+        icon="mdi mdi-plus-thick"
+        color="primary"
+        @click="$router.push({ name: '회사 정보 관리' })"
+      />
+    </CCol>
+  </CRow>
+</template>
