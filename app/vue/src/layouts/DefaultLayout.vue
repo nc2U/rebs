9c1731ec38e4bf6f458b7410eec3e2b1618ca2ec
austin kho <kori.susie@gmail.com>
2024-02-16 17:05:49 +0900

    default layout update

--- app/vue/src/layouts/DefaultLayout.vue
+++ app/vue/src/layouts/DefaultLayout.vue
@@ -11,8 +11,8 @@ import BackToTop from '@/components/BackToTop/index.vue'
   <AppSidebar />
   <div class="wrapper d-flex flex-column min-vh-100 bg-light dark:bg-body">
     <AppHeader />
-    <div class="body flex-grow-1 px-md-3 px-xl-5 dark:bg-body">
-      <CContainer fluid class="dark:bg-body">
+    <div class="body flex-grow-1 px-md-3 px-xl-5">
+      <CContainer fluid>
         <router-view />
       </CContainer>
     </div>
