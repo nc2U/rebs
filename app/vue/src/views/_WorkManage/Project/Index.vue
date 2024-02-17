f30c62439492c950dc736e95f35367de3456cf8e
Austin Kho <kori.susie@gmail.com>
2024-02-17 21:53:13 +0900

    site layout update

--- app/vue/src/views/_WorkManage/Project/Index.vue
+++ app/vue/src/views/_WorkManage/Project/Index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { pageTitle, navMenu } from '@/views/_WorkManage/_menu/headermixin12'
-import ContentHeader from '../_common/ContentHeader/Index.vue'
-import ContentBody from '../_common/ContentBody/Index.vue'
+import ContentHeader from '../_pubComp/Header/Index.vue'
+import ContentBody from '../_pubComp/Body/Index.vue'
 </script>
 
 <template>
