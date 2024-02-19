c6f47f5ae6943aa984929fec6ad55bdb5b3354ff
Austin Kho <kori.susie@gmail.com>
2024-02-19 20:56:04 +0900

    site layout update

--- app/vue/src/layouts/__tessts__/DefaultLayout.spec.ts
+++ app/vue/src/layouts/__tessts__/DefaultLayout.spec.ts
@@ -19,7 +19,7 @@ describe('DefaultLayout', () => {
     expect(wrapper.html()).toContain('app-sidebar-stub')
     expect(div1.attributes('class')).toContain('wrapper d-flex')
     expect(div1.html()).toContain('app-header-stub')
-    expect(div2.attributes('class')).toContain('body flex-grow-1')
+    expect(div2.attributes('class')).toContain('body d-flex flex-column flex-grow-1')
     expect(div2.html()).toContain('c-container-stub')
     expect(wrapper.html()).toContain('app-footer-stub')
     expect(wrapper.html()).toContain('back-to-top-stub')
