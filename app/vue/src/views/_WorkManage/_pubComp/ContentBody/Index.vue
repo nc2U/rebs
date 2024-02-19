30f63b9db053d0a6abbc2ff1c31c853fcb7b287b
Austin Kho <kori.susie@gmail.com>
2024-02-19 20:50:01 +0900

    site layout update

--- app/vue/src/views/_WorkManage/_pubComp/ContentBody/Index.vue
+++ app/vue/src/views/_WorkManage/_pubComp/ContentBody/Index.vue
@@ -1,15 +1,52 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from '@/store'
 
+const visible = ref(false)
 const isDark = computed(() => useStore().theme === 'dark')
 const backGround = computed(() => (isDark.value ? 'bg-dark' : 'bg-white'))
 </script>
 
 <template>
   <CRow class="flex-grow-1">
-    <CCol md="9" class="text-body pl-4 p-3 main" :class="backGround">a</CCol>
+    <CCol md="10" class="text-body pl-4 p-3 main" :class="backGround">a</CCol>
     <CCol class="text-body p-3">b</CCol>
+
+    <!--    <CButton-->
+    <!--      color="primary"-->
+    <!--      @click="-->
+    <!--        () => {-->
+    <!--          visible = !visible-->
+    <!--        }-->
+    <!--      "-->
+    <!--      >Toggle offcanvas-->
+    <!--    </CButton>-->
+
+    <!--    <COffcanvas-->
+    <!--      placement="end"-->
+    <!--      :visible="visible"-->
+    <!--      @hide="-->
+    <!--        () => {-->
+    <!--          visible = !visible-->
+    <!--        }-->
+    <!--      "-->
+    <!--    >-->
+    <!--      <COffcanvasHeader>-->
+    <!--        <COffcanvasTitle>Offcanvas</COffcanvasTitle>-->
+    <!--        <CCloseButton-->
+    <!--          class="text-reset"-->
+    <!--          @click="-->
+    <!--            () => {-->
+    <!--              visible = false-->
+    <!--            }-->
+    <!--          "-->
+    <!--        />-->
+    <!--      </COffcanvasHeader>-->
+    <!--      <COffcanvasBody>-->
+    <!--        Content for the offcanvas goes here. You can place just about any Bootstrap component or-->
+    <!--        custom elements here.-->
+    <!--      </COffcanvasBody>-->
+    <!--    </COffcanvas>-->
   </CRow>
 </template>
 
--- app/vue/src/views/_WorkManage/_pubComp/Header/Index.vue
+++ app/vue/src/views/_WorkManage/_pubComp/Header/Index.vue
@@ -16,15 +16,14 @@ defineProps({
 
 const isDark = computed(() => useStore().theme === 'dark')
 const backGround = computed(() =>
-  isDark.value ? 'bg-blue-grey-darken-3' : 'bg-blue-grey-lighten-3',
+  isDark.value ? 'bg-blue-grey-darken-5' : 'bg-blue-grey-lighten-4',
 )
 </script>
 
 <template>
   <div class="mb-0 text-body" :class="backGround">
-    <div class="mb-4 p-3 text-white">
-      <v-icon icon="mdi mdi-text-box-check-outline" size="small" />
-      <strong class="pl-1"> {{ pageTitle }}</strong>
+    <div class="mb-2 p-4 text-white">
+      <strong class="title pl-1"> {{ pageTitle }}</strong>
     </div>
 
     <div class="pl-5">
@@ -32,3 +31,10 @@ const backGround = computed(() =>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+strong.title {
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 20px;
+}
+</style>
