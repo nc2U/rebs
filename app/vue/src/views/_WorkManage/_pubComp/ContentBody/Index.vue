24b867b2e8aaf1027e240ad4b63ef059508864a4
Austin Kho <kori.susie@gmail.com>
2024-02-19 20:19:07 +0900

    site layout update

--- app/vue/src/views/_WorkManage/_pubComp/ContentBody/Index.vue
+++ app/vue/src/views/_WorkManage/_pubComp/ContentBody/Index.vue
@@ -9,12 +9,16 @@ const backGround = computed(() => (isDark.value ? 'bg-dark' : 'bg-white'))
 <template>
   <CRow class="flex-grow-1">
     <CCol md="9" class="text-body pl-4 p-3 main" :class="backGround">a</CCol>
-    <CCol class="p-3">b</CCol>
+    <CCol class="text-body p-3">b</CCol>
   </CRow>
 </template>
 
 <style lang="scss" scoped>
 .main {
   border-right: 1px solid #ddd !important;
+}
+
+.dark-theme .main {
+  border-right: 1px solid #333 !important;
 }
 </style>
