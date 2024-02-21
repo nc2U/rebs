927fd770042363a6fff120fb69821c6866d49287
Austin Kho <kori.susie@gmail.com>
2024-02-21 19:19:27 +0900

    work layout update

--- app/vue/src/views/_Work/Projects/Index.vue
+++ app/vue/src/views/_Work/Projects/Index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { pageTitle, navMenu } from '@/views/_Work/_menu/headermixin11'
+import { computed, ref } from 'vue'
+import { pageTitle, navMenu1, navMenu2 } from '@/views/_Work/_menu/headermixin11'
 import Header from '@/views/_Work/components/Header/Index.vue'
 import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
+import { useRoute } from 'vue-router'
 
 const cBody = ref()
+
+const route = useRoute()
+const navMenu = computed(() => (route.params['projId'] ? navMenu2 : navMenu1))
+
 const sideNavCAll = () => cBody.value.toggle()
 </script>
 
