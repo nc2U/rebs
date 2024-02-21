f686d456d33ec41d26d1a4898417ca90221983ea
austin kho <kori.susie@gmail.com>
2024-02-21 10:46:10 +0900

    default layout update

--- app/vue/src/views/_WorkManage/_pubComp/Header/components/HeaderNav.vue
+++ app/vue/src/views/_WorkManage/_pubComp/Header/components/HeaderNav.vue
@@ -9,6 +9,19 @@ defineProps({
 
 <template>
   <CNav variant="tabs" class="mb-0 pl-4">
+    <CDropdown color="secondary" class="m-0">
+      <CDropdownToggle component="button" color=""></CDropdownToggle>
+      <CDropdownMenu>
+        <CDropdownItem>새 업무 만들기</CDropdownItem>
+        <CDropdownItem>새 업무 카테고리</CDropdownItem>
+        <CDropdownItem>새 버전</CDropdownItem>
+        <CDropdownItem>작업시간 기록</CDropdownItem>
+        <CDropdownItem>새 뉴스</CDropdownItem>
+        <CDropdownItem>새 문서</CDropdownItem>
+        <CDropdownItem>새 위키</CDropdownItem>
+        <CDropdownItem>파일추가</CDropdownItem>
+      </CDropdownMenu>
+    </CDropdown>
     <CNavItem v-for="(menu, i) in menus" :key="i">
       <CNavLink
         :active="$route.name === menu || $route.meta.title === menu"
