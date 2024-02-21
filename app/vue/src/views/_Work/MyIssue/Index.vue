430f4a0b59e680557f2dccbb750545a81f61544b
austin kho <kori.susie@gmail.com>
2024-02-21 13:14:17 +0900

    work layout update

--- app/vue/src/views/_Work/MyIssue/Index.vue
+++ app/vue/src/views/_Work/MyIssue/Index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { pageTitle, navMenu } from '@/views/_Work/_menu/headermixin11'
 import Header from '../components/Header/Index.vue'
 import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
+
+const cBody = ref()
+const sideNavCAll = () => cBody.value.toggle()
 </script>
 
 <template>
-  <Header :page-title="pageTitle" :nav-menu="navMenu" />
+  <Header :page-title="pageTitle" :nav-menu="navMenu" @side-nav-call="sideNavCAll" />
 
-  <ContentBody />
+  <ContentBody ref="cBody" :nav-menu="navMenu" :query="$route?.query">
+    <template v-slot:default>a</template>
+
+    <template v-slot:aside></template>
+  </ContentBody>
 </template>
