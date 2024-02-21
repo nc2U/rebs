6dcd41e83443c6f989aeeec1ea860d04299cbad8
austin kho <kori.susie@gmail.com>
2024-02-21 12:32:47 +0900

    work layout update

---
+++ app/vue/src/views/_Work/MyIssue/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { pageTitle, navMenu } from '@/views/_Work/_menu/headermixin11'
+import Header from '../components/Header/Index.vue'
+import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
+</script>
+
+<template>
+  <Header :page-title="pageTitle" :nav-menu="navMenu" />
+
+  <ContentBody />
+</template>
--- app/vue/src/views/_Work/Projects/Index.vue
+++ app/vue/src/views/_Work/Projects/Index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { pageTitle, navMenu } from '@/views/_WorkManage/_menu/headermixin12'
-import Header from '../_pubComp/Header/Index.vue'
-import ContentBody from '@/views/_WorkManage/_pubComp/ContentBody/Index.vue'
+import { pageTitle, navMenu } from '@/views/_Work/_menu/headermixin12'
+import Header from '../components/Header/Index.vue'
+import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
 
 const cBody = ref()
 const sideNavCAll = () => cBody.value.toggle()
