9d4ca0ebba3c6ef2667c831cb5e29cf0ff945972
austin kho <kori.susie@gmail.com>
2024-02-23 14:08:16 +0900

    work settings app layout update

--- app/vue/src/views/_Work/Settings/components/Status/Index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup="">
-import { ref } from 'vue'
-
-const msg = ref('Hello world')
-</script>
-
-<template>
-  <div>{{ msg }}</div>
-</template>
--- app/vue/src/views/_Work/Settings/components/Trackers/Index.vue
+++ app/vue/src/views/_Work/Settings/components/Trackers/Index.vue
@@ -1,9 +1,19 @@
-<script lang="ts" setup="">
+<script setup lang="ts">
 import { ref } from 'vue'
+import { pageTitle, navMenu } from '@/views/_Work/_menu/headermixin2'
+import Header from '@/views/_Work/components/Header/Index.vue'
+import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
 
-const msg = ref('Hello world')
+const cBody = ref()
+const sideNavCAll = () => cBody.value.toggle()
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <Header :page-title="pageTitle" :nav-menu="navMenu" @side-nav-call="sideNavCAll" />
+
+  <ContentBody ref="cBody" :nav-menu="navMenu" :query="$route?.query">
+    <template v-slot:default> {{ $route.name }} == </template>
+
+    <template v-slot:aside></template>
+  </ContentBody>
 </template>
