2f1b06587e09888d0803f4e3fb4515bda40c0320
austin kho <kori.susie@gmail.com>
2024-02-23 14:13:38 +0900

    work settings app layout update

--- app/vue/src/views/_Work/Settings/Index.vue
+++ app/vue/src/views/_Work/Settings/Index.vue
@@ -12,7 +12,7 @@ const sideNavCAll = () => cBody.value.toggle()
   <Header :page-title="pageTitle" :nav-menu="navMenu" @side-nav-call="sideNavCAll" />
 
   <ContentBody ref="cBody" :nav-menu="navMenu" :query="$route?.query">
-    <template v-slot:default> {{ $route.name }} ===</template>
+    <template v-slot:default> {{ $route.name }} =---==</template>
 
     <template v-slot:aside></template>
   </ContentBody>
