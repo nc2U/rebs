2e71c2cb182482ece27ba15099f3f5a8cb724c1f
austin kho <kori.susie@gmail.com>
2024-02-23 17:08:02 +0900

    work app layout update

--- app/vue/src/views/_Work/Projects/components/Overview/Index.vue
+++ app/vue/src/views/_Work/Projects/components/Overview/Index.vue
@@ -1,26 +1,11 @@
 <script lang="ts" setup="">
-import { computed, ref } from 'vue'
-import { pageTitle, navMenu1, navMenu2 } from '@/views/_Work/_menu/headermixin1'
 import { useRoute } from 'vue-router'
-import Header from '@/views/_Work/components/Header/Index.vue'
-import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
-
-const cBody = ref()
 
 const route = useRoute()
-const navMenu = computed(() => (route.params['projId'] ? navMenu2 : navMenu1))
-
-const sideNavCAll = () => cBody.value.toggle()
 </script>
 
 <template>
-  <Header :page-title="pageTitle" :nav-menu="navMenu" @side-nav-call="sideNavCAll" />
-
-  <ContentBody ref="cBody" :nav-menu="navMenu" :query="$route?.query">
-    <template v-slot:default>
-      {{ route.name }}
-    </template>
-
-    <template v-slot:aside></template>
-  </ContentBody>
+  <CRow>
+    <CCol>{{ route.name }}</CCol>
+  </CRow>
 </template>
