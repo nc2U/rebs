26155b194462ec8533157c645662a8f883e54432
austin kho <kori.susie@gmail.com>
2024-02-23 14:27:39 +0900

    work app layout update

---
+++ app/vue/src/views/_Work/Projects/components/ProjectHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+  <CRow>
+    <CCol>
+      <h6>{{ $route.name || '프로젝트' }}</h6>
+    </CCol>
+    <CCol class="text-right">
+      <span v-show="$route.name !== '프로젝트 - 생성'" class="mr-2">
+        <v-icon icon="mdi-plus-circle" color="success" size="sm" />
+        <router-link :to="{ name: '프로젝트 - 생성' }" class="ml-1">새 프로젝트</router-link>
+      </span>
+      <span>
+        <v-icon icon="mdi-cog" color="secondary" size="sm" />
+        <router-link :to="{ name: '프로젝트 목록' }" class="ml-1">관리</router-link>
+      </span>
+    </CCol>
+  </CRow>
+</template>
--- app/vue/src/views/_Work/Projects/components/ProjectList.vue
+++ app/vue/src/views/_Work/Projects/components/ProjectList.vue
@@ -1,17 +1,9 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import ProjectHeader from './ProjectHeader.vue'
 </script>
 
 <template>
-  <CRow>
-    <CCol>
-      <h6>{{ $route.name || '프로젝트' }}</h6>
-    </CCol>
-    <CCol class="text-right">
-      <router-link :to="{ name: '프로젝트 - 생성' }">새 프로젝트</router-link>
-      <router-link :to="{ name: '프로젝트 목록' }" class="ml-3">관리</router-link>
-    </CCol>
-  </CRow>
+  <ProjectHeader />
 
   <!--  <router-link :to="{ name: '(개요)', params: { projId: 'dongchun' } }">-->
   <!--    개별 프로젝트-->
