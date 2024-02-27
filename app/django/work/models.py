17de78f29069e87c3592aa6e95161a7db3a30648
austin kho <kori.susie@gmail.com>
2024-02-27 13:24:00 +0900

    work role model update

--- app/django/work/models.py
+++ app/django/work/models.py
@@ -108,11 +108,11 @@ class Member(models.Model):
 
 class Role(models.Model):
     name = models.CharField('이름', max_length=20)
-    can_task_assign = models.BooleanField('업무 위탁 권한', default=True)
+    assignable = models.BooleanField('업무 위탁 권한', default=True)
     TASK_VIEW_PERM = (('ALL', '모든 업무'), ('PUB', '비공개 업무 제외'), ('PRI', '직접 생성 또는 담당한 업무'))
-    task_visible = models.CharField('업무 보기 권한', max_length=3, choices=TASK_VIEW_PERM, default='PUB')
+    issue_visible = models.CharField('업무 보기 권한', max_length=3, choices=TASK_VIEW_PERM, default='PUB')
     TIME_VIEW_PERM = (('ALL', '모든 시간기록'), ('PRI', '직접 생성한 시간기록'))
-    time_log_visible = models.CharField('시간기록 보기 권한', max_length=3, choices=TIME_VIEW_PERM, default='ALL')
+    time_entry_visible = models.CharField('시간기록 보기 권한', max_length=3, choices=TIME_VIEW_PERM, default='ALL')
     USER_VIEW_PERM = (('ALL', '모든 활성 사용자'), ('PRJ', '보이는 프로젝트 사용자'))
     user_visible = models.CharField('사용자 보기 권한', max_length=3, choices=USER_VIEW_PERM, default='ALL')
     default_time_activity = models.ForeignKey('CodeActivity', on_delete=models.SET_NULL, null=True, blank=True,
