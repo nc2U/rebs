bb1dd487bb32d9182ec9ad2685b639c1e8ef523d
Austin Kho <kori.susie@gmail.com>
2024-03-01 11:02:17 +0900

    work search update

--- app/vue/src/views/_Work/components/Header/components/Search.vue
+++ app/vue/src/views/_Work/components/Header/components/Search.vue
@@ -1,9 +1,22 @@
 <script lang="ts" setup="">
 import { ref } from 'vue'
+import Option from '@/views/projects/Option/components/Option.vue'
 
 const msg = ref('head search')
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <CRow>
+    <CCol class="p-1">
+      <CInputGroup size="" class="mb-3">
+        <CInputGroupText id="inputGroup-sizing-sm">검색</CInputGroupText>
+        <CFormInput aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+      </CInputGroup>
+    </CCol>
+    <CCol class="p-1">
+      <CFormSelect size="" class="mb-3">
+        <option>프로젝트 바로가기</option>
+      </CFormSelect>
+    </CCol>
+  </CRow>
 </template>
