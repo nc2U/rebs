ebeb1da846c9936c8e058929ff824423c14261b0
Austin Kho <kori.susie@gmail.com>
2024-03-01 13:27:00 +0900

    work search update

--- app/vue/src/views/_Work/components/SearchBody/Index.vue
+++ app/vue/src/views/_Work/components/SearchBody/Index.vue
@@ -1,9 +1,33 @@
-<script lang="ts" setup="">
-import { ref } from 'vue'
+<script setup lang="ts">
+import { computed, type ComputedRef, inject, onBeforeMount, ref } from 'vue'
+import { navMenu } from '@/views/_Work/_menu/headermixin1'
+import type { Company } from '@/store/types/settings'
+import Header from '@/views/_Work/components/Header/Index.vue'
+import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
 
-const msg = ref('Search Body!!!')
+const emit = defineEmits(['aside-visible'])
+
+const cBody = ref()
+const company = inject<ComputedRef<Company>>('company')
+const comName = computed(() => company?.value?.name)
+
+const sideNavCAll = () => cBody.value.toggle()
+
+onBeforeMount(() => emit('aside-visible', false))
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <Header :page-title="comName" :nav-menu="navMenu" @side-nav-call="sideNavCAll" />
+
+  <ContentBody ref="cBody" :nav-menu="navMenu" :query="$route?.query" :aside="false">
+    <template v-slot:default>
+      <CRow class="py-2">
+        <CCol>
+          <h5>{{ $route.name }}</h5>
+        </CCol>
+      </CRow>
+    </template>
+
+    <template v-slot:aside></template>
+  </ContentBody>
 </template>
