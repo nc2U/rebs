a5975da288a398dd7895503c4cb4a61cc0806b13
Austin Kho <kori.susie@gmail.com>
2024-03-02 23:04:34 +0900

    work setting project update

--- app/vue/src/views/_Work/Settings/Projects/components/ProjectTable.vue
+++ app/vue/src/views/_Work/Settings/Projects/components/ProjectTable.vue
@@ -27,7 +27,8 @@ defineProps({
         <CTableDataCell>
           <CFormCheck />
         </CTableDataCell>
-        <CTableDataCell>
+        <CTableDataCell :class="'pl-' + iproj.depth">
+          <v-icon v-if="iproj.parent" color="secondary" icon="mdi-chevron-right" />
           <router-link :to="{ name: '(개요)', params: { projId: iproj.slug } }">
             {{ iproj.name }}
           </router-link>
@@ -35,7 +36,40 @@ defineProps({
         <CTableDataCell>{{ iproj.slug }}</CTableDataCell>
         <CTableDataCell>{{ iproj.description }}</CTableDataCell>
         <CTableDataCell>
-          <v-icon icon="mdi-dots-horizontal" />
+          <span>
+            <CDropdown color="secondary" variant="input-group" placement="bottom-end">
+              <CDropdownToggle
+                :caret="false"
+                color="light"
+                variant="ghost"
+                size="sm"
+                shape="rounded-pill"
+              >
+                <v-icon icon="mdi-dots-horizontal" class="pointer" color="grey-darken-1" />
+                <v-tooltip activator="parent" location="top">Actions</v-tooltip>
+              </CDropdownToggle>
+              <CDropdownMenu>
+                <CDropdownItem>
+                  <router-link to="">
+                    <v-icon icon="mdi-lock" color="yellow-darken-2" size="sm" />
+                    잠금보관
+                  </router-link>
+                </CDropdownItem>
+                <CDropdownItem>
+                  <router-link to="">
+                    <v-icon icon="mdi-content-copy" color="grey" size="sm" />
+                    복사
+                  </router-link>
+                </CDropdownItem>
+                <CDropdownItem>
+                  <router-link to="">
+                    <v-icon icon="mdi-trash-can-outline" color="grey" size="sm" />
+                    삭제
+                  </router-link>
+                </CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </span>
         </CTableDataCell>
       </CTableRow>
     </CTableBody>
