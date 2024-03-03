c96c077739539aecc46c677b2898606004e98fcb
Austin Kho <kori.susie@gmail.com>
2024-03-03 12:48:45 +0900

    work setting issue tracking layout

--- app/vue/src/views/_Work/Manages/Projects/components/Settings/components/IssueTracking.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Settings/components/IssueTracking.vue
@@ -1,9 +1,92 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import { computed } from 'vue'
 
-const msg = ref('IssueTracking!')
+const memberList = computed(() => [])
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <CRow class="py-2">
+    <CCol class="text-right"></CCol>
+  </CRow>
+
+  <CRow>
+    <CCol>
+      <h6>
+        <v-icon icon="mdi-check" color="success" size="sm" />
+        업무 유형
+      </h6>
+    </CCol>
+  </CRow>
+
+  <CRow>
+    <CCol>
+      <CCard class="mb-3" color="light">
+        <CCardBody>
+          <CRow>
+            <CCol>
+              <CFormCheck inline id="bug" label="버그" />
+              <CFormCheck inline id="feature" label="기능" />
+              <CFormCheck inline id="support" label="지원" />
+            </CCol>
+            <CCol class="text-right">
+              <span>
+                <v-icon icon="mdi-cog" color="secondary" size="sm" />
+                <router-link :to="{ name: '업무 유형' }" class="ml-1">관리</router-link>
+              </span>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+
+  <CRow>
+    <CCol>
+      <CCard class="mb-3" color="light">
+        <CCardBody>
+          <CRow class="mb-2">
+            <CFormLabel for="inputEmail3" class="col-sm-2 col-form-label text-right">
+              기본 버전
+            </CFormLabel>
+            <CCol sm="4">
+              <CFormSelect>
+                <option value="">없음</option>
+              </CFormSelect>
+            </CCol>
+          </CRow>
+          <CRow class="mb-2">
+            <CFormLabel for="inputEmail3" class="col-sm-2 col-form-label text-right">
+              기본 담당자
+            </CFormLabel>
+            <CCol sm="4">
+              <CFormSelect>
+                <option value="">없음</option>
+              </CFormSelect>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CFormLabel for="inputEmail3" class="col-sm-2 col-form-label text-right">
+              기본 업무 요청
+            </CFormLabel>
+            <CCol sm="4">
+              <CFormSelect>
+                <option value="">없음</option>
+                <option value="1">나에게 할당된 업무</option>
+                <option value="2">보고된 업무</option>
+                <option value="3">업데이트된 업무</option>
+                <option value="4">지켜보는 업무</option>
+              </CFormSelect>
+              <span class="form-text">공개(모든 사용자에게) 요청만 선택 가능</span>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+
+  <CRow>
+    <CCol>
+      <CButton color="primary" size="sm" variant="outline">저장</CButton>
+    </CCol>
+  </CRow>
 </template>
--- app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Member.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Member.vue
@@ -1,9 +1,30 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import { computed } from 'vue'
+import NoData from '@/views/_Work/components/NoData.vue'
 
-const msg = ref('Member!')
+const memberList = computed(() => [])
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <CRow class="py-2">
+    <CCol>
+      <span class="mr-2">
+        <v-icon icon="mdi-plus-circle" color="success" size="sm" />
+        <router-link to="" class="ml-1">새 구성원</router-link>
+      </span>
+    </CCol>
+
+    <CCol class="text-right">
+      <span>
+        <v-icon icon="mdi-cog" color="secondary" size="sm" />
+        <router-link to="" class="ml-1">관리</router-link>
+      </span>
+    </CCol>
+  </CRow>
+
+  <NoData v-if="!memberList.length" />
+
+  <CRow v-else>
+    <CCol></CCol>
+  </CRow>
 </template>
