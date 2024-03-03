3af8d658f0c578d3350e33f924a248a74448032c
Austin Kho <kori.susie@gmail.com>
2024-03-03 13:03:53 +0900

    work setting version layout

--- app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Version.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Version.vue
@@ -1,9 +1,69 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import { computed } from 'vue'
+import NoData from '@/views/_Work/components/NoData.vue'
 
-const msg = ref('Version!')
+const versionList = computed(() => [])
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <CRow class="py-2">
+    <CCol>
+      <span class="mr-2">
+        <v-icon icon="mdi-plus-circle" color="success" size="sm" />
+        <router-link to="" class="ml-1">새 버전</router-link>
+      </span>
+    </CCol>
+  </CRow>
+
+  <CRow class="mt-3">
+    <CCol>
+      <h6>
+        <v-icon icon="mdi-check" color="success" size="sm" />
+        검색조건
+      </h6>
+    </CCol>
+  </CRow>
+
+  <CRow>
+    <CCol>
+      <CCard class="mb-3" color="light">
+        <CCardBody>
+          <CRow>
+            <CFormLabel for="inputEmail3" class="col-sm-1 col-form-label text-right">
+              상태
+            </CFormLabel>
+            <CCol sm="2">
+              <CFormSelect>
+                <option value="0">모두</option>
+                <option value="1">진행</option>
+                <option value="2">잠김</option>
+                <option value="3">닫힘</option>
+              </CFormSelect>
+            </CCol>
+
+            <CFormLabel for="inputEmail3" class="col-sm-1 col-form-label text-right">
+              버전
+            </CFormLabel>
+            <CCol sm="2">
+              <CFormInput />
+            </CCol>
+
+            <CCol class="pt-1">
+              <CButton color="primary" size="sm" variant="outline">적용</CButton>
+              <span class="ml-2">
+                <v-icon icon="mdi-reload" size="sm" color="success" />
+                <router-link to="">지우기</router-link>
+              </span>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+
+  <NoData v-if="!versionList.length" />
+
+  <CRow v-else>
+    <CCol></CCol>
+  </CRow>
 </template>
