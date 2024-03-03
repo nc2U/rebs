6b1bec572a7d5346f4bae15389eee915f60ee5c6
Austin Kho <kori.susie@gmail.com>
2024-03-03 13:05:06 +0900

    work setting issue category layout

--- app/vue/src/views/_Work/Manages/Projects/components/Settings/components/IssueCategory.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Settings/components/IssueCategory.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup="">
-import { ref } from 'vue'
+import { computed } from 'vue'
+import NoData from '@/views/_Work/components/NoData.vue'
 
-const msg = ref('IssueCategory!')
+const iCategoryList = computed(() => [])
 </script>
 
 <template>
-  <div>{{ msg }}</div>
+  <CRow class="py-2">
+    <CCol>
+      <span class="mr-2">
+        <v-icon icon="mdi-plus-circle" color="success" size="sm" />
+        <router-link to="" class="ml-1">새 업무범주</router-link>
+      </span>
+    </CCol>
+  </CRow>
+
+  <NoData v-if="!iCategoryList.length" />
+
+  <CRow v-else>
+    <CCol></CCol>
+  </CRow>
 </template>
