4f707fbf01b8bbf4262cd1275d92cc53df398c89
austin kho <kori.susie@gmail.com>
2024-03-05 10:23:35 +0900

    work model update

--- app/django/work/models.py
+++ app/django/work/models.py
@@ -5,6 +5,42 @@ from datetime import datetime
 from django.db import models
 
 
+class IssueProject(models.Model):
+    company = models.ForeignKey('company.Company', on_delete=models.CASCADE, verbose_name="회사")
+    name = models.CharField('이름', max_length=100)
+    description = models.TextField('설명', blank=True, default='')
+    homepage = models.URLField('홈페이지', max_length=255, null=True, blank=True)
+    is_public = models.BooleanField('공개', default=True, help_text='공개 프로젝트는 모든 로그인한 사용자가 접속할 수 있습니다.')
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='상위 프로젝트')
+    slug = models.CharField('식별자', max_length=100, unique=True,
+                            help_text='1에서 100글자 소문자(a-z), 숫자, 대쉬(-)와 밑줄(_)만 가능합니다. 식별자는 저장 후에는 수정할 수 없습니다.')
+    status = models.CharField('사용여부', max_length=1, default='1', choices=(('1', '사용'), ('9', '잠금보관(모든 접근이 차단됨)')))
+    is_inherit_members = models.BooleanField('상위 프로젝트 멤버 상속', default=False)
+    depth = models.PositiveSmallIntegerField('단계', default=1,
+                                             help_text='프로젝트 간 상하 소속 관계에 의한 단계, 최상위인 경우 1단계 이후 각 뎁스 마다 1씩 증가')
+    members = models.ManyToManyField('Member', through='Membership')
+    user = models.ForeignKey('accounts.User', on_delete=models.PROTECT, verbose_name='생성자')
+    created = models.DateTimeField('추가', auto_now_add=True)
+    updated = models.DateTimeField('수정', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def _recurse_parents(self, parents=None):
+        if parents is None:
+            parents = []
+
+        if self.parent:
+            parents.insert(0, self.parent)
+            return self.parent._recurse_parents(parents)
+        return parents
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = '01. 프로젝트(업무)'
+        verbose_name_plural = '01. 프로젝트(업무)'
+
+
 class Role(models.Model):
     name = models.CharField('이름', max_length=20)
     assignable = models.BooleanField('업무 위탁 권한', default=True)
@@ -134,42 +170,6 @@ class Permission(models.Model):
 
     def __str__(self):
         return f'{self.role.name} - 권한'
-
-
-class IssueProject(models.Model):
-    company = models.ForeignKey('company.Company', on_delete=models.CASCADE, verbose_name="회사")
-    name = models.CharField('이름', max_length=100)
-    description = models.TextField('설명', blank=True, default='')
-    homepage = models.URLField('홈페이지', max_length=255, null=True, blank=True)
-    is_public = models.BooleanField('공개', default=True, help_text='공개 프로젝트는 모든 로그인한 사용자가 접속할 수 있습니다.')
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='상위 프로젝트')
-    slug = models.CharField('식별자', max_length=100, unique=True,
-                            help_text='1에서 100글자 소문자(a-z), 숫자, 대쉬(-)와 밑줄(_)만 가능합니다. 식별자는 저장 후에는 수정할 수 없습니다.')
-    status = models.CharField('사용여부', max_length=1, default='1', choices=(('1', '사용'), ('9', '잠금보관(모든 접근이 차단됨)')))
-    is_inherit_members = models.BooleanField('상위 프로젝트 멤버 상속', default=False)
-    depth = models.PositiveSmallIntegerField('단계', default=1,
-                                             help_text='프로젝트 간 상하 소속 관계에 의한 단계, 최상위인 경우 1단계 이후 각 뎁스 마다 1씩 증가')
-    members = models.ManyToManyField('Member', through='Membership')
-    user = models.ForeignKey('accounts.User', on_delete=models.PROTECT, verbose_name='생성자')
-    created = models.DateTimeField('추가', auto_now_add=True)
-    updated = models.DateTimeField('수정', auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-    def _recurse_parents(self, parents=None):
-        if parents is None:
-            parents = []
-
-        if self.parent:
-            parents.insert(0, self.parent)
-            return self.parent._recurse_parents(parents)
-        return parents
-
-    class Meta:
-        ordering = ('-created',)
-        verbose_name = '01. 프로젝트(업무)'
-        verbose_name_plural = '01. 프로젝트(업무)'
 
 
 class Membership(models.Model):
