ca07efc55282b7357045a9daf964e6672b0280b8
austin kho <kori.susie@gmail.com>
2024-03-15 18:19:05 +0900

    issue list update

--- app/vue/src/views/_Work/Manages/Issues/Index.vue
+++ app/vue/src/views/_Work/Manages/Issues/Index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { computed, type ComputedRef, inject, ref } from 'vue'
+import { computed, type ComputedRef, inject, onBeforeMount, ref } from 'vue'
 import { navMenu2 as navMenu } from '@/views/_Work/_menu/headermixin1'
+import { timeFormat } from '@/utils/baseMixins'
 import type { Company } from '@/store/types/settings'
+import { useWork } from '@/store/pinia/work'
 import Header from '@/views/_Work/components/Header/Index.vue'
 import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
 import SearchList from '@/views/_Work/components/SearchList.vue'
@@ -13,7 +15,12 @@ const comName = computed(() => company?.value?.name)
 
 const sideNavCAll = () => cBody.value.toggle()
 
-const issueList = computed(() => [])
+const workStore = useWork()
+const issueList = computed(() => workStore.issueList)
+
+onBeforeMount(() => {
+  workStore.fetchIssueList()
+})
 </script>
 
 <template>
@@ -39,7 +46,38 @@ const issueList = computed(() => [])
       <NoData v-if="!issueList.length" />
 
       <CRow v-else>
-        <CCol></CCol>
+        <CCol col="12">
+          <v-divider class="mb-0" />
+          <CTable striped hover small>
+            <CTableHead>
+              <CTableRow class="text-center">
+                <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col">프로젝트</CTableHeaderCell>
+                <CTableHeaderCell scope="col">유형</CTableHeaderCell>
+                <CTableHeaderCell scope="col">상태</CTableHeaderCell>
+                <CTableHeaderCell scope="col">우선순위</CTableHeaderCell>
+                <CTableHeaderCell scope="col">제목</CTableHeaderCell>
+                <CTableHeaderCell scope="col">담당자</CTableHeaderCell>
+                <CTableHeaderCell scope="col">변경</CTableHeaderCell>
+                <CTableHeaderCell scope="col"></CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+
+            <CTableBody>
+              <CTableRow v-for="issue in issueList" :key="issue.pk">
+                <CTableDataCell>{{ issue.pk }}</CTableDataCell>
+                <CTableDataCell>{{ issue.project }}</CTableDataCell>
+                <CTableDataCell>{{ issue.tracker }}</CTableDataCell>
+                <CTableDataCell>{{ issue.status }}</CTableDataCell>
+                <CTableDataCell>{{ issue.priority }}</CTableDataCell>
+                <CTableDataCell>{{ issue.subject }}</CTableDataCell>
+                <CTableDataCell class="text-center">{{ issue.assigned_to }}</CTableDataCell>
+                <CTableDataCell class="text-center">{{ timeFormat(issue.updated) }}</CTableDataCell>
+                <CTableDataCell></CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
+        </CCol>
       </CRow>
     </template>
 
