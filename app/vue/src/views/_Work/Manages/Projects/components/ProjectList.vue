e7dda7711730d6727a06911b35fc85f15daa4b27
austin kho <kori.susie@gmail.com>
2024-03-19 15:25:38 +0900

    issue project list update

--- app/vue/src/views/_Work/Manages/Projects/components/ProjectList.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/ProjectList.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { inject, onBeforeMount, type PropType } from 'vue'
+import { ref, computed, inject, onBeforeMount, type ComputedRef, type PropType } from 'vue'
+import type { User } from '@/store/types/accounts'
 import type { IssueProject } from '@/store/types/work'
 import SearchList from '@/views/_Work/components/SearchList.vue'
 import NoData from '@/views/_Work/components/NoData.vue'
@@ -14,6 +15,7 @@ defineProps({
 const emit = defineEmits(['aside-visible'])
 
 const superAuth = inject('superAuth', false)
+const userInfo = inject<ComputedRef<User>>('userInfo')
 
 onBeforeMount(() => emit('aside-visible', true))
 </script>
@@ -42,7 +44,14 @@ onBeforeMount(() => emit('aside-visible', true))
 
   <CRow v-else>
     <CCol v-for="proj in projectList" :key="proj.pk" sm="12" lg="6" xl="4">
-      <CCard class="my-2">
+      <CCard
+        v-if="
+          proj.is_public ||
+          proj.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+          superAuth
+        "
+        class="my-2"
+      >
         <CCardBody class="project-set">
           <router-link :to="{ name: '(개요)', params: { projId: proj.slug } }">
             {{ proj.name }}
@@ -52,42 +61,94 @@ onBeforeMount(() => emit('aside-visible', true))
           <!-- c1 -->
           <div v-if="!!proj.sub_projects?.length" class="child">
             <blockquote v-for="c1 in proj.sub_projects" :key="c1.pk">
-              <router-link :to="{ name: '(개요)', params: { projId: c1.slug } }">
-                {{ c1.name }}
-              </router-link>
-              <p v-html="c1.description" />
+              <span
+                v-if="
+                  c1.is_public ||
+                  c1.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+                  (c1.parent_members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) &&
+                    c1.is_inherit_members) ||
+                  superAuth
+                "
+              >
+                <router-link :to="{ name: '(개요)', params: { projId: c1.slug } }">
+                  {{ c1.name }}
+                </router-link>
+                <p v-html="c1.description" />
+              </span>
 
               <!-- c2 -->
               <div v-if="!!c1.sub_projects?.length" class="child">
                 <blockquote v-for="c2 in c1.sub_projects" :key="c2.pk">
-                  <router-link :to="{ name: '(개요)', params: { projId: c2.slug } }">
-                    {{ c2.name }}
-                  </router-link>
-                  <p v-html="c2.description" />
+                  <span
+                    v-if="
+                      c2.is_public ||
+                      c2.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+                      (c2.parent_members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) &&
+                        c2.is_inherit_members) ||
+                      superAuth
+                    "
+                  >
+                    <router-link :to="{ name: '(개요)', params: { projId: c2.slug } }">
+                      {{ c2.name }}
+                    </router-link>
+                    <p v-html="c2.description" />
+                  </span>
 
                   <!-- c3 -->
                   <div v-if="!!c2.sub_projects?.length" class="child">
                     <blockquote v-for="c3 in c2.sub_projects" :key="c3.pk">
-                      <router-link :to="{ name: '(개요)', params: { projId: c3.slug } }">
-                        {{ c3.name }}
-                      </router-link>
-                      <p v-html="c3.description" />
+                      <span
+                        v-if="
+                          c3.is_public ||
+                          c3.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+                          (c3.parent_members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) &&
+                            c3.is_inherit_members) ||
+                          superAuth
+                        "
+                      >
+                        <router-link :to="{ name: '(개요)', params: { projId: c3.slug } }">
+                          {{ c3.name }}
+                        </router-link>
+                        <p v-html="c3.description" />
+                      </span>
 
                       <!-- c4 -->
                       <div v-if="!!c3.sub_projects?.length" class="child">
                         <blockquote v-for="c4 in c3.sub_projects" :key="c4.pk">
-                          <router-link :to="{ name: '(개요)', params: { projId: c4.slug } }">
-                            {{ c4.name }}
-                          </router-link>
-                          <p v-html="c4.description" />
+                          <span
+                            v-if="
+                              c4.is_public ||
+                              c4.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+                              (c4.parent_members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) &&
+                                c4.is_inherit_members) ||
+                              superAuth
+                            "
+                          >
+                            <router-link :to="{ name: '(개요)', params: { projId: c4.slug } }">
+                              {{ c4.name }}
+                            </router-link>
+                            <p v-html="c4.description" />
+                          </span>
 
                           <!-- c5 -->
                           <div v-if="!!c4.sub_projects?.length" class="child">
                             <blockquote v-for="c5 in c4.sub_projects" :key="c5.pk">
-                              <router-link :to="{ name: '(개요)', params: { projId: c5.slug } }">
-                                {{ c5.name }}
-                              </router-link>
-                              <p v-html="c5.description" />
+                              <span
+                                v-if="
+                                  c5.is_public ||
+                                  c5.members.map(m => m.user.pk).includes(userInfo?.pk ?? 0) ||
+                                  (c5.parent_members
+                                    .map(m => m.user.pk)
+                                    .includes(userInfo?.pk ?? 0) &&
+                                    c5.is_inherit_members) ||
+                                  superAuth
+                                "
+                              >
+                                <router-link :to="{ name: '(개요)', params: { projId: c5.slug } }">
+                                  {{ c5.name }}
+                                </router-link>
+                                <p v-html="c5.description" />
+                              </span>
                             </blockquote>
                           </div>
                         </blockquote>
