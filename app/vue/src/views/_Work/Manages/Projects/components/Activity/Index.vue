da76a595f1a736a8cf637154edd33d036cc18e98
Austin Kho <kori.susie@gmail.com>
2024-03-30 01:18:32 +0900

    work activity update ing

--- app/vue/src/views/_Work/Manages/Projects/components/Activity/Index.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Activity/Index.vue
@@ -1,29 +1,33 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount } from 'vue'
+import { ref, computed, onBeforeMount, watch } from 'vue'
+import { dateFormat, timeFormat } from '@/utils/baseMixins'
 import { useWork } from '@/store/pinia/work'
+import type { ActLogEntry } from '@/store/types/work'
 import NoData from '@/views/_Work/components/NoData.vue'
 
 const emit = defineEmits(['aside-visible'])
 
-const fromDate = computed(
-  () =>
-    new Date().getFullYear() +
-    '/' +
-    (new Date().getMonth() + 1) +
-    '/' +
-    (new Date().getDate() - 10),
-)
+const toDate = ref(new Date())
+watch(toDate, nVal => {
+  workStore.fetchActivityLogList({
+    from_act_date: dateFormat(fromDate.value),
+    to_act_date: dateFormat(nVal),
+  })
+})
 
-const toDate = computed(
-  () => new Date().getFullYear() + '/' + (new Date().getMonth() + 1) + '/' + new Date().getDate(),
-)
+const fromDate = computed(() => new Date(toDate.value.getTime() - 10 * 24 * 60 * 60 * 1000))
 
 const workStore = useWork()
-const ActivityLogList = computed(() => workStore.ActivityLogList)
+const groupedActivities = computed<{ [key: string]: ActLogEntry[] }>(
+  () => workStore.groupedActivities,
+)
 
 onBeforeMount(() => {
   emit('aside-visible', true)
-  workStore.fetchActivityLogList()
+  workStore.fetchActivityLogList({
+    from_act_date: dateFormat(fromDate.value),
+    to_act_date: dateFormat(toDate.value),
+  })
 })
 </script>
 
@@ -35,14 +39,25 @@ onBeforeMount(() => {
   </CRow>
 
   <CRow class="fst-italic">
-    <CCol> {{ fromDate }}부터 {{ toDate }}까지</CCol>
+    <CCol> {{ dateFormat(fromDate, '/') }}부터 {{ dateFormat(toDate, '/') }}까지</CCol>
   </CRow>
 
-  <NoData v-if="!ActivityLogList.length" />
+  <NoData v-if="!groupedActivities" />
 
-  <CRow v-else>
-    <CCol v-for="act in ActivityLogList" :key="act.pk">
-      {{ act }}
+  <CRow v-else class="my-3">
+    <CCol v-for="(val, key) in groupedActivities" :key="key">
+      <CAlert color="secondary" class="px-3 py-2">
+        <span class="date-title">{{ String(key) === dateFormat(new Date()) ? '오늘' : key }}</span>
+      </CAlert>
+
+      <div v-for="act in val" :key="act.pk">
+        <span>{{ timeFormat(act.timestamp, true) }}</span>
+        <span class="ml-2">{{ act.project }}</span> -
+        <span>{{ act.issue }}</span>
+        <div>{{ act.user.username }}</div>
+        <v-divider class="my-2" />
+        {{ act }}
+      </div>
     </CCol>
   </CRow>
 
@@ -55,3 +70,10 @@ onBeforeMount(() => {
     </CCol>
   </CRow>
 </template>
+
+<style lang="scss" scoped>
+.date-title {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+</style>
