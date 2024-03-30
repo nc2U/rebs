e7ff680e61ce583796ea1501c25a71facd80fa4d
Austin Kho <kori.susie@gmail.com>
2024-03-31 01:00:56 +0900

    work time entry update ing

--- app/vue/src/views/_Work/Manages/SpentTime/Index.vue
+++ app/vue/src/views/_Work/Manages/SpentTime/Index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { computed, type ComputedRef, inject, ref } from 'vue'
+import { computed, type ComputedRef, inject, onBeforeMount, ref } from 'vue'
 import { navMenu2 as navMenu } from '@/views/_Work/_menu/headermixin1'
+import { useWork } from '@/store/pinia/work'
 import type { Company } from '@/store/types/settings'
 import Header from '@/views/_Work/components/Header/Index.vue'
 import ContentBody from '@/views/_Work/components/ContentBody/Index.vue'
 import NoData from '@/views/_Work/components/NoData.vue'
 import SearchList from '@/views/_Work/components/SearchList.vue'
+import TimeEntryList from '@/views/_Work/Manages/SpentTime/components/TimeEntryList.vue'
 
 const cBody = ref()
 const company = inject<ComputedRef<Company>>('company')
@@ -13,7 +15,12 @@ const comName = computed(() => company?.value?.name)
 
 const sideNavCAll = () => cBody.value.toggle()
 
-const timeEntryList = computed(() => [])
+const workStore = useWork()
+const timeEntryList = computed(() => workStore.timeEntryList)
+
+onBeforeMount(() => {
+  workStore.fetchTimeEntryList()
+})
 </script>
 
 <template>
@@ -39,7 +46,9 @@ const timeEntryList = computed(() => [])
       <NoData v-if="!timeEntryList.length" />
 
       <CRow v-else>
-        <CCol></CCol>
+        <CCol>
+          <TimeEntryList :time-entry-list="timeEntryList" />
+        </CCol>
       </CRow>
     </template>
 
---
+++ app/vue/src/views/_Work/Manages/SpentTime/components/TimeEntryList.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { type PropType } from 'vue'
+import type { TimeEntry } from '@/store/types/work'
+
+defineProps({
+  timeEntryList: { type: Array as PropType<TimeEntry[]>, default: () => [] },
+})
+</script>
+
+<template>
+  <div>{{ timeEntryList }}</div>
+</template>
