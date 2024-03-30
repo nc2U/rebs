0bfd386a49c40de12c613861115adb57a3739fd7
Austin Kho <kori.susie@gmail.com>
2024-03-30 16:36:42 +0900

    activity update ing

--- app/vue/src/views/_Work/Manages/Activity/components/ActivityLogList.vue
+++ app/vue/src/views/_Work/Manages/Activity/components/ActivityLogList.vue
@@ -40,32 +40,41 @@ const toNext = () => emit('to-next')
   <CRow v-else class="my-3">
     <CCol>
       <CRow v-for="(val, key) in groupedActivities" :key="key">
-        <CAlert color="secondary" class="px-3 py-2">
-          <span class="date-title">{{
-            String(key) === dateFormat(new Date()) ? '오늘' : key
-          }}</span>
-        </CAlert>
+        <CCol>
+          <CAlert color="secondary" class="px-3 py-2">
+            <span class="date-title">{{
+              String(key) === dateFormat(new Date()) ? '오늘' : key
+            }}</span>
+          </CAlert>
 
-        <div v-for="act in val" :key="act.pk" class="pl-5">
-          <div>
-            <v-icon icon="mdi-cog" size="sm" color="warning" class="mr-1" />
-            <span class="form-text">{{ timeFormat(act.timestamp, true) }}</span>
-            <span class="ml-2 bold">{{ act.project }}</span> -
-            <span v-if="act.issue">
-              <router-link to="">
-                {{ act.issue.tracker }} #{{ act.pk }} ({{ act.status_log || act.issue.status }})
-                {{ act.issue.subject }}
-              </router-link>
-            </span>
-          </div>
-          <div class="ml-4 pl-5">
-            <VueMarkdownIt :source="act.issue?.description" class="form-text" />
-          </div>
-          <div class="form-text ml-5 pl-2 bg-amber-accent-2">
-            <router-link to="">{{ act.user.username }}</router-link>
-          </div>
-          <v-divider class="my-2" />
-        </div>
+          <CRow v-for="(act, i) in val" :key="act.pk" class="pl-3">
+            <CCol :class="{ 'ml-5': i }">
+              <v-icon icon="mdi-cog" size="sm" color="warning" class="mr-1" />
+              <span class="form-text">{{ timeFormat(act.timestamp, true) }}</span>
+              <span class="ml-2 bold">{{ act.project }}</span> -
+              <span v-if="act.issue">
+                <router-link to="">
+                  {{ act.issue.tracker }} #{{ act.issue.pk }} ({{
+                    act.status_log || act.issue.status
+                  }})
+                  {{ act.issue.subject }}
+                </router-link>
+              </span>
+
+              <div class="ml-4 pl-5">
+                <VueMarkdownIt
+                  v-if="!act.status_log"
+                  :source="act.issue?.description"
+                  class="form-text"
+                />
+              </div>
+              <div class="form-text ml-5 pl-2">
+                <router-link to="">{{ act.user.username }}</router-link>
+              </div>
+              <v-divider class="my-2" />
+            </CCol>
+          </CRow>
+        </CCol>
       </CRow>
     </CCol>
   </CRow>
