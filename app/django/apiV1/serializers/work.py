58d557766f54c4ff26fd6f9f6e13ca627c7efba5
austin kho <kori.susie@gmail.com>
2024-04-02 16:23:24 +0900

    work api update

--- app/django/apiV1/serializers/work.py
+++ app/django/apiV1/serializers/work.py
@@ -417,6 +417,23 @@ class TimeEntrySerializer(serializers.ModelSerializer):
         total_hours = filtered_queryset.aggregate(total_hours=Sum('hours'))
         return total_hours['total_hours'] or 0
 
+    @transaction.atomic
+    def create(self, validated_data):
+        issue = self.initial_data.get('issue', None)
+        activity = self.initial_data.get('activity', None)
+
+        time_entry = TimeEntry.objects.create(issue_id=issue,
+                                              activity_id=activity,
+                                              **validated_data)
+        return time_entry
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        instance.__dict__.update(**validated_data)
+        instance.issue = Issue.objects.get(pk=self.initial_data.get('issue'))
+        instance.activity = CodeActivity.objects.get(pk=self.initial_data.get('activity'))
+        return super().update(instance, validated_data)
+
 
 class TimeEntryInActivityLogSerializer(serializers.ModelSerializer):
     class Meta:
