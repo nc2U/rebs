cda9c25eb242f38aef9e56990dc2b41534050782
austin kho <kori.susie@gmail.com>
2024-04-02 13:57:34 +0900

    work history update

--- app/vue/src/views/_Work/Manages/Issues/components/IssueHistory.vue
+++ app/vue/src/views/_Work/Manages/Issues/components/IssueHistory.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { type PropType, ref } from 'vue'
-import type { IssueComment, IssueLogEntry, TimeEntryFilter } from '@/store/types/work'
+import type { IssueComment, IssueLogEntry, TimeEntry } from '@/store/types/work'
 import { elapsedTime } from '@/utils/baseMixins'
 import { VueMarkdownIt } from '@f3ve/vue-markdown-it'
 
 defineProps({
   issueLogList: { type: Array as PropType<IssueLogEntry[]>, default: () => [] },
   issueCommentList: { type: Array as PropType<IssueComment[]>, default: () => [] },
-  timeEntryList: { type: Array as PropType<TimeEntryFilter[]>, default: () => [] },
+  timeEntryList: { type: Array as PropType<TimeEntry[]>, default: () => [] },
 })
 
 const tabPaneActiveKey = ref(1)
@@ -19,9 +19,9 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
   <CNav variant="tabs">
     <CNavItem>
       <CNavLink
-          href="javascript:void(0);"
-          :active="tabPaneActiveKey === 1"
-          @click="
+        href="javascript:void(0);"
+        :active="tabPaneActiveKey === 1"
+        @click="
           () => {
             tabPaneActiveKey = 1
           }
@@ -32,9 +32,9 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
     </CNavItem>
     <CNavItem v-if="issueCommentList.length">
       <CNavLink
-          href="javascript:void(0);"
-          :active="tabPaneActiveKey === 2"
-          @click="
+        href="javascript:void(0);"
+        :active="tabPaneActiveKey === 2"
+        @click="
           () => {
             tabPaneActiveKey = 2
           }
@@ -45,9 +45,9 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
     </CNavItem>
     <CNavItem>
       <CNavLink
-          href="javascript:void(0);"
-          :active="tabPaneActiveKey === 3"
-          @click="
+        href="javascript:void(0);"
+        :active="tabPaneActiveKey === 3"
+        @click="
           () => {
             tabPaneActiveKey = 3
           }
@@ -58,9 +58,9 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
     </CNavItem>
     <CNavItem v-if="timeEntryList.length">
       <CNavLink
-          href="javascript:void(0);"
-          :active="tabPaneActiveKey === 4"
-          @click="
+        href="javascript:void(0);"
+        :active="tabPaneActiveKey === 4"
+        @click="
           () => {
             tabPaneActiveKey = 4
           }
@@ -72,9 +72,9 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
   </CNav>
 
   <CCard
-      v-if="issueLogList.length"
-      class="border-top-0 p-2"
-      :style="{ '--cui-card-border-radius': 0 }"
+    v-if="issueLogList.length"
+    class="border-top-0 p-2"
+    :style="{ '--cui-card-border-radius': 0 }"
   >
     <CCardBody>
       <CTabContent>
@@ -87,7 +87,7 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
                 </router-link>
                 이(가)
                 <router-link
-                    :to="{
+                  :to="{
                     name: '(작업내역)',
                     params: { projId: 'redmine' },
                     query: { from: log.timestamp.substring(0, 10) },
@@ -99,16 +99,16 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
               </CCol>
               <CCol class="text-right">#{{ log.pk }}</CCol>
             </CRow>
-            <v-divider class="mt-0 mb-2"/>
+            <v-divider class="mt-0 mb-2" />
             <div class="history pl-4 mb-2">
               <ul>
                 <li v-for="(src, i) in getHistory(log.details)" :key="i">
-                  <VueMarkdownIt :source="src"/>
+                  <VueMarkdownIt :source="src" />
                   <span v-if="log.diff">
                     <router-link to="">
                       (변경 내용)
                       <v-tooltip activator="parent" location="start">
-                        <VueMarkdownIt :source="log.diff"/>
+                        <VueMarkdownIt :source="log.diff" />
                       </v-tooltip>
                     </router-link>
                   </span>
@@ -141,7 +141,7 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
                 </router-link>
                 이(가)
                 <router-link
-                    :to="{
+                  :to="{
                     name: '(작업내역)',
                     params: { projId: 'redmine' },
                     query: { from: log.timestamp.substring(0, 10) },
@@ -153,16 +153,16 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
               </CCol>
               <CCol class="text-right">#{{ log.pk }}</CCol>
             </CRow>
-            <v-divider class="mt-0 mb-2"/>
+            <v-divider class="mt-0 mb-2" />
             <div class="history pl-4 mb-2">
               <ul>
                 <li v-for="(src, i) in getHistory(log.details)" :key="i">
-                  <VueMarkdownIt :source="src"/>
+                  <VueMarkdownIt :source="src" />
                   <span v-if="log.diff">
                     <router-link to="">
                       (변경 내용)
                       <v-tooltip activator="parent" location="start">
-                        <VueMarkdownIt :source="log.diff"/>
+                        <VueMarkdownIt :source="log.diff" />
                       </v-tooltip>
                     </router-link>
                   </span>
@@ -173,17 +173,37 @@ const getHistory = (h: string) => h.split('|').filter(str => str.trim() !== '')
         </CTabPane>
 
         <CTabPane role="tabpanel" aria-labelledby="profile-tab" :visible="tabPaneActiveKey === 4">
-          <span class="history">
-            Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid.
-            Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson
-            artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo
-            enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud
-            organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia
-            yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes
-            anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson
-            biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente
-            accusamus tattooed echo park.
-          </span>
+          <div v-for="time in timeEntryList" :key="time.pk">
+            <CRow>
+              <CCol v-if="time.user">
+                <router-link :to="{ name: '사용자 - 보기', params: { userId: time.user.pk } }">
+                  {{ time.user.username }}
+                </router-link>
+                이(가)
+                <router-link
+                  :to="{
+                    name: '(작업내역)',
+                    params: { projId: 'redmine' },
+                    query: { from: time.created.substring(0, 10) },
+                  }"
+                >
+                  {{ elapsedTime(time.created) }}
+                </router-link>
+                에 추가함
+              </CCol>
+              <CCol class="text-right pr-3">
+                <v-icon icon="mdi-pencil" color="amber" size="sm" class="mr-2" />
+                <v-icon icon="mdi-trash-can" color="secondary" size="sm" />
+              </CCol>
+            </CRow>
+            <v-divider class="mt-0 mb-2" />
+            <div class="history pl-4 mb-2">
+              <ul>
+                <li>작업시간 : {{ time.hours }} 시간</li>
+              </ul>
+            </div>
+            <div class="mb-2">{{ time.comment }}</div>
+          </div>
         </CTabPane>
       </CTabContent>
     </CCardBody>
