57d97d0704967b548258faf29a3c67eb73762682
Austin Kho <kori.susie@gmail.com>
2024-04-18 20:26:34 +0900

    work header update

--- app/vue/src/views/_Work/components/Header/components/HeaderNav.vue
+++ app/vue/src/views/_Work/components/Header/components/HeaderNav.vue
@@ -21,14 +21,18 @@ const getTitle = (title: string) => title.replace(/[() ]/gim, '')
     <CDropdown v-if="$route.params['projId']">
       <CDropdownToggle :color="isDark ? 'dark' : 'light'" />
       <CDropdownMenu>
-        <CDropdownItem>새 업무 만들기</CDropdownItem>
-        <CDropdownItem v-if="superAuth">새 업무 카테고리</CDropdownItem>
-        <CDropdownItem v-if="superAuth">새 버전</CDropdownItem>
-        <CDropdownItem v-if="superAuth">작업시간 기록</CDropdownItem>
-        <CDropdownItem v-if="superAuth">새 뉴스</CDropdownItem>
-        <CDropdownItem v-if="superAuth">새 문서</CDropdownItem>
-        <CDropdownItem v-if="superAuth">새 위키</CDropdownItem>
-        <CDropdownItem v-if="superAuth">파일추가</CDropdownItem>
+        <CDropdownItem @click="$router.push({ name: '(업무) - 추가' })">
+          새 업무 만들기
+        </CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>새 업무 카테고리</CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>새 버전</CDropdownItem>
+        <CDropdownItem v-if="superAuth" @click="$router.push({ name: '(소요시간) - 추가' })">
+          작업시간 기록
+        </CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>새 뉴스</CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>새 문서</CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>새 위키</CDropdownItem>
+        <CDropdownItem v-if="superAuth" disabled>파일추가</CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
     <CNavItem v-for="(menu, i) in menus" :key="i">
