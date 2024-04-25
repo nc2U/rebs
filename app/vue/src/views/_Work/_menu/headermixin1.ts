fe36a2b06f8e97626f058db8fe3dea3d70dd0ab1
austin kho <kori.susie@gmail.com>
2024-04-25 15:16:48 +0900

    work menu update

--- app/vue/src/views/_Work/_menu/headermixin1.ts
+++ app/vue/src/views/_Work/_menu/headermixin1.ts
@@ -1,3 +1,4 @@
 export const pageTitle = '업 무 관 리'
 export const navMenu1 = ['프로젝트', '작업내역']
-export const navMenu2 = ['프로젝트', '작업내역', '업무', '소요시간', '차트', '달력', '공지']
+export const navMenu2 = ['프로젝트', '작업내역', '업무', '소요시간', '공지']
+// export const navMenu2 = ['프로젝트', '작업내역', '업무', '소요시간', '차트', '달력', '공지']
