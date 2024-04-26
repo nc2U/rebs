1365c8af16f9ae9ef7a6b73bb69e3a006b73cfea
austin kho <kori.susie@gmail.com>
2024-04-26 11:40:35 +0900

    work api update

--- app/django/apiV1/urls.py
+++ app/django/apiV1/urls.py
@@ -66,7 +66,7 @@ router.register(r'issue-comment', work.IssueCommentViewSet)
 router.register(r'time-entry', work.TimeEntryViewSet)
 router.register(r'act-entry', work.ActivityLogEntryViewSet)
 router.register(r'log-entry', work.IssueLogEntryViewSet)
-router.register(r'search', work.SearchViewSet)
+router.register(r'issue-search', work.SearchViewSet)
 # project
 router.register(r'project', project.ProjectViewSet)
 router.register(r'inc-budget', project.ProjectIncBudgetViewSet)  # only list
