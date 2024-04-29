c2beae7a1e7844e8cb053da20e5b0e52187cbd95
Austin Kho <kori.susie@gmail.com>
2024-04-29 20:52:45 +0900

    work api update

--- app/django/apiV1/serializers/work.py
+++ app/django/apiV1/serializers/work.py
@@ -706,9 +706,27 @@ class TimeEntryInActivityLogSerializer(serializers.ModelSerializer):
 
 
 class NewsSerializer(serializers.ModelSerializer):
+    project = FamilyTreeSerializer(read_only=True)
+
     class Meta:
         model = News
-        fields = ('pk', 'title', 'summary', 'description', 'author', 'created')
+        fields = ('pk', 'project', 'title', 'summary', 'description', 'author', 'created')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        project__slug = self.initial_data.get('project__slug', None)
+        project = IssueProject.objects.get(slug=project__slug)
+        news = News.objects.create(**validated_data, project=project)
+        return news
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        instance.__dict__.update(**validated_data)
+        project = self.initial_data.get('project__slug', None)
+        if instance.project.slug != project:
+            instance.project = IssueProject.objects.get(slug=project)
+        instance.save()
+        return instance
 
 
 class ActivityLogEntrySerializer(serializers.ModelSerializer):
