0eba1cc142734e2c6aa24889749a9aca77d16d99
Austin Kho <kori.susie@gmail.com>
2024-05-02 23:34:46 +0900

    work model & signal update

--- app/vue/src/views/_Work/Manages/Issues/components/aside/AsideIssue.vue
+++ app/vue/src/views/_Work/Manages/Issues/components/aside/AsideIssue.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
-defineProps({ watchers: { type: Array, default: () => [] } })
+import { inject, type PropType } from 'vue'
+
+defineProps({
+  watchers: { type: Array as PropType<{ pk: number; username: string }[]>, default: () => [] },
+})
+
+const superAuth = inject('superAuth')
+
+const delWatcher = (pk: number) => alert(pk)
 </script>
 
 <template>
@@ -10,14 +18,26 @@ defineProps({ watchers: { type: Array, default: () => [] } })
   <!--    <CCol class="col-xxl-5"></CCol>-->
   <!--  </CRow>-->
 
-  <CRow v-if="$route.name === '(업무) - 보기'" class="mb-3">
-    <CCol><h6 class="asideTitle">업무 관람자</h6></CCol>
-  </CRow>
-  <CRow class="mb-2">
-    <CCol class="col-xxl-5">
-      {{ watchers }}
-    </CCol>
-  </CRow>
+  <template v-if="superAuth && $route.name === '(업무) - 보기'">
+    <CRow class="mb-1">
+      <CCol>
+        <h6 class="asideTitle">업무 관람자 ({{ watchers.length }})</h6>
+      </CCol>
+      <CCol class="text-right">
+        <router-link to="">추가</router-link>
+      </CCol>
+    </CRow>
+    <CRow v-for="watcher in watchers" :key="watcher.pk">
+      <CCol class="col-xxl-5">
+        <router-link :to="{ name: '사용자 - 보기', params: { userId: watcher.pk } }">
+          {{ watcher.username }}
+        </router-link>
+        <span @click="delWatcher(watcher.pk)">
+          <v-icon icon="mdi-trash-can-outline" size="sm" color="grey" class="ml-2 pointer" />
+        </span>
+      </CCol>
+    </CRow>
+  </template>
 </template>
 
 <style lang="scss" scoped>
