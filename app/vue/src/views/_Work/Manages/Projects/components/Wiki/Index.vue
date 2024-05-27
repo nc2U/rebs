2b62eb0be7d31a23f2c010f92a3d4a2c417f35f2
austin-kho <kori.susie@gmail.com>
2024-05-27 18:00:40 +0900

    work wiki layout update

--- app/vue/src/views/_Work/Manages/Projects/components/Wiki/Index.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Wiki/Index.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount } from 'vue'
+import { ref, computed, onBeforeMount } from 'vue'
 import { useRoute } from 'vue-router'
+import MdEditor from '@/components/MdEditor/Index.vue'
 
 const emit = defineEmits(['aside-visible'])
 
 const route = useRoute()
+
+const form = ref({
+  wiki: '',
+  description: '',
+  files: [],
+})
 
 const capitalize = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 
@@ -12,7 +19,10 @@ const wikiTitle = computed(() =>
   route.params.title ? capitalize(route.params.title as string) : 'Wiki',
 )
 
-onBeforeMount(() => emit('aside-visible', false))
+onBeforeMount(() => {
+  emit('aside-visible', false)
+  form.value.wiki = `# ${wikiTitle.value}`
+})
 </script>
 
 <template>
@@ -22,13 +32,27 @@ onBeforeMount(() => emit('aside-visible', false))
     </CCol>
   </CRow>
 
-  <CRow>
-    <CCol>
-      <CFormTextarea> #{{ wikiTitle }}</CFormTextarea>
+  <CRow class="mb-3">
+    <CCol sm="11">
+      <MdEditor v-model="form.wiki" />
     </CCol>
   </CRow>
 
-  <CRow class="mt-2">
+  <CRow class="mb-3">
+    <CFormLabel for="desc" class="col-form-label text-center col-1">설명</CFormLabel>
+    <CCol class="col-10">
+      <CFormInput v-model="form.description" id="desc" />
+    </CCol>
+  </CRow>
+
+  <CRow class="mb-4">
+    <CFormLabel for="files" class="col-form-label text-center col-1">파일</CFormLabel>
+    <CCol class="col-10">
+      <CFormInput v-model="form.files" type="file" id="files" />
+    </CCol>
+  </CRow>
+
+  <CRow>
     <CCol>
       <CButton color="primary">저장</CButton>
       <CButton color="light">취소</CButton>
