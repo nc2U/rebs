d37d261eb0d9a0f5e56ff6781396cebd16d97ae2
Austin Kho <kori.susie@gmail.com>
2024-05-28 22:01:35 +0900

    work roadmap app update ing

--- app/vue/src/views/_Work/Manages/Projects/components/Roadmap/components/VersionView.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Roadmap/components/VersionView.vue
@@ -1,17 +1,38 @@
 <script lang="ts" setup>
-import { onBeforeMount, type PropType } from 'vue'
+import { computed, onBeforeMount, type PropType } from 'vue'
 import { useRoute } from 'vue-router'
 import { useWork } from '@/store/pinia/work'
 import type { Version } from '@/store/types/work'
 import IssueDropDown from '@/views/_Work/Manages/Issues/components/IssueDropDown.vue'
 
-defineProps({ version: { type: Object as PropType<Version>, required: true } })
+const props = defineProps({ version: { type: Object as PropType<Version>, required: true } })
 
 const emit = defineEmits(['aside-visible'])
 
 const workStore = useWork()
 
 const boxClass = ['primary-box', 'danger-box', 'success-box']
+
+const closedNum = computed(() => props.version?.issues?.filter(i => i.closed).length ?? 0)
+const closedStr = computed(() => {
+  if (closedNum.value === 0) return '모두 미완료'
+  else if (closedNum.value === 1) return '한 건 완료'
+  else return `${closedNum.value} 건 완료`
+})
+
+const progressNum = computed(() => props.version?.issues?.filter(i => !i.closed).length ?? 0)
+const progressStr = computed(() => {
+  if (progressNum.value === 0) return '모두 완료'
+  else if (progressNum.value === 1) return '한 건 진행 중'
+  else return `${progressNum.value} 건 진행 중`
+})
+
+const done_ratio = computed(() => {
+  const done_sum = props.version.issues?.reduce((sum, issue) => sum + issue.done_ratio, 0) ?? 0
+
+  if (!props.version?.issues?.length) return 0
+  else return Math.round(done_sum / props.version?.issues?.length)
+})
 
 const route = useRoute()
 onBeforeMount(() => {
@@ -40,22 +61,28 @@ onBeforeMount(() => {
   <template v-else>
     <CRow>
       <CCol class="col-sm-10 col-md-8 col-lg-6 col-xl-4 p-0 mx-2">
-        <CProgress color="success" :value="50" :style="{ '--cui-border-radius': 0 }">
-          50%
+        <CProgress color="success" :value="done_ratio" :style="{ '--cui-border-radius': 0 }">
+          {{ done_ratio }}%
         </CProgress>
       </CCol>
     </CRow>
+
     <CRow class="mb-4">
       <CCol class="form-text">
         <span>
-          <router-link to="">6 업무</router-link>
+          <router-link to="">업무 {{ version?.issues?.length }} 건</router-link>
         </span>
-        <span>( <router-link to="">5 건 완료</router-link> - </span>
-        <span> <router-link to="">한 건 진행 중</router-link>) </span>
+        <span>
+          ( <router-link to="">{{ closedStr }}</router-link> -
+        </span>
+        <span>
+          <router-link to="">{{ progressStr }}</router-link
+          >)
+        </span>
       </CCol>
     </CRow>
 
-    <CRow>
+    <CRow class="flex-md-row flex-column-reverse">
       <CCol md="8" class="mb-4">
         <h6>연결된 업무</h6>
         <v-divider class="mb-0" />
@@ -86,34 +113,53 @@ onBeforeMount(() => {
       </CCol>
 
       <CCol md="4" class="mb-4">
-        <CRow class="mb-2">
-          <CCol class="col-4 col-lg-6 col-xl-4">
-            <CFormSelect readonly disabled>
-              <option>유형</option>
-              <option>상태</option>
-              <option>우선순위</option>
-              <option>저자</option>
-              <option>담당자</option>
-              <option>범주</option>
-            </CFormSelect>
-          </CCol>
-          <CCol style="padding-top: 6px">별 업무</CCol>
-        </CRow>
-        <div class="p-2 border">
-          <CRow class="my-2">
-            <CCol class="col-3 text-right">결함</CCol>
-            <CCol class="col-6">
-              <CProgress value="10" color="success" />
-            </CCol>
-            <CCol class="col-3">4/5</CCol>
+        <CRow class="mb-4">
+          <CCol>
+            <div class="p-2 border bold">
+              <h6>시간추적</h6>
+              <CRow class="my-2">
+                <CCol class="col-6 text-center">추정시간</CCol>
+                <CCol class="col-6 text-right pr-5"> 5:00 시간</CCol>
 
-            <CCol class="col-3 text-right">기능</CCol>
-            <CCol class="col-6">
-              <CProgress />
-            </CCol>
-            <CCol class="col-3">1/1</CCol>
-          </CRow>
-        </div>
+                <CCol class="col-6 text-center">소요시간</CCol>
+                <CCol class="col-6 text-right pr-5"> 0:00 시간</CCol>
+              </CRow>
+            </div>
+          </CCol>
+        </CRow>
+
+        <CRow class="mb-4">
+          <CCol>
+            <div class="p-2 border">
+              <CRow class="mb-2">
+                <CCol class="col-4 col-lg-6 col-xl-4">
+                  <CFormSelect readonly disabled>
+                    <option>유형</option>
+                    <option>상태</option>
+                    <option>우선순위</option>
+                    <option>저자</option>
+                    <option>담당자</option>
+                    <option>범주</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol style="padding-top: 6px">별 업무</CCol>
+              </CRow>
+              <CRow class="my-2">
+                <CCol class="col-3 text-right">결함</CCol>
+                <CCol class="col-6">
+                  <CProgress value="10" color="success" />
+                </CCol>
+                <CCol class="col-3">4/5</CCol>
+
+                <CCol class="col-3 text-right">기능</CCol>
+                <CCol class="col-6">
+                  <CProgress />
+                </CCol>
+                <CCol class="col-3">1/1</CCol>
+              </CRow>
+            </div>
+          </CCol>
+        </CRow>
       </CCol>
     </CRow>
   </template>
