3925702cefc7f7bf389e63db0455c728c2999a72
Austin Kho <kori.susie@gmail.com>
2024-06-01 02:50:46 +0900

    workflows update

--- volume/backups/script_dev_restore.sh
+++ volume/backups/script_dev_restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d")
-SQL_FILE=/var/backups/db-${MYSQL_DATABASE}-${DATE}.sql
+SQL_FILE=/docker-entrypoint-initdb.d/db-${MYSQL_DATABASE}-${DATE}.sql
 USER=${MYSQL_USER}
 PASSWORD=${MYSQL_PASSWORD}
 DATABASE=${MYSQL_DATABASE}
 
-mariadb -u"${USER}" -p"${PASSWORD}" "${DATABASE}" < /docker-entrypoint-initdb.d/"${SQL_FILE}"
+mariadb -u"${USER}" -p"${PASSWORD}" "${DATABASE}" < "${SQL_FILE}"
