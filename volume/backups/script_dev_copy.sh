abce47b532f37eb8fa8ee9aa185e829c095fb698
Austin Kho <kori.susie@gmail.com>
2024-06-01 02:47:10 +0900

    workflows update

--- volume/backups/script_dev_copy.sh
+++ volume/backups/script_dev_copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d")
-SQL_FILE=db-${MYSQL_DATABASE}-${DATE}.sql
+SQL_FILE=db-rebs-${DATE}.sql
 
 cp /mnt/nfs/prod/volume/backups/"${SQL_FILE}" /mnt/nfs/dev/volume/sql/
