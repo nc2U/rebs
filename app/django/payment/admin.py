46a3f8824f461921e8f6785605018e9fb0e74dae
Austin Kho <kori.susie@gmail.com>
2024-06-04 23:39:55 +0900

    payment model & admin update

--- app/django/payment/admin.py
+++ app/django/payment/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from import_export.admin import ImportExportMixin
 
-from .models import SalesPriceByGT, InstallmentPaymentOrder, DownPayment, OverDueRule
+from .models import (SalesPriceByGT, InstallmentPaymentOrder, DownPayment, OverDueRule,
+                     SpecialPaymentOrder, SpecialDownPayment, SpecialOverDueRule)
 
 
+@admin.register(SalesPriceByGT)
 class SalesPriceByGTAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = (
         'id', 'project', 'order_group', 'unit_type', 'unit_floor_type', 'price_build', 'price_land', 'price_tax',
@@ -13,6 +15,7 @@ class SalesPriceByGTAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ('project', 'order_group', 'unit_type')
 
 
+@admin.register(InstallmentPaymentOrder)
 class InstallmentPaymentOrderAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = (
         'id', 'project', 'pay_name', 'pay_sort', 'pay_code', 'pay_time', 'pay_ratio',
@@ -23,6 +26,7 @@ class InstallmentPaymentOrderAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ('project', 'pay_sort')
 
 
+@admin.register(DownPayment)
 class DownPaymentAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('id', 'project', 'order_group', 'unit_type', 'payment_amount')
     list_display_links = ('project', 'order_group', 'unit_type')
@@ -30,6 +34,7 @@ class DownPaymentAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ('project', 'order_group', 'unit_type')
 
 
+@admin.register(OverDueRule)
 class OverDueRuleAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('id', 'project', '__str__', 'term_start', 'term_end', 'rate_year')
     list_display_links = ('__str__',)
@@ -37,7 +42,28 @@ class OverDueRuleAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ('project',)
 
 
-admin.site.register(SalesPriceByGT, SalesPriceByGTAdmin)
-admin.site.register(InstallmentPaymentOrder, InstallmentPaymentOrderAdmin)
-admin.site.register(DownPayment, DownPaymentAdmin)
-admin.site.register(OverDueRule, OverDueRuleAdmin)
+@admin.register(SpecialPaymentOrder)
+class SpecialPaymentOrderAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = (
+        'id', 'project', 'pay_name', 'pay_sort', 'pay_code', 'pay_time', 'pay_ratio',
+        'alias_name', 'is_pm_cost', 'days_since_prev', 'pay_due_date', 'extra_due_date')
+    search_fields = ('pay_name', 'alias_name',)
+    list_editable = ('alias_name', 'is_pm_cost', 'days_since_prev', 'pay_due_date', 'extra_due_date')
+    list_display_links = ('project', 'pay_name',)
+    list_filter = ('project', 'pay_sort')
+
+
+@admin.register(SpecialDownPayment)
+class SpecialDownPaymentAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('id', 'project', 'order_group', 'unit_type', 'payment_amount')
+    list_display_links = ('project', 'order_group', 'unit_type')
+    list_editable = ('payment_amount',)
+    list_filter = ('project', 'order_group', 'unit_type')
+
+
+@admin.register(SpecialOverDueRule)
+class SpecialOverDueRuleAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('id', 'project', '__str__', 'term_start', 'term_end', 'rate_year')
+    list_display_links = ('__str__',)
+    list_editable = ('term_start', 'term_end', 'rate_year')
+    list_filter = ('project',)
--- app/django/payment/models.py
+++ app/django/payment/models.py
@@ -80,3 +80,65 @@ class OverDueRule(models.Model):
         ordering = ('-id',)
         verbose_name = '04. 선납할인/연체이율 관리'
         verbose_name_plural = '04. 선납할인/연체이율 관리'
+
+
+class SpecialPaymentOrder(models.Model):  # 가산금 / 할인액 계산을 위한 별도 테이블
+    project = models.ForeignKey('project.Project', on_delete=models.CASCADE, verbose_name='프로젝트')
+    SORT_CHOICES = (('1', '계약금'), ('2', '중도금'), ('3', '잔금'))
+    pay_sort = models.CharField('종류', max_length=1, choices=SORT_CHOICES, default='1')
+    pay_code = models.PositiveSmallIntegerField('납입회차 코드', help_text='프로젝트 내에서 모든 납부회차를 고유 순서대로 숫자로 부여한다.')
+    pay_time = models.PositiveSmallIntegerField('납부순서',
+                                                help_text='''동일 납부회차에 2가지 항목을 별도로 납부하여야 하는 경우(ex: 분담금 + 업무대행료)
+                                                하나의 납입회차 코드(ex: 1)에 2개의 납부순서(ex: 1, 2)를 등록한다.''')
+    pay_ratio = models.DecimalField('회당 납부비율(%)', max_digits=7, decimal_places=4, null=True, blank=True,
+                                    help_text='''분양가 대비 납부비율, 계약금 항목인 경우 Downpayment 
+                                    테이블 데이터 우선, 잔금 항목인 경우 분양가와 비교 차액 데이터 우선''')
+    is_pm_cost = models.BooleanField('PM용역비 여부', default=False)
+    pay_name = models.CharField('납부회차 명', max_length=20)
+    alias_name = models.CharField('회차 별칭', max_length=20, blank=True)
+    days_since_prev = models.PositiveSmallIntegerField('전회 기준 경과일수', null=True, blank=True,
+                                                       help_text="전 회차(예: 계약일)로부터 __일 이내 형식으로 납부기한을 지정할 경우 해당 일수")
+    pay_due_date = models.DateField('지정 납부기한', null=True, blank=True, help_text="특정일자를 납부기한으로 지정할 경우")
+    extra_due_date = models.DateField('납부유예일', null=True, blank=True,
+                                      help_text='연체료 계산 기준은 지정 납부기한이 원칙이나 이 값이 있는 경우 납부유예일을 연체료 계산 기준으로 한다.')
+
+    def __str__(self):
+        return f'[{self.get_pay_sort_display()}] - {self.pay_name}'
+
+    class Meta:
+        ordering = ['-project', 'pay_code']
+        verbose_name = '05. 특별 납입회차'
+        verbose_name_plural = '05. 특별 납입회차'
+
+
+class SpecialDownPayment(models.Model):  # 가산금 / 할인액 계산을 위한 별도 테이블
+    project = models.ForeignKey('project.Project', on_delete=models.CASCADE, verbose_name='프로젝트')
+    order_group = models.ForeignKey('contract.OrderGroup', on_delete=models.CASCADE, verbose_name='차수정보')
+    unit_type = models.ForeignKey('items.UnitType', on_delete=models.CASCADE, verbose_name='타입정보')
+    payment_amount = models.PositiveIntegerField('회차별 계약금액',
+                                                 help_text='차수 및 타입별 고정 납부 계약금액, 납부 회수는 납부 회차 모델에서 별도 등록/설정')
+
+    def __str__(self):
+        return f'{self.payment_amount}'
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = '06. 특별 타입별 계약금'
+        verbose_name_plural = '06. 특별 타입별 계약금'
+
+
+class SpecialOverDueRule(models.Model):  # 가산금 / 할인액 계산을 위한 별도 테이블
+    project = models.ForeignKey('project.Project', on_delete=models.CASCADE, verbose_name='프로젝트')
+    term_start = models.IntegerField('최소연체일', null=True, blank=True, help_text='비어 있을 경우 최대 음수')
+    term_end = models.IntegerField('최대연체일', null=True, blank=True, help_text='비어 있을 경우 최대 양수')
+    rate_year = models.DecimalField('연체이율', max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        ts = str(self.term_start) + '일' if self.term_start != None else 'Min'
+        te = str(self.term_end) + '일' if self.term_end != None else 'Max'
+        return f'{ts} - {te}'
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = '07. 특별 선납할인/연체이율'
+        verbose_name_plural = '07. 특별 선납할인/연체이율'
