c34de6a319694e5c5ee26f42c1bfb907c23ea079
austin-kho <kori.susie@gmail.com>
2024-06-27 16:56:05 +0900

    proCash imprest index update

--- app/vue/src/views/proCash/Imprest/Index.vue
+++ app/vue/src/views/proCash/Imprest/Index.vue
@@ -23,7 +23,7 @@ const listControl = ref()
 
 const bankFees = ref([14, 58]) // 은행수수료 d2(id), d3(id)
 const transferD3 = ref([70, 71]) // 대체 출금(id), 입금(id)
-const cancelD3 = ref([72, 73]) // 취소 출금(id), 입금(id
+const cancelD3 = ref([72, 73]) // 취소 출금(id), 입금(id)
 
 provide('transfers', [17, 70]) // 대체 출금 d2(id), d3(id)
 provide('cancels', [18, 72]) // 취소 출금 d2(id), d3(id)
