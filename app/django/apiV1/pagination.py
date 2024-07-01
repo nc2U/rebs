4a3d16c84f92ed13da6767b9b69793d725f9bb13
austin-kho <kori.susie@gmail.com>
2024-07-01 16:38:15 +0900

    work api update

--- app/django/apiV1/pagination.py
+++ app/django/apiV1/pagination.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
@@ -11,30 +12,93 @@ class LimitOffsetPaginationCustomBasic(LimitOffsetPagination):
 
 class PageNumberPaginationThreeThousand(PageNumberPagination):
     page_size = 3000
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationOneThousand(PageNumberPagination):
     page_size = 1000
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationFiveHundred(PageNumberPagination):
     page_size = 500
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationThreeHundred(PageNumberPagination):
     page_size = 300
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationTwoHundred(PageNumberPagination):
     page_size = 200
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationOneHundred(PageNumberPagination):
     page_size = 100
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationFifty(PageNumberPagination):
     page_size = 50
+    page_size_query_param = 'limit'  # 쿼리 파라미터로 사용할 이름
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
 
 class PageNumberPaginationTwenty(PageNumberPagination):
