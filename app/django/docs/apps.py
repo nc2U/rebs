01fe8aa2f476342cef5295997fc5f34d8a3549f2
austin-kho <kori.susie@gmail.com>
2024-08-08 17:43:56 +0900

    ducument -> board move update

--- app/django/docs/apps.py
+++ app/django/docs/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class DocsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'docs'
-    verbose_name = '*** 문서관리2'
+    verbose_name = '*** 문서관리'
