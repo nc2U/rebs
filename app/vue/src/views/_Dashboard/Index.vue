a49b36c36d180f164549cfbd588a1b754af01cfc
Austin Kho <kori.susie@gmail.com>
2024-09-06 22:45:34 +0900

    dashboard update

--- app/vue/src/views/_Dashboard/Index.vue
+++ app/vue/src/views/_Dashboard/Index.vue
@@ -5,9 +5,9 @@ import MainCarousel from './components/MainCarousel.vue'
 import WiseWord from './components/WiseWord.vue'
 import MyIssue from '@/views/_Work/MyIssue/Index.vue'
 import NoticeBoard from './components/NoticeBoard/ListComp.vue'
-import WidgetsStatsA from './components/Widgets/WidgetsStatsTypeA.vue'
+// import WidgetsStatsA from './components/Widgets/WidgetsStatsTypeA.vue'
 import WidgetsStatsB from './components/Widgets/WidgetsStatsTypeB.vue'
-import WidgetsStatsC from './components/Widgets/WidgetsStatsTypeC.vue'
+// import WidgetsStatsC from './components/Widgets/WidgetsStatsTypeC.vue'
 
 const noticeRoute = ref('공지 사항')
 
@@ -30,32 +30,32 @@ onBeforeMount(() => boardStore.fetchPostList({ board: 1 }))
         <WiseWord />
       </CCol>
     </CRow>
+    <!--    <CRow>-->
+    <!--      <CCol>-->
+    <!--        <WidgetsStatsA />-->
+    <!--      </CCol>-->
+    <!--    </CRow>-->
+    <!--    <CRow>-->
+    <!--      <CCol>-->
+    <!--        <WidgetsStatsC />-->
+    <!--      </CCol>-->
+    <!--    </CRow>-->
+    <CRow class="mb-3">
+      <CCol>
+        <MyIssue />
+      </CCol>
+    </CRow>
     <CRow>
-      <CCol xl="6">
+      <CCol xl="12">
         <NoticeBoard
           :main-view-name="noticeRoute"
           :notice-list="noticeList"
           :post-list="postList"
         />
       </CCol>
-      <CCol xl="6">
-        <WidgetsStatsB />
-      </CCol>
-    </CRow>
-    <CRow>
-      <CCol>
-        <WidgetsStatsA />
-      </CCol>
-    </CRow>
-    <CRow>
-      <CCol>
-        <WidgetsStatsC />
-      </CCol>
-    </CRow>
-    <CRow class="mb-3">
-      <CCol>
-        <MyIssue />
-      </CCol>
+      <!--      <CCol xl="6">-->
+      <!--        <WidgetsStatsB />-->
+      <!--      </CCol>-->
     </CRow>
   </CContainer>
 </template>
