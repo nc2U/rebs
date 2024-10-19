3fc4adf8900d11246e76123ce973453ba32db09b
Austin Kho <kori.susie@gmail.com>
2024-10-19 19:44:19 +0900

    workflows update

--- volume/backups/script_copy.sh
+++ volume/backups/script_copy.sh
@@ -2,4 +2,17 @@
 DATE=$(date +"%Y-%m-%d")
 SQL_FILE=db-rebs-${DATE}.sql
 
-cp "${SQL_FILE}" ../../../dev/volume/sql/
+# 파일 존재 여부 확인
+if [ -f "${SQL_FILE}" ]; then
+    # 디렉토리 존재 여부 확인 및 없으면 생성
+    DEST_DIR=../../../dev/volume/sql/
+    if [ ! -d "${DEST_DIR}" ]; then
+        mkdir -p "${DEST_DIR}"
+    fi
+
+    # 파일 복사
+    cp "${SQL_FILE}" "${DEST_DIR}"
+    echo "File ${SQL_FILE} copied to ${DEST_DIR}"
+else
+    echo "File ${SQL_FILE} does not exist."
+fi
