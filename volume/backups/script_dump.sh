095d1ee01f58cfa974fd8469ac4ba63c0790b96b
Austin Kho <kori.susie@gmail.com>
2024-10-19 20:25:00 +0900

    backup script update

--- volume/backups/script_dump.sh
+++ volume/backups/script_dump.sh
@@ -5,7 +5,14 @@ SQL_FILE=/var/backups/backup-${DATE}.sql
 # (2) in case you run this more than once a day,
 # remove the previous version of the file
 # shellcheck disable=SC2046
-rm -rf $(find ./ -name "*.sql"  -mtime +2 -delete)
+find ./ -name "*.sql" -mtime +2 -delete
 
 # (3) do the mysql database backup (dump)
 mariadb-dump -u"${USER}" -p"${PASSWORD}" "${DATABASE}" --ignore-table="${DATABASE}".django_migrations > "${SQL_FILE}"
+
+# 백업이 성공했는지 확인
+if [ $? -eq 0 ]; then
+    echo "Backup completed successfully: ${SQL_FILE}"
+else
+    echo "Backup failed"
+fi
--- volume/backups/script_restore.sh
+++ volume/backups/script_restore.sh
@@ -3,3 +3,10 @@ DATE=$(date +"%Y-%m-%d")
 SQL_FILE=/var/backups/backup-${DATE}.sql
 
 mariadb -u"${USER}" -p"${PASSWORD}" "${DATABASE}" < "${SQL_FILE}"
+
+# 복원 성공 여부 확인
+if [ $? -eq 0 ]; then
+    echo "Database restoration completed successfully: ${SQL_FILE}"
+else
+    echo "Database restoration failed"
+fi
