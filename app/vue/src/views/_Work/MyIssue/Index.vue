53b3e4f4002ab91cbebc32941c26cbb04ea42616
austin-kho <kori.susie@gmail.com>
2024-10-22 15:29:40 +0900

    myissue update

--- app/vue/src/views/_Work/MyIssue/Index.vue
+++ app/vue/src/views/_Work/MyIssue/Index.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { GridLayout } from 'grid-layout-plus'
+import { GridLayout, type LayoutItemRequired } from 'grid-layout-plus'
 
-const layout = reactive([
-  { x: 0, y: 0, w: 6, h: 3, i: '0' },
-  { x: 6, y: 0, w: 6, h: 3, i: '1' },
+const layouts = reactive([
+  { x: 0, y: 0, w: 6, h: 3, i: '내가 맡은 업무 (0)' },
+  { x: 6, y: 0, w: 6, h: 3, i: '보고한 업무 (0)' },
 ])
+
+interface LayoutItem extends LayoutItemRequired {
+  minW?: number
+  minH?: number
+  maxW?: number
+  maxH?: number
+  moved?: boolean
+  static?: boolean
+  isDraggable?: boolean
+  isResizable?: boolean
+}
+
+type Layout = Array<LayoutItem>
+type Breakpoint = 'xxs' | 'xs' | 'sm' | 'md' | 'lg'
+type Breakpoints = Record<Breakpoint, number>
+type ResponsiveLayout = Record<Breakpoint, Layout>
 </script>
 
 <template>
   <CCard>
     <CCardBody>
-      <CRow>
-        <CCol>
-          <CFormSelect style="width: 200px" class="align-self-end">
+      <CRow class="px-2">
+        <CCol style="display: flex; justify-content: flex-end">
+          <CFormSelect style="width: 200px">
             <option>추가하기</option>
             <option>내가 맡은 업무</option>
             <option>보고한 업무</option>
@@ -30,7 +46,7 @@ const layout = reactive([
       </CRow>
       <!-- Item slot usage -->
       <GridLayout
-        v-model:layout="layout"
+        v-model:layout="layouts"
         :col-num="12"
         :row-height="30"
         is-draggable
@@ -40,7 +56,7 @@ const layout = reactive([
       >
         <template #item="{ item }">
           <div class="w-100 h-100 border p-3">
-            <CRow class="text-right px-2 mb-1">
+            <CRow class="px-2 mb-1">
               <CCol>{{ item.i }}</CCol>
             </CRow>
             <CAlert color="warning">표시할 데이터가 없습니다.</CAlert>
