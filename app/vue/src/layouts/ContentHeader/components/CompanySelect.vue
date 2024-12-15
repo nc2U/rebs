c80dbb2d2822e309192e98602ccaf68be0474709
Austin Kho <kori.susie@gmail.com>
2024-12-15 16:18:41 +0900

    initCompany debug

--- app/vue/src/layouts/ContentHeader/components/CompanySelect.vue
+++ app/vue/src/layouts/ContentHeader/components/CompanySelect.vue
@@ -26,22 +26,22 @@ onBeforeMount(() => {
     <CFormLabel class="col-lg-1 col-form-label text-body">회사명</CFormLabel>
     <CCol md="6" lg="3">
       <Multiselect
-        :value="company"
-        :options="comSelectList as { value: number; label: string }[]"
-        placeholder="회사선택"
-        autocomplete="label"
-        :classes="{ search: 'form-control multiselect-search' }"
-        :add-option-on="['enter', 'tab']"
-        searchable
-        @select="comSelect"
-        @clear="comClear"
+          :value="company"
+          :options="comSelectList"
+          placeholder="회사선택"
+          autocomplete="label"
+          :classes="{ search: 'form-control multiselect-search' }"
+          :add-option-on="['enter', 'tab']"
+          searchable
+          @select="comSelect"
+          @clear="comClear"
       />
     </CCol>
     <CCol v-if="!comSelectList?.length" class="pl-0 align-middle">
       <v-icon
-        icon="mdi mdi-plus-thick"
-        color="primary"
-        @click="router.push({ name: '회사 정보 관리' })"
+          icon="mdi mdi-plus-thick"
+          color="primary"
+          @click="router.push({ name: '회사 정보 관리' })"
       />
     </CCol>
   </CRow>
