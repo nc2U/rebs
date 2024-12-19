bc833d9acbd59c388fd12aeb077bdbf1acecf5e9
Austin Kho <kori.susie@gmail.com>
2024-12-19 23:45:46 +0900

    notice admin update

--- app/django/notice/admin.py
+++ app/django/notice/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import SalesBillIssue
 
 
+@admin.register(SalesBillIssue)
 class SalesBillIssueAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(SalesBillIssue, SalesBillIssueAdmin)
